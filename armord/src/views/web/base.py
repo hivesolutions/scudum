5f14366731a99a1b2b939213ed0a4d2cab35b177
joamag <joamag@gmail.com>
2013-04-01 18:46:19 +0100

    new views added

---
+++ armord/src/views/web/base.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Armor Daemon
+# Copyright (c) 2008-2012 Hive Solutions Lda.
+#
+# This file is part of Hive Armor Daemon.
+#
+# Hive Armor Daemon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Armor Daemon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Armor Daemon. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import models
+
+from armord import app
+from armord import flask
+from armord import quorum
+
+@app.route("/", methods = ("GET",))
+@app.route("/index", methods = ("GET",))
+@quorum.ensure("index")
+def index():
+    return flask.render_template(
+        "index.html.tpl",
+        link = "home"
+    )
+
+@app.route("/about", methods = ("GET",))
+@quorum.ensure("about")
+def about():
+    return flask.render_template(
+        "about.html.tpl",
+        link = "about"
+    )
+
+@app.route("/signin", methods = ("GET",))
+def signin():
+    return flask.render_template(
+        "signin.html.tpl"
+    )
+
+@app.route("/signin", methods = ("POST",))
+def login():
+    username = quorum.get_field("username")
+    password = quorum.get_field("password")
+    try: account = models.Account.login(username, password)
+    except quorum.OperationalError, error:
+        return flask.render_template(
+            "signin.html.tpl",
+            username = username,
+            error = error.message
+        )
+
+    # updates the current user (name) in session with
+    # the username that has just be accepted in the login
+    flask.session["username"] = account.username
+    flask.session["tokens"] = account.tokens
+    flask.session["instance_id"] = account.instance_id
+    flask.session["acl"] = quorum.check_login
+
+    # makes the current session permanent this will allow
+    # the session to persist along multiple browser initialization
+    flask.session.permanent = True
+
+    return flask.redirect(
+        flask.url_for("index")
+    )
+
+@app.route("/signout", methods = ("GET", "POST"))
+def logout():
+    if "username" in flask.session: del flask.session["username"]
+    if "tokens" in flask.session: del flask.session["tokens"]
+    if "instance_id" in flask.session: del flask.session["instance_id"]
+    if "nav_data" in flask.session: del flask.session["nav_data"]
+
+    return flask.redirect(
+        flask.url_for("signin")
+    )
+
+@app.route("/accounts/<username>", methods = ("GET",))
+@quorum.ensure("accounts.show")
+def show_account(username):
+    account = models.Account.get(username = username)
+    return flask.render_template(
+        "account_show.html.tpl",
+        link = "accounts",
+        sub_link = "info",
+        account = account
+    )
