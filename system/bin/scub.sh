f200de2669b1fccbf4cd44e04c1545d289268715
joamag <joamag@gmail.com>
2014-01-07 22:57:37 +0000

    new build structure

--- system/bin/scub.sh
+++ system/bin/scub.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+TARGET=${TARGET-.}
 NAME=${NAME-program}
 VERSION=${VERSION-1.0.0}
+ARCH=${ARCH-amd63}
 HOST=${HOST-files.hive}
 USERNAME=${USERNAME-anonymous}
 PASSWORD=${PASSWORD-anonymous}
-NAME_F=$NAME_$VERSION
+NAME_F=$NAME-$VERSION-$ARCH
+FILE=$NAME_F.scu
 
-tar -cjf $NAME_F.scu *
-scp $NAME_F.tar.gz $USERNAME@$HOST:/scu
+# changes the current directory to the target one
+# and then compresses all of the files contained
+# in the directory into the target file
+cd $TARGET
+tar -cjf $FILE * > /dev/null
+
+# copies the file that was just created into the
+# target repository and then removes it from the file
+# system as it's no longer going to be used
+scp -q $FILE $USERNAME@$HOST:/scu
+rm $FILE
