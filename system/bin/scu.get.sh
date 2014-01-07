a9dc8f3932864eef0d23a8546a3fe68c3bd87609
joamag <joamag@gmail.com>
2014-01-07 23:51:31 +0000

    new script file

--- system/bin/scu.get.sh
+++ system/bin/scu.get.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+# allocates and sets the values of the various
+# variables that are going to be used for the
+# retrieval and installation of the file
 NAME=${NAME-program}
 VERSION=${VERSION-1.0.0}
 ARCH=${ARCH-amd64}
@@ -8,5 +11,7 @@ URL=${URL-http://hole1.hive:9090/repos/scu}
 NAME_F="$NAME"_"$VERSION"_"$ARCH"
 FILE=$NAME_F.scu
 
+# retrieves the target file from the repository
+# and then uses the proper script for installation
 wget $URL/$FILE
 scu.install $FILE
