a496cef915749041b30ce1dffb7e24917488034d
joamag <joamag@gmail.com>
2014-01-08 00:27:59 +0000

    new comments added

--- system/bin/scu.get.sh
+++ system/bin/scu.get.sh
@@ -5,12 +5,19 @@
 # variables that are going to be used for the
 # retrieval and installation of the file
 BASE=$1
-NAME=${NAME-${BASE-program}}
+NAME=${NAME-$BASE}
 VERSION=${VERSION-1.0.0}
 ARCH=${ARCH-amd64}
 URL=${URL-http://repo.stage.hive.pt/scu}
 NAME_F="$NAME"_"$VERSION"_"$ARCH"
 FILE=$NAME_F.scu
+
+# verifies if a valid name for retrieval has been
+# provided if not returns immediately in error
+if [ -z "$NAME" ]; then
+    echo "No valid name has been provided"
+    exit -1
+fi
 
 # retrieves the target file from the repository
 # and then uses the proper script for installation
