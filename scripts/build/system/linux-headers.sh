150966fa09b65572cf5e81924cdebb60603bad14
joamag <joamag@gmail.com>
2014-08-25 19:04:44 +0100

    new code

--- scripts/build/system/linux-headers.sh
+++ scripts/build/system/linux-headers.sh
@@ -9,4 +9,4 @@ make mrproper
 make headers_check
 make INSTALL_HDR_PATH=dest headers_install
 find dest/include \( -name .install -o -name ..install.cmd \) -delete
-cp -rv dest/include/* /tools/include
+cp -rv dest/include/* /usr/include
