08969efbe341f60bf830730581d7d67249f7c448
joamag <joamag@gmail.com>
2014-08-25 16:39:51 +0100

    fixed

--- scripts/build/tools/wget.sh
+++ scripts/build/tools/wget.sh
@@ -1,8 +1,8 @@
 VERSION=${VERSION-1.14}
 
 wget -q "http://ftp.gnu.org/gnu/wget/wget-$VERSION.tar.gz"
-tar -Jxf "wget-$VERSION.tar.xz"
-rm -f "wget-$VERSION.tar.xz"
+tar -zxf "wget-$VERSION.tar.gz"
+rm -f "wget-$VERSION.tar.gz"
 cd wget-$VERSION
 
 ./configure --prefix=$PREFIX
