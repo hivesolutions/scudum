c4031272f2724078ae1ef4b18de7d1fa94707c1f
joamag <joamag@gmail.com>
2014-08-25 19:17:46 +0100

    new glibc

---
+++ scripts/build/system/glibc.sh
@@ -0,0 +1,24 @@
+VERSION=${VERSION-2.17}
+
+wget -q "http://ftp.gnu.org/gnu/glibc/glibc-$VERSION.tar.xz"
+rm -rf glibc-$VERSION && tar -Jxf "glibc-$VERSION.tar.xz"
+rm -f "glibc-$VERSION.tar.xz"
+cd glibc-$VERSION
+
+cd ..
+rm -rf glibc-build
+mkdir glibc-build
+cd glibc-build
+
+../glibc-$VERSION/configure\
+    --prefix=/usr\
+    --disable-profile\
+    --enable-kernel=2.6.25\
+    --libexecdir=/usr/lib/glibc
+
+make
+make -k check 2>&1 | tee glibc-check-log
+grep Error glibc-check-log
+
+touch /etc/ld.so.conf
+make install
