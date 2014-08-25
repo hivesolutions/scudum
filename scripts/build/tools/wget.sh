b7179a84397264b8eade625cc97b8ab20b8e0809
joamag <joamag@gmail.com>
2014-08-25 18:31:11 +0100

    new code structure

--- scripts/build/tools/wget.sh
+++ scripts/build/tools/wget.sh
@@ -5,5 +5,5 @@ tar -zxf "wget-$VERSION.tar.gz"
 rm -f "wget-$VERSION.tar.gz"
 cd wget-$VERSION
 
-./configure --prefix=$PREFIX --with-ssl=openssl
+./configure --prefix=$PREFIX --with-ssl=openssl --with-libssl-prefix=$PREFIX
 make && make install
