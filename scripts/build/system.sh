0bb81d8e5ad88fa42fb08b4fa5a3831c36f5dec5
joamag <joamag@gmail.com>
2014-08-25 16:10:38 +0100

    new comments

--- scripts/build/system.sh
+++ scripts/build/system.sh
@@ -5,6 +5,10 @@
 # of the commands fails the execution is broken
 set -e
 
+# changes the currently working directory to the build one
+# taking into account the deployed repository
+cd /repo/scripts/build
+
 # runs the building part of the system, this is considered
 # to be the main stage of the process
-../system/tree.sh
+system/tree.sh
