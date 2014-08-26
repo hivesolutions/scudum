21c9bacb4831a4e04d454082edb9891ec9d3d334
joamag <joamag@gmail.com>
2014-08-26 17:12:18 +0100

    new bash

---
+++ scripts/build/system/bash.sh
@@ -0,0 +1,23 @@
+VERSION=${VERSION-4.2}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/bash/bash-$VERSION.tar.gz"
+rm -rf bash-$VERSION && tar -zxf "bash-$VERSION.tar.gz"
+rm -f "bash-$VERSION.tar.gz"
+cd bash-$VERSION
+
+wget --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/bash-$VERSION-fixes-11.patch"
+patch -Np1 -i ../bash-$VERSION-fixes-11.patch
+
+./configure\
+    --prefix=/usr\
+    --bindir=/bin\
+    --htmldir=/usr/share/doc/bash-$VERSION\
+    --without-bash-malloc\
+    --with-installed-readline
+    
+make
+
+chown -Rv nobody .
+su nobody -s /bin/bash -c "PATH=$PATH make tests"
+make install
+exec /bin/bash --login +h
