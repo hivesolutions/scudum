c942dfd9389c75547a6aa0cd9def1091e7a8f296
joamag <joamag@gmail.com>
2014-08-26 19:08:36 +0100

    new testing

--- scripts/oldies/system/gmp.sh
+++ scripts/oldies/system/gmp.sh
@@ -3,8 +3,9 @@ tar -Jxf "gmp-$VERSION.tar.xz"
 cd gmp-$VERSION
 
 ./configure --prefix=/usr --enable-cxx
+
 make
-make check 2>&1 | tee gmp-check-log
+test $TEST && make check 2>&1 | tee gmp-check-log
 awk '/tests passed/{total+=$2} ; END{print total}' gmp-check-log
 make install
 
