376a262bea688b7cfb6003c046a5550dede2043b
joamag <joamag@gmail.com>
2014-08-26 09:49:50 +0100

    small code change

--- scripts/build/system/zlib.sh
+++ scripts/build/system/zlib.sh
@@ -1,7 +1,7 @@
 VERSION=${VERSION-1.2.7}
 
 wget -q --no-check-certificate "http://zlib.net/fossils/zlib-$VERSION.tar.gz"
-rm -rf zlib-$VERSION && tar -jxf "zlib-$VERSION.tar.bz2"
+rm -rf zlib-$VERSION && tar -zxf "zlib-$VERSION.tar.gz"
 cd zlib-$VERSION
 
 ./configure --prefix=/usr
