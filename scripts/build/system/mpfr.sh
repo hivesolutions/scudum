e100d82c11cd4a243cd175213f01b482f54f9bb7
joamag <joamag@gmail.com>
2014-08-26 15:40:32 +0100

    new mpfr support

---
+++ scripts/build/system/mpfr.sh
@@ -0,0 +1,14 @@
+VERSION=${VERSION-3.1.1}
+
+wget "http://www.mpfr.org/mpfr-$VERSION/mpfr-$VERSION.tar.xz"
+rm -rf mpfr-$VERSION && tar -Jxf "mpfr-$VERSION.tar.xz"
+rm -f "mpfr-$VERSION.tar.xz"
+cd mpfr-$VERSION
+
+./configure --prefix=/usr --enable-thread-safe\
+    --docdir=/usr/share/doc/mpfr-$VERSION
+make
+make check
+make install
+make html
+make install-html
