d0736d73dcbc394114868695b1b030f95f1d9dab
joamag <joamag@gmail.com>
2014-08-26 17:57:55 +0100

    new code

--- scripts/build/system.sh
+++ scripts/build/system.sh
@@ -47,3 +47,4 @@ cd sources
 /tools/repo/scripts/build/system/diffutils.sh
 /tools/repo/scripts/build/system/gawk.sh
 /tools/repo/scripts/build/system/findutils.sh
+/tools/repo/scripts/build/system/flex.sh
