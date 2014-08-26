4fa535c9494216df2b589726fad88fd77abe8da7
joamag <joamag@gmail.com>
2014-08-26 18:49:37 +0100

    new vim

---
+++ scripts/build/system/vim.sh
@@ -0,0 +1,32 @@
+VERSION=${VERSION-7.3}
+VERSION_L=${VERSION_L-73}
+
+wget --no-check-certificate "ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2"
+rm -rf vim$VERSION_L && tar -jxf "vim-$VERSION.tar.bz2"
+rm -f "vim-$VERSION.tar.bz2"
+cd vim$VERSION_L
+
+echo '#define SYS_VIMRC_FILE "/etc/vimrc"' >> src/feature.h
+
+./configure --prefix=/usr --enable-multibyte
+
+make
+test $TEST && make test
+make install
+
+ln -sv vim /usr/bin/vi
+for L in  /usr/share/man/{,*/}man1/vim.1; do
+    ln -sv vim.1 $(dirname $L)/vi.1
+done
+
+ln -sv ../vim/vim$VERSION_L/doc /usr/share/doc/vim-$VERSION
+
+cat > /etc/vimrc << "EOF"
+set nocompatible
+set backspace=2
+syntax on
+if (&term == "iterm") || (&term == "putty")
+  set background=dark
+endif
+
+EOF
