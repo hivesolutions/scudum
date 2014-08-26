117389779518d5134fdaae258d0e1b6cef2e2d90
joamag <joamag@gmail.com>
2014-08-26 18:09:50 +0100

    new grub

---
+++ scripts/build/system/grub.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.0.0}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/grub/grub-$VERSION.tar.xz"
+rm -rf grub-$VERSION && tar -Jxf "grub-$VERSION.tar.xz"
+rm "grub-$VERSION.tar.xz"
+cd grub-$VERSION
+
+sed -i -e '/gets is a/d' grub-core/gnulib/stdio.in.h
+
+./configure\
+    --prefix=/usr\
+    --sysconfdir=/etc\
+    --disable-grub-emu-usb\
+    --disable-efiemu\
+    --disable-werror
+
+make && make install
