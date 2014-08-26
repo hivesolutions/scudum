aea9c4edc58dc50ab7fff9b9c41359170ab1c890
joamag <joamag@gmail.com>
2014-08-26 18:23:04 +0100

    new libpipeline

---
+++ scripts/build/system/libpipeline.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-1.2.2}
+
+wget --no-check-certificate "http://download.savannah.gnu.org/releases/libpipeline/libpipeline-$VERSION.tar.gz"
+rm -rf libpipeline-$VERSION && tar -zxf "libpipeline-$VERSION.tar.gz"
+rm -f "libpipeline-$VERSION.tar.gz"
+cd libpipeline-$VERSION
+
+PKG_CONFIG_PATH=/tools/lib/pkgconfig ./configure --prefix=/usr
+
+make
+test $TEST && make check
+make install
