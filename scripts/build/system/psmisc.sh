1ea73e345f49d8b1aa4c3ef92280239102a60428
joamag <joamag@gmail.com>
2014-08-26 16:24:08 +0100

    new code

---
+++ scripts/build/system/psmisc.sh
@@ -0,0 +1,13 @@
+VERSION=${VERSION-22.20}
+
+wget --no-check-certificate "http://prdownloads.sourceforge.net/psmisc/psmisc-$VERSION.tar.gz"
+rm -rf psmisc-$VERSION && tar -zxf "psmisc-$VERSION.tar.gz"
+rm -f "psmisc-$VERSION.tar.gz"
+cd psmisc-$VERSION
+
+./configure --prefix=/usr
+
+make && make install
+
+mv -v /usr/bin/fuser /bin
+mv -v /usr/bin/killall /bin
