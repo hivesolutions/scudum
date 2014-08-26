5ee95d95dd4467132203ad5cf1225995988bde21
joamag <joamag@gmail.com>
2014-08-26 18:28:33 +0100

    new patch file

---
+++ scripts/build/system/patch.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-2.7.1}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/patch/patch-$VERSION.tar.xz"
+rm -rf atch-$VERSION && tar -Jxf "patch-$VERSION.tar.xz"
+rm -f  "patch-$VERSION.tar.xz"
+cd patch-$VERSION
+
+./configure --prefix=/usr
+
+make
+test $TEST && make check
+make install
