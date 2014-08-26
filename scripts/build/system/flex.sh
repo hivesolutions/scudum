a65a4d49ce1a6f6284ff6530d67082c0b03373e2
joamag <joamag@gmail.com>
2014-08-26 17:57:34 +0100

    new flex

---
+++ scripts/build/system/flex.sh
@@ -0,0 +1,26 @@
+VERSION=${VERSION-2.5.37}
+
+wget --no-check-certificate "http://prdownloads.sourceforge.net/flex/flex-$VERSION.tar.bz2"
+rm -rf flex-$VERSION && tar -jxf "flex-$VERSION.tar.bz2"
+rm -f "flex-$VERSION.tar.bz2"
+cd flex-$VERSION
+
+wget  --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/flex-$VERSION-bison-2.6.1-1.patch"
+patch -Np1 -i flex-$VERSION-bison-2.6.1-1.patch
+
+./configure\
+    --prefix=/usr\
+    --docdir=/usr/share/doc/flex-$VERSION
+
+make
+test $TEST && make check
+make install
+
+ln -sv libfl.a /usr/lib/libl.a
+cat > /usr/bin/lex << "EOF"
+#!/bin/sh
+
+exec /usr/bin/flex -l "$@"
+
+EOF
+chmod -v 755 /usr/bin/lex
