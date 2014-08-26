f1878efce2959322dc63a5706f32b87aa6b46fef
joamag <joamag@gmail.com>
2014-08-26 17:13:51 +0100

    new libtool

---
+++ scripts/build/system/libtool.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-2.4.2}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/libtool/libtool-$VERSION.tar.gz"
+rm -rf libtool-$VERSION && tar -zxf "libtool-$VERSION.tar.gz"
+rm -f "libtool-$VERSION.tar.gz"
+cd libtool-$VERSION
+
+./configure --prefix=/usr
+
+make
+make check
+make install
