924f4c67f667b1c0f3a377007e00b1b0a412198c
joamag <joamag@gmail.com>
2014-08-26 18:26:40 +0100

    new man db

---
+++ scripts/build/system/man-db.sh
@@ -0,0 +1,20 @@
+VERSION=${VERSION-2.6.3}
+
+wget --no-check-certificate "http://download.savannah.gnu.org/releases/man-db/man-db-$VERSION.tar.xz"
+rm -rf man-db-$VERSION && tar -Jxf "man-db-$VERSION.tar.xz"
+rm -f "man-db-$VERSION.tar.xz"
+cd man-db-$VERSION
+
+./configure\
+    --prefix=/usr\
+    --libexecdir=/usr/lib\
+    --docdir=/usr/share/doc/man-db-$VERSION\
+    --sysconfdir=/etc\
+    --disable-setuid\
+    --with-browser=/usr/bin/lynx\
+    --with-vgrind=/usr/bin/vgrind\
+    --with-grap=/usr/bin/grap
+
+make
+test $TEST && make check
+make install
