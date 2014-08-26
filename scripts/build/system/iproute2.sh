9e1042a59f800ac0c7ee86438a47720b59bbc5cc
joamag <joamag@gmail.com>
2014-08-26 18:17:24 +0100

    new iproute

---
+++ scripts/build/system/iproute2.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-3.8.0}
+
+wget --no-check-certificate "http://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-$VERSION.tar.xz"
+rm -rf iproute2-$VERSION && tar -Jxf "iproute2-$VERSION.tar.xz"
+rm -f "iproute2-$VERSION.tar.xz"
+cd iproute2-$VERSION
+
+sed -i '/^TARGETS/s@arpd@@g' misc/Makefile
+sed -i /ARPD/d Makefile
+sed -i 's/arpd.8//' man/man8/Makefile
+
+sed -i 's/-Werror//' Makefile
+
+make DESTDIR=
+make DESTDIR=\
+    MANDIR=/usr/share/man\
+    DOCDIR=/usr/share/doc/iproute2-$VERSION install
