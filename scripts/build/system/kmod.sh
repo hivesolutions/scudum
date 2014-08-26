2d3c4345c7b6e5540317f38667ef838793089485
joamag <joamag@gmail.com>
2014-08-26 18:21:19 +0100

    new kmod

---
+++ scripts/build/system/kmod.sh
@@ -0,0 +1,25 @@
+VERSION=${VERSION-12}
+
+wget --no-check-certificate "http://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-$VERSION.tar.xz"
+rm -rf kmod-$VERSION && tar -Jxf "kmod-$VERSION.tar.xz"
+rm -f "kmod-$VERSION.tar.xz"
+cd kmod-$VERSION
+
+./configure\
+    --prefix=/usr\
+    --bindir=/bin\
+    --libdir=/lib\
+    --sysconfdir=/etc\
+    --disable-manpages\
+    --with-xz\
+    --with-zlib
+
+make
+test $TEST && make check
+make pkgconfigdir=/usr/lib/pkgconfig install
+
+for target in depmod insmod modinfo modprobe rmmod; do
+    ln -sv ../bin/kmod /sbin/$target
+done
+
+ln -sv kmod /bin/lsmod
