36b8417d90a1f23176114b041b208c9d5fc8961c
joamag <joamag@gmail.com>
2014-08-26 18:30:28 +0100

    new syslogd

---
+++ scripts/build/system/sysklogd.sh
@@ -0,0 +1,20 @@
+VERSION=${VERSION-1.5}
+
+wget --no-check-certificate "http://www.infodrom.org/projects/sysklogd/download/sysklogd-$VERSION.tar.gz"
+rm -rf sysklogd-$VERSION && tar -zxf "sysklogd-$VERSION.tar.gz"
+rm -f "sysklogd-$VERSION.tar.gz"
+cd sysklogd-$VERSION
+
+make
+make BINDIR=/sbin install
+
+cat > /etc/syslog.conf << "EOF"
+auth,authpriv.* -/var/log/auth.log
+*.*;auth,authpriv.none -/var/log/sys.log
+daemon.* -/var/log/daemon.log
+kern.* -/var/log/kern.log
+mail.* -/var/log/mail.log
+user.* -/var/log/user.log
+*.emerg *
+
+EOF
