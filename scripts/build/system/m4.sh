d68157c8b70dfc28761064d2461519e4d24fdf16
joamag <joamag@gmail.com>
2014-08-26 16:56:42 +0100

    new m4

---
+++ scripts/build/system/m4.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-1.4.16}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/m4/m4-$VERSION.tar.bz2"
+rm -rf cd m4-$VERSION && tar -jxf "m4-$VERSION.tar.bz2"
+rm -f "m4-$VERSION.tar.bz2"
+cd m4-$VERSION
+
+sed -i -e '/gets is a/d' lib/stdio.in.h
+./configure --prefix=/usr
+
+make
+
+sed -i -e '41s/ENOENT/& || errno == EINVAL/' tests/test-readlink.h
+make check
+
+make install
