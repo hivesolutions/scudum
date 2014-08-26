6f1a2b07039f304008103d34e923edad599edb0a
joamag <joamag@gmail.com>
2014-08-26 17:59:27 +0100

    new gettext

---
+++ scripts/build/system/gettext.sh
@@ -0,0 +1,14 @@
+VERSION=${VERSION-0.18.2}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/gettext/gettext-$VERSION.tar.gz"
+rm -rf gettext-$VERSION && tar -zxf "gettext-$VERSION.tar.gz"
+rm -f "gettext-$VERSION.tar.gz"
+cd gettext-$VERSION
+
+./configure\
+    --prefix=/usr\
+    --docdir=/usr/share/doc/gettext-$VERSION
+
+make
+test $TEST && make check
+make install
