1a035b2b1c0a892347787fbab6ca33bf61257b09
joamag <joamag@gmail.com>
2014-08-26 16:30:02 +0100

    new shadow

---
+++ scripts/build/system/shadow.sh
@@ -0,0 +1,21 @@
+VERSION=${VERSION-4.1.5.1}
+
+wget --no-check-certificate "http://pkg-shadow.alioth.debian.org/releases/shadow-$VERSION.tar.bz2"
+rm -rf shadow-$VERSION && tar -jxf "shadow-$VERSION.tar.bz2"
+rm -f "shadow-$VERSION.tar.bz2"
+cd shadow-$VERSION
+
+sed -i 's/groups$(EXEEXT) //' src/Makefile.in
+find man -name Makefile.in -exec sed -i 's/groups\.1 / /' {} \;
+
+sed -i -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD SHA512@'\
+    -e 's@/var/spool/mail@/var/mail@' etc/login.defs
+
+./configure --sysconfdir=/etc
+
+make && make install
+
+mv -v /usr/bin/passwd /bin
+
+pwconv
+grpconv
