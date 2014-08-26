4c2360138fe019ba075388f64001d9dc74afbcca
joamag <joamag@gmail.com>
2014-08-26 19:26:02 +0100

    removed old scripts (not required anymore)

--- scripts/oldies/system/bootscripts.sh
+++
@@ -1,8 +0,0 @@
-VERSION="20130123"
-tar -jxf "lfs-bootscripts-$VERSION.tar.bz2"
-cd lfs-bootscripts-$VERSION
-
-make install
-
-cd ..
-rm -rf bootscripts-$VERSION
--- scripts/oldies/system/zlib.sh
+++
@@ -1,9 +0,0 @@
-VERSION="1.2.7"
-tar -jxf "zlib-$VERSION.tar.bz2"
-cd zlib-$VERSION
-
-./configure --prefix=/usr
-make && make install
-
-cd ..
-rm -rf zlib-$VERSION
