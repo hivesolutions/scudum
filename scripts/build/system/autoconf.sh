a6ac688f77f2da2602033b37b164c41fdbf66b84
joamag <joamag@gmail.com>
2014-08-26 17:44:47 +0100

    new autoconf

--- scripts/build/system/autoconf.sh
+++ scripts/build/system/autoconf.sh
@@ -1,5 +1,6 @@
 VERSION=${VERSION-2.69}
 
+wget --no-check-certificate "http://ftp.gnu.org/gnu/autoconf/autoconf-$VERSION.tar.xz"
 rm -rf autoconf-$VERSION && tar -Jxf "autoconf-$VERSION.tar.xz"
 rm -f "autoconf-$VERSION.tar.xz"
 cd autoconf-$VERSION
