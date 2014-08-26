f3d9731456d25fc16f3e12d277a47e80ab2ea50c
joamag <joamag@gmail.com>
2014-08-26 17:46:40 +0100

    new automake

---
+++ scripts/build/system/automake.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-1.13.1}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/automake/automake-$VERSION.tar.xz"
+rm -rf automake-$VERSION && tar -Jxf "automake-$VERSION.tar.xz"
+rm -f "automake-$VERSION.tar.xz"
+cd automake-$VERSION
+
+./configure --prefix=/usr --docdir=/usr/share/doc/automake-$VERSION
+
+make
+test $TEST && make check
+make install
