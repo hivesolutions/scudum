8cc6f35c35143dea065fb06c065c026bdb2f4697
joamag <joamag@gmail.com>
2014-08-26 22:30:16 +0100

    new test value

--- scripts/build/base.sh
+++ scripts/build/base.sh
@@ -12,3 +12,7 @@ export PREFIX=/tools
 # exports the unsafe configuration flag so that
 # a root user may configure all the packages
 export FORCE_UNSAFE_CONFIGURE=1
+
+# the test value that defines if the current build
+# should be done with unit tests runnig (more time)
+export TEST=
