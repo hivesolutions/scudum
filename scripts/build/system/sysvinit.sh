e88666dab9dcf844daaa65f9148865271efb0b90
joamag <joamag@gmail.com>
2014-08-26 18:35:15 +0100

    new sysv init

---
+++ scripts/build/system/sysvinit.sh
@@ -0,0 +1,14 @@
+VERSION=${VERSION-2.88dsf}
+
+wget --no-check-certificate "http://download.savannah.gnu.org/releases/sysvinit/sysvinit-$VERSION.tar.bz2"
+rm -rf sysvinit-$VERSION && tar -jxf "sysvinit-$VERSION.tar.bz2"
+rm -f "sysvinit-$VERSION.tar.bz2"
+cd sysvinit-$VERSION
+
+sed -i 's@Sending processes@& configured via /etc/inittab@g' src/init.c
+
+sed -i -e '/utmpdump/d'\
+    -e '/mountpoint/d' src/Makefile
+
+make -C src
+make -C src install
