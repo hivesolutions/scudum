4fe38114f3c03dbb899edae26d0a4682d7311c0c
joamag <joamag@gmail.com>
2014-08-26 16:14:32 +0100

    new package config

---
+++ scripts/build/system/pkg-config.sh
@@ -0,0 +1,19 @@
+VERSION=${VERSION-0.28}
+
+wget "http://pkgconfig.freedesktop.org/releases/pkg-config-$VERSION.tar.gz"
+rm -rf pkg-config-$VERSION && tar -zxf "pkg-config-$VERSION.tar.gz"
+rm -f "pkg-config-$VERSION.tar.gz"
+cd pkg-config-$VERSION
+
+./configure\
+    --prefix=/usr\
+    --with-internal-glib\
+    --disable-host-tool\
+    --docdir=/usr/share/doc/pkg-config-$VERSION
+
+make
+make check
+make install
+
+cd ..
+rm -rf pkg-config-$VERSION
