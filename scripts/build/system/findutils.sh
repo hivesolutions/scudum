6cb105982029c810c994426e5b7ec9aaab6c3596
joamag <joamag@gmail.com>
2014-08-26 17:53:36 +0100

    new findutils

---
+++ scripts/build/system/findutils.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-4.4.2}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/findutils/findutils-$VERSION.tar.gz"
+rm -rf findutils-$VERSION && tar -zxf "findutils-$VERSION.tar.gz"
+rm -f "findutils-$VERSION.tar.gz"
+cd findutils-$VERSION
+
+./configure\
+    --prefix=/usr\
+    --libexecdir=/usr/lib/findutils\
+    --localstatedir=/var/lib/locate
+    
+make
+test $TEST && make check
+make install
+
+mv -v /usr/bin/find /bin
+sed -i 's/find:=${BINDIR}/find:=\/bin/' /usr/bin/updatedb
