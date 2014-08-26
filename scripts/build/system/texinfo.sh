65d9aae8eb8bd14647b5f6c264cf9f9fd005f781
joamag <joamag@gmail.com>
2014-08-26 18:38:36 +0100

    new texinfo

---
+++ scripts/build/system/texinfo.sh
@@ -0,0 +1,13 @@
+VERSION=${VERSION-5.0}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/texinfo/texinfo-$VERSION.tar.xz"
+rm -rf texinfo-$VERSION && tar -Jxf "texinfo-$VERSION.tar.xz"
+rm -f "texinfo-$VERSION.tar.xz"
+cd texinfo-$VERSION
+
+./configure --prefix=/usr
+
+make
+test $TEST && make check
+make install
+make TEXMF=/usr/share/texmf install-tex
