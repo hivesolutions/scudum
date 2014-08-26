a88df4a0bf334a3f4b865d835a468ac2025598d5
joamag <joamag@gmail.com>
2014-08-26 17:00:40 +0100

    new bison

---
+++ scripts/build/system/bison.sh
@@ -0,0 +1,14 @@
+VERSION="2.7"
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/bison/bison-$VERSION.tar.xz"
+rm -rf bison-$VERSION && tar -Jxf "bison-$VERSION.tar.xz"
+rm -f "bison-$VERSION.tar.xz"
+cd bison-$VERSION
+
+./configure --prefix=/usr
+
+echo '#define YYENABLE_NLS 1' >> lib/config.h
+
+make
+make check
+make install
