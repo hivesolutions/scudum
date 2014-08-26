35234641b99a1c72998a65d118e6321023474f5b
joamag <joamag@gmail.com>
2014-08-26 15:35:06 +0100

    new gmp file

---
+++ scripts/build/system/gmp.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-5.1.1}
+
+wget "ftp://ftp.gmplib.org/pub/gmp-$VERSION/gmp-$VERSION.tar.xz"
+rm -rf gmp-$VERSION && tar -Jxf "gmp-$VERSION.tar.xz"
+rm -f "gmp-$VERSION.tar.xz"
+cd gmp-$VERSION
+
+./configure --prefix=/usr --enable-cxx
+make
+make check 2>&1 | tee gmp-check-log
+awk '/tests passed/{total+=$2} ; END{print total}' gmp-check-log
+make install
