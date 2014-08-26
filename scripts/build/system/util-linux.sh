cfda4e52cbfc486f36bced275ed43fd0e7735efc
joamag <joamag@gmail.com>
2014-08-26 16:20:49 +0100

    new util linux

---
+++ scripts/build/system/util-linux.sh
@@ -0,0 +1,14 @@
+VERSION=${VERSION-2.22.2}
+
+wget "http://www.kernel.org/pub/linux/utils/util-linux/v2.22/util-linux-$VERSION.tar.xz"
+rm -rf util-linux-$VERSION && tar -Jxf "util-linux-$VERSION.tar.xz"
+rm -f "util-linux-$VERSION.tar.xz"
+cd util-linux-$VERSION
+
+sed -i -e 's@etc/adjtime@var/lib/hwclock/adjtime@g'\
+    $(grep -rl '/etc/adjtime' .)
+mkdir -pv /var/lib/hwclock
+
+./configure --disable-su --disable-sulogin --disable-login
+
+make && make install
