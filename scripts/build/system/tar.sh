ba6b2b3b4363bdf396b3058cd8e7b03ac2fe96b9
joamag <joamag@gmail.com>
2014-08-26 18:37:01 +0100

    new tar

---
+++ scripts/build/system/tar.sh
@@ -0,0 +1,19 @@
+VERSION=${VERSION-1.26}
+
+wget --no-check-certificate "http://ftp.gnu.org/gnu/tar/tar-$VERSION.tar.bz2"
+rm -rf tar-$VERSION && tar -jxf "tar-$VERSION.tar.bz2"
+rm -f "tar-$VERSION.tar.bz2"
+cd tar-$VERSION
+
+sed -i -e '/gets is a/d' gnu/stdio.in.h
+
+FORCE_UNSAFE_CONFIGURE=1\
+    ./configure\
+    --prefix=/usr\
+    --bindir=/bin\
+    --libexecdir=/usr/sbin
+
+make
+test $TEST && make check
+make install
+make -C doc install-html docdir=/usr/share/doc/tar-$VERSION
