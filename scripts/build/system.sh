88ae0895795bcc62089c028a570e94a7ab2c6246
joamag <joamag@gmail.com>
2014-08-26 17:21:27 +0100

    new inetutils

--- scripts/build/system.sh
+++ scripts/build/system.sh
@@ -40,3 +40,4 @@ cd sources
 /tools/repo/scripts/build/system/bash.sh
 /tools/repo/scripts/build/system/libtool.sh
 /tools/repo/scripts/build/system/gdbm.sh
+/tools/repo/scripts/build/system/inetutils.sh
