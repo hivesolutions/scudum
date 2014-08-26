e25295cdda787d03195186be8f60313a939e6e87
joamag <joamag@gmail.com>
2014-08-26 19:37:59 +0100

    small code fix

--- scripts/build/system/nano.sh
+++ scripts/build/system/nano.sh
@@ -3,9 +3,9 @@ VERSION=${VERSION-2.2.6}
 set -e
 
 wget --no-check-certificate "http://www.nano-editor.org/dist/v2.2/nano-$VERSION.tar.gz"
-rm -rf nano-$VERSION.tar && tar -zxf "nano-$VERSION.tar.gz"
+rm -rf nano-$VERSION && tar -zxf "nano-$VERSION.tar.gz"
 rm -f "nano-$VERSION.tar.gz"
-cd nano-$VERSION.tar
+cd nano-$VERSION
 
 ./configure --prefix=/usr
 
