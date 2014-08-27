dbb3ab1eccdfc463c970c5f61ec07833cdce2f9d
joamag <joamag@gmail.com>
2014-08-27 12:07:23 +0100

    fixed naming

--- scripts/build/system/git.sh
+++ scripts/build/system/git.sh
@@ -3,8 +3,8 @@ VERSION=${VERSION-2.1.0}
 set -e
 
 wget --no-check-certificate "https://www.kernel.org/pub/software/scm/git/git-$VERSION.tar.xz"
-rm -rf "git-$VERSION" && tar -Jxf "git-$VERSION.tar.gz"
-rm -f "git-$VERSION.tar.gz"
+rm -rf "git-$VERSION" && tar -Jxf "git-$VERSION.tar.xz"
+rm -f "git-$VERSION.tar.xz"
 cd git-$VERSION
 
 ./configure --prefix=/usr
