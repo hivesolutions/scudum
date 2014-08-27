0073e7bf0f1894a5a4afc6c28e2b8c0bd532d60e
joamag <joamag@gmail.com>
2014-08-27 12:22:12 +0100

    new sqlite3

---
+++ scripts/build/extra/sqlite3.sh
@@ -0,0 +1,11 @@
+VERSION=${VERSION-3080600}
+
+set -e
+
+wget --no-check-certificate "http://www.sqlite.org/2014/sqlite-autoconf-$VERSION.tar.gz"
+rm -rf sqlite-autoconf-$VERSION && tar -zxf "sqlite-autoconf-$VERSION.tar.gz"
+rm -f "sqlite-autoconf-$VERSION.tar.gz"
+cd sqlite-autoconf-$VERSION
+
+./configure --prefix=/usr
+make && make install
