795198505f36c396439706ab3d833a020b479207
joamag <joamag@gmail.com>
2014-08-28 00:42:57 +0100

    new testing

--- scripts/build/system/coreutils.sh
+++ scripts/build/system/coreutils.sh
@@ -20,7 +20,7 @@ make NON_ROOT_USERNAME=nobody check-root
 echo "dummy:x:1000:nobody" >> /etc/group
 chown -Rv nobody .
 
-su nobody -s /bin/bash\
+test $TEST && su nobody -s /bin/bash\
     -c "PATH=$PATH make RUN_EXPENSIVE_TESTS=yes check"
 
 sed -i '/dummy/d' /etc/group
