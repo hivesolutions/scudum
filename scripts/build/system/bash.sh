b1fab69559cb01b885825cdb856b343eb08dcaea
joamag <joamag@gmail.com>
2014-08-28 00:48:21 +0100

    small fix

--- scripts/build/system/bash.sh
+++ scripts/build/system/bash.sh
@@ -8,7 +8,7 @@ rm -f "bash-$VERSION.tar.gz"
 cd bash-$VERSION
 
 wget --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/bash-$VERSION-fixes-11.patch"
-patch -Np1 -i ../bash-$VERSION-fixes-11.patch
+patch -Np1 -i bash-$VERSION-fixes-11.patch
 
 ./configure\
     --prefix=/usr\
