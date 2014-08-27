ac4e9eae06159a4f911a307a26e9efb79046ef0b
joamag <joamag@gmail.com>
2014-08-27 12:06:48 +0100

    small fix

--- scripts/build/system/git.sh
+++ scripts/build/system/git.sh
@@ -2,7 +2,7 @@ VERSION=${VERSION-2.1.0}
 
 set -e
 
-wget --no-check-certificate "https://www.kernel.org/pub/software/scm/git/git-$version.tar.xz"
+wget --no-check-certificate "https://www.kernel.org/pub/software/scm/git/git-$VERSION.tar.xz"
 rm -rf "git-$VERSION" && tar -Jxf "git-$VERSION.tar.gz"
 rm -f "git-$VERSION.tar.gz"
 cd git-$VERSION
