409ecfdd03d73a02fc70897201780b8950fbadb1
joamag <joamag@gmail.com>
2014-08-27 16:22:09 +0100

    small code fix

--- scripts/build/system/curl.sh
+++ scripts/build/system/curl.sh
@@ -3,8 +3,8 @@ VERSION=${VERSION-7.37.1}
 set -e
 
 wget --no-check-certificate "http://curl.haxx.se/download/curl-$VERSION.tar.gz"
-rm -rf "curl-$VERSION" && tar -zxf "curl-$VERSION.tar.xz"
-rm -f "curl-$VERSION.tar.xz"
+rm -rf "curl-$VERSION" && tar -zxf "curl-$VERSION.tar.gz"
+rm -f "curl-$VERSION.tar.gz"
 cd curl-$VERSION
 
 ./configure --prefix=/usr
