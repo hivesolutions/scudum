51f72f7d3b7d79e9fe799c955a173fcdcd3476bf
joamag <joamag@gmail.com>
2014-08-27 13:48:46 +0100

    new pack

--- scripts/build/pack.sh
+++ scripts/build/pack.sh
@@ -5,7 +5,7 @@ NAME=${NAME-scudum}
 VERSION=${VERSION-v1}
 DATE=${DATE-$(date +%y%m%d%H%M)}
 
-FULL_NAME="%NAME_%VERSION_%DATE.tar.gz"
+FULL_NAME="$NAME_$VERSION_$DATE.tar.gz"
 
 set -e
 
@@ -13,5 +13,10 @@ source base/config.sh
 
 rm -rf $SCUDUM/tools
 rm -rf $SCUDUM/sources
+
+mountpoint -q $SCUDUM/dev && umount $SCUDUM/dev
+mountpoint -q $SCUDUM/dev/pts && umount $SCUDUM/dev/pts
+mountpoint -q $SCUDUM/proc && umount $SCUDUM/proc
+mountpoint -q $SCUDUM/sys && umount $SCUDUM/sys
 
 tar -zcvf $FULL_NAME $SCUDUM/*
