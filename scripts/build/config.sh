96955fbea474d1c7ac92768ae2b6b55d6413749b
joamag <joamag@gmail.com>
2014-08-27 16:25:40 +0100

    small code fix

--- scripts/build/config.sh
+++ scripts/build/config.sh
@@ -4,6 +4,8 @@
 BASE=$(pwd)
 DIR=$(dirname $(readlink -f $0))
 
+set -e
+
 source $DIR/base/config.sh
 
 touch $SCUDUM/CONFIGURED
@@ -11,5 +13,3 @@ touch $SCUDUM/CONFIGURED
 git clone https://github.com/hivesolutions/scudum.git $BASE/scudum.git
 cp -rpv $BASE/scudum.git/system/* $SCUDUM
 rm -rf $BASE/scudum.git
-
-#tenho de por aki o second stage da configuration (kernel + initramfs)
