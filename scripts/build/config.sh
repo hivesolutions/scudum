2c320d5360eb2cc27cce4d6d9bf1daa76d1344a3
joamag <joamag@gmail.com>
2014-08-27 16:21:50 +0100

    new curl reference

--- scripts/build/config.sh
+++ scripts/build/config.sh
@@ -11,3 +11,5 @@ touch $SCUDUM/CONFIGURED
 git clone https://github.com/hivesolutions/scudum.git $BASE/scudum.git
 cp -rpv $BASE/scudum.git/system/* $SCUDUM
 rm -rf $BASE/scudum.git
+
+#tenho de por aki o second stage da configuration (kernel + initramfs)
