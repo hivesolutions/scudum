ee5d2e4999f5bca0f3bc04583680b33f2a4d7827
joamag <joamag@gmail.com>
2014-08-27 12:31:18 +0100

    new llvm

---
+++ scripts/build/extra/llvm.sh
@@ -0,0 +1,33 @@
+VERSION=${VERSION-3.4.2}
+VERSION_M=${VERSION_M-3.4}
+
+set -e
+
+wget --no-check-certificate "http://llvm.org/releases/$VERSION/llvm-$VERSION.src.tar.gz"
+rm -rf llvm-$VERSION && tar -zxf "llvm-$VERSION.tar.gz"
+rm -f "llvm-$VERSION.tar.gz"
+cd llvm-$VERSION
+
+wget --no-check-certificate "http://llvm.org/releases/$VERSION/cfe-$VERSION.src.tar.gz"
+tar -zxf cfe-$VERSION.src.tar.gz -C tools
+mv tools/cfe-$VERSION.src tools/clang
+
+wget --no-check-certificate "http://llvm.org/releases/$VERSION_M/compiler-rt-$VERSION_M.src.tar.gz"
+tar -zxf compiler-rt-$VERSION_M.src.tar.gz -C projects
+mv projects/compiler-rt-$VERSION_M projects/compiler-rt
+
+sed -e 's:/docs/llvm:/share/doc/llvm-3.4.2:'\
+    -i Makefile.config.in
+
+CC=gcc CXX=g++ ./configure --prefix=/usr\
+    --sysconfdir=/etc\
+    --enable-libffi\
+    --enable-optimized\
+    --enable-shared\
+    --disable-assertions
+
+make && make install
+
+for file in /usr/lib/lib{clang,LLVM,LTO}*.a; do
+    test -f $file && chmod -v 644 $file
+done
