f0f314ccb0bf6e1fb7015b5aea07a8de8d0d550a
joamag <joamag@gmail.com>
2014-08-27 17:52:18 +0100

    initial config

--- scripts/build/config.sh
+++ scripts/build/config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+DEV_NAME=${DEV_NAME-/dev/null}
+DEV_BOOT=${DEV_BOOT-/dev/null}
+DEV_SWAP=${DEV_SWAP-/dev/null}
+DEV_ROOT=${DEV_ROOT-/dev/null}
+SCHEMA=${SCHEMA-transient}
+
 BASE=$(pwd)
 DIR=$(dirname $(readlink -f $0))
 
@@ -13,6 +19,22 @@ touch $SCUDUM/CONFIGURED
 git clone --depth 1 https://github.com/hivesolutions/scudum.git $BASE/scudum.git
 cp -rpv $BASE/scudum.git/system/* $SCUDUM
 rm -rf $BASE/scudum.git
+
+cp -p $SCUDUM/etc/fstab.orig $SCUDUM/etc/fstab
+
+case $SCHEMA in
+    stored)
+        echo "UUID=$ROOT_UUID / $ROOT_FS defaults,noatime 0 1" >> $SCUDUM/etc/fstab
+        echo "UUID=$SWAP_UUID none swap pri=1 0 0" >> $SCUDUM/etc/fstab
+        if [ $DEV_ROOT != $DEV_BOOT ]; then
+            echo "UUID=$BOOT_UUID /boot $BOOT_FS noauto,noatime 1 2" >> $SCUDUM/etc/fstab
+        fi
+        ;;
+
+    transient)
+        echo "tmpfs / tmpfs defaults 0 0" >> $SCUDUM/etc/fstab
+        ;;
+esac
 
 $DIR/../util/chroot.sh /bin/kernel.build
 $DIR/../util/chroot.sh /sbin/mkinitramfs
