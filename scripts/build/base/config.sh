a6163a0cfb1e5ae9279d47ac33149e30d8104287
joamag <joamag@gmail.com>
2014-08-27 16:56:12 +0100

    new simpler config

--- scripts/build/base/config.sh
+++ scripts/build/base/config.sh
@@ -3,12 +3,6 @@
 export SCUDUM=${SCUDUM-/scudum}
 export SCUDUM_TARGET=$(uname -m)-scudum-linux-gnu
 
-# exposes the various build specific values so that
-# the initial value for the building is "tools based"
-export LC_ALL=POSIX
-export PATH=/tools/bin:/bin:/usr/bin
-export PREFIX=/tools
-
 # exports the unsafe configuration flag so that
 # a root user may configure all the packages
 export FORCE_UNSAFE_CONFIGURE=1
