7569855d1475f27b3ad70629f39857d3a2f2b4d5
joamag <joamag@gmail.com>
2014-08-28 00:46:36 +0100

    new sync support

--- scripts/build/system/coreutils.sh
+++ scripts/build/system/coreutils.sh
@@ -27,11 +27,12 @@ sed -i '/dummy/d' /etc/group
 
 make install
 
-mv -v /usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} /bin
+mv -v /usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} /bin && sync
 mv -v /usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} /bin && sync
-mv -v /usr/bin/{rmdir,stty,sync,true,uname,test,[} /bin
-mv -v /usr/bin/chroot /usr/sbin
-mv -v /usr/share/man/man1/chroot.1 /usr/share/man/man8/chroot.8
-sed -i s/\"1\"/\"8\"/1 /usr/share/man/man8/chroot.8
+mv -v /usr/bin/{rmdir,stty,sync,true,uname,test,[} /bin && sync
+mv -v /usr/bin/{head,sleep,nice} /bin && sync
+mv -v /usr/bin/chroot /usr/sbin && sync
 
-mv -v /usr/bin/{head,sleep,nice} /bin
+mv -v /usr/share/man/man1/chroot.1 /usr/share/man/man8/chroot.8 && sync
+
+sed -i s/\"1\"/\"8\"/1 /usr/share/man/man8/chroot.8
