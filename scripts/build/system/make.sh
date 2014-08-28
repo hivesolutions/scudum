aa07275decf960bd20ad9c01be4d36e483e2aaba
joamag <joamag@gmail.com>
2014-08-28 09:00:24 +0100

    new make version

--- scripts/build/system/make.sh
+++ scripts/build/system/make.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-3.82}
+VERSION=${VERSION-4.0}
 
 set -e
 
@@ -6,9 +6,6 @@ wget --no-check-certificate "http://ftp.gnu.org/gnu/make/make-$VERSION.tar.bz2"
 rm -rf make-$VERSION && tar -jxf "make-$VERSION.tar.bz2"
 rm -f "make-$VERSION.tar.bz2"
 cd make-$VERSION
-
-wget --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/make-$VERSION-upstream_fixes-3.patch"
-patch -Np1 -i make-$VERSION-upstream_fixes-3.patch
 
 ./configure --prefix=/usr
 
