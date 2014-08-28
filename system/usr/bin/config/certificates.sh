c4ca7eb462515f958bf2fc111da4c565e74f5625
joamag <joamag@gmail.com>
2014-08-28 16:30:16 +0100

    new location

--- system/usr/bin/config/certificates.sh
+++ system/usr/bin/config/certificates.sh
@@ -3,7 +3,7 @@ SSLDIR=/etc/ssl
 set -e
 
 rm -f certdata.txt &&\
-wget --no-check-certificate https://hg.mozilla.org/releases/mozilla-release/raw-file/default/security/nss/lib/ckfw/builtins/certdata.txt &&\
+wget --no-check-certificate http://anduin.linuxfromscratch.org/sources/other/certdata.txt &&\
 make-ca.sh &&\
 remove-expired-certs.sh certs
 
