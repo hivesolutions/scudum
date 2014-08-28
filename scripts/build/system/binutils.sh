8a73024de77a7115efb7583d90e420fa6a31048f
joamag <joamag@gmail.com>
2014-08-28 10:39:39 +0100

    new binutils

--- scripts/build/system/binutils.sh
+++ scripts/build/system/binutils.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-2.23.1}
+VERSION=${VERSION-2.24}
 
 set -e
 
@@ -6,12 +6,6 @@ wget --no-check-certificate "http://ftp.gnu.org/gnu/binutils/binutils-$VERSION.t
 rm -rf binutils-$VERSION && tar -jxf "binutils-$VERSION.tar.bz2"
 rm -f "binutils-$VERSION.tar.bz2"
 cd binutils-$VERSION
-
-wget --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/binutils-$VERSION-testsuite_fix-1.patch"
-patch -Np1 -i binutils-$VERSION-testsuite_fix-1.patch
-
-rm -fv etc/standards.info
-sed -i.bak '/^INFO/s/standards.info //' etc/Makefile.in
 
 mkdir -v ../binutils-build
 cd ../binutils-build
@@ -21,5 +15,3 @@ cd ../binutils-build
 make tooldir=/usr
 test $TEST && make check
 make tooldir=/usr install
-
-cp -v ../binutils-2.23.1/include/libiberty.h /usr/include
