f7ac3eb0dde39e8c90a832c4f1522f0734deb8b0
joamag <joamag@gmail.com>
2014-08-28 17:49:58 +0100

    new compiler directive

--- scripts/build/tools/gcc.pass2.sh
+++ scripts/build/tools/gcc.pass2.sh
@@ -51,7 +51,7 @@ cd ..
 rm -rf gcc-build && mkdir gcc-build
 cd gcc-build
 
-CC=$SCUDUM_TARGET-gcc AR=$SCUDUM_TARGET-ar RANLIB=$SCUDUM_TARGET-ranlib ../gcc-$VERSION/configure\
+CC=$SCUDUM_TARGET-gcc CXX=$SCUDUM_TARGET-g++ AR=$SCUDUM_TARGET-ar RANLIB=$SCUDUM_TARGET-ranlib ../gcc-$VERSION/configure\
     --prefix=/tools\
     --with-local-prefix=/tools\
     --with-native-system-header-dir=/tools/include\
