ba54e76e1698e23a4244861dc10d7e2ec8669a4e
joamag <joamag@gmail.com>
2014-08-29 16:30:36 +0100

    new nethogs

---
+++ scripts/build/extras/nethogs.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-0.8.0}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget --no-check-certificate "http://prdownloads.sourceforge.net/nethogs/nethogs-$VERSION.tar.gz"
+rm -rf "nethogs-$VERSION" && tar -zxf "nethogs-$VERSION.tar.gz"
+rm -f "nethogs-$VERSION.tar.gz"
+cd nethogs-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
