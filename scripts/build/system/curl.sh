25d67d7e388c274f7d3215cc721c085abf8529f2
joamag <joamag@gmail.com>
2014-08-29 16:01:02 +0100

    new curl

--- scripts/build/system/curl.sh
+++ scripts/build/system/curl.sh
@@ -7,6 +7,6 @@ rm -rf "curl-$VERSION" && tar -zxf "curl-$VERSION.tar.gz"
 rm -f "curl-$VERSION.tar.gz"
 cd curl-$VERSION
 
-./configure --prefix=/usr
+./configure --prefix=/usr --with-ca-bundle=/usr/ssl/ca-bundle.crt
 
 make && make install
