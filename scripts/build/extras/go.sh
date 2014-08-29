e62f9b2d44c3950286943c7ba16a0463922788ec
joamag <joamag@gmail.com>
2014-08-29 16:57:07 +0100

    new code

--- scripts/build/extras/go.sh
+++ scripts/build/extras/go.sh
@@ -7,9 +7,9 @@ set -e +h
 source $DIR/common.sh
 
 wget --no-check-certificate "https://storage.googleapis.com/golang/go$VERSION.src.tar.gz"
-rm -rf go-$VERSION && tar -zxf "go-$VERSION.tar.gz"
-rm -f "go-$VERSION.tar.gz"
-cd go-$VERSION
+rm -rf go && tar -zxf "go-$VERSION.src.tar.gz"
+rm -f "go-$VERSION.src.tar.gz"
+cd go
 
 ./configure --prefix=$PREFIX
 make && make install
