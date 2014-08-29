48a717bd917baff4e9f843b4623cf0bd3aebd5e3
joamag <joamag@gmail.com>
2014-08-29 14:00:55 +0100

    new code support

--- scripts/build/system/net-tools.sh
+++ scripts/build/system/net-tools.sh
@@ -7,6 +7,8 @@ rm -rf net-tools-$VERSION && tar -jxf "net-tools-$VERSION.tar.bz2"
 rm -f "net-tools-$VERSION.tar.bz2"
 cd net-tools-$VERSION
 
-less Makefile
-make config
+wget --no-check-certificate http://www.linuxfromscratch.org/patches/blfs/6.3/net-tools-$VERSION-gcc34-3.patch
+patch -Np1 -i net-tools-$VERSION-gcc34-3.patch
+
+yes "" | make config > /dev/null
 make && make -n install && make install
