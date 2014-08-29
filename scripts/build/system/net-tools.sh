0caf932e1a06274cc5df68dcef511ae756ad9fa4
joamag <joamag@gmail.com>
2014-08-29 14:04:12 +0100

    new code

--- scripts/build/system/net-tools.sh
+++ scripts/build/system/net-tools.sh
@@ -3,7 +3,7 @@ VERSION=${VERSION-CVS_20101030}
 set -e +h
 
 wget --no-check-certificate "http://anduin.linuxfromscratch.org/sources/BLFS/svn/n/net-tools-$VERSION.tar.gz"
-rm -rf net-tools-$VERSION && tar -jxf "net-tools-$VERSION.tar.gz"
+rm -rf net-tools-$VERSION && tar -zxf "net-tools-$VERSION.tar.gz"
 rm -f "net-tools-$VERSION.tar.gz"
 cd net-tools-$VERSION
 
