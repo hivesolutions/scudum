23d6c820afa160b8dd752b4c198f4e09e08c8faf
joamag <joamag@gmail.com>
2014-08-29 19:13:48 +0100

    new iptables

---
+++ scripts/build/extras/iptables.sh
@@ -0,0 +1,19 @@
+VERSION=${VERSION-1.4.21}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget --no-check-certificate "http://www.netfilter.org/projects/iptables/files/iptables-$VERSION.tar.bz2"
+rm -rf iptables-$VERSION && tar -Jxf "iptables-$VERSION.tar.bz2"
+rm -f "iptables-$VERSION.tar.bz2"
+cd iptables-$VERSION
+
+./configure --prefix=$PREFIX\
+    --sbindir=/sbin\
+    --with-xtlibdir=/lib/xtables \
+    --enable-libipq
+
+make && make install
