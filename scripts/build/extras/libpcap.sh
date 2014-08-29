5ffc50aa680825c4f444ce2b4bb66f50a4765bbc
joamag <joamag@gmail.com>
2014-08-29 16:22:24 +0100

    new libpcap

---
+++ scripts/build/extras/libpcap.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-1.6.1}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget --no-check-certificate "http://www.tcpdump.org/release/libpcap-1.6.1.tar.gz"
+rm -rf "libpcap-$VERSION" && tar -zxf "libpcap-$VERSION.tar.gz"
+rm -f "libpcap-$VERSION.tar.gz"
+cd libpcap-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
