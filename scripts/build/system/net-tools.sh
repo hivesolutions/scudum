a88da098d15781416b47ee9394920a423cc655e0
joamag <joamag@gmail.com>
2014-08-29 13:52:39 +0100

    small fix

--- scripts/build/system/net-tools.sh
+++ scripts/build/system/net-tools.sh
@@ -3,8 +3,8 @@ VERSION=${VERSION-1.60}
 set -e +h
 
 wget --no-check-certificate "http://prdownloads.sourceforge.net/net-tools/net-tools-$VERSION.tar.bz2"
-rm -rf net-tools-$VERSION && tar -zxf "net-tools-$VERSION.tar.bz2"
-rm -f "net-tools-$VERSION.tar.gz"
+rm -rf net-tools-$VERSION && tar -jxf "net-tools-$VERSION.tar.bz2"
+rm -f "net-tools-$VERSION.tar.bz2"
 cd net-tools-$VERSION
 
 ./configure --prefix=/usr
