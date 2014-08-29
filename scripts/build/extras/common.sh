6d88347c873d044855cb7cc1bc0baf7b15b4185c
joamag <joamag@gmail.com>
2014-08-29 16:14:46 +0100

    new common support

---
+++ scripts/build/extras/common.sh
@@ -0,0 +1,1 @@
+PREFIX=${PREFIX-/usr}
--- scripts/build/extras/htop.sh
+++ scripts/build/extras/htop.sh
@@ -1,12 +1,16 @@
 VERSION=${VERSION-1.0.3}
 
+DIR=$(dirname $(readlink -f $0))
+
 set -e +h
+
+source $DIR/common.sh
 
 wget --no-check-certificate "http://hisham.hm/htop/releases/$VERSION/htop-$VERSION.tar.gz"
 rm -rf "htop-$VERSION" && tar -zxf "htop-$VERSION.tar.gz"
 rm -f "htop-$VERSION.tar.gz"
 cd htop-$VERSION
 
-./configure --prefix=/usr
+./configure --prefix=$PREFIX
 
 make && make install
