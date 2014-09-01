107e67c8360b82a074da5741a16bb94e594753ad
joamag <joamag@gmail.com>
2014-09-01 16:54:01 +0100

    new "faster" script

--- scripts/build/extras/viriatum.sh
+++ scripts/build/extras/viriatum.sh
@@ -4,7 +4,7 @@ set -e +h
 
 source $DIR/common.sh
 
-rm -rf viriatum && git clone https://github.com/hivesolutions/viriatum.git
+rm -rf viriatum && git clone --depth 1 https://github.com/hivesolutions/viriatum.git
 cd viriatum
 
 ./autogen.sh && ./configure --prefix=$PREFIX
