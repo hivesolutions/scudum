0aae9d4638eab8f1c4c292bb2ef2de1608b26ad1
joamag <joamag@gmail.com>
2014-09-01 17:06:13 +0100

    new code

--- scripts/build/extras/php.sh
+++ scripts/build/extras/php.sh
@@ -11,6 +11,6 @@ rm -rf php-$VERSION && tar -zxf "php-$VERSION.tar.gz"
 rm -f "php-$VERSION.tar.gz"
 cd php-$VERSION
 
-./configure --prefix=$PREFIX --enable-embed --disable-dom --disable-simplexml\
+./configure --prefix=$PREFIX --enable-embed --disable-libxml  --disable-dom --disable-simplexml\
     --disable-xml --disable-xmlreader --disable-xmlwriter --without-pear --without-iconv
 make && make install
