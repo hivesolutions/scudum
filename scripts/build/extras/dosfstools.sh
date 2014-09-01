741add1b2393d20433c5517b653b1b927feaadbf
joamag <joamag@gmail.com>
2014-09-01 23:48:40 +0100

    new dosfs tools

---
+++ scripts/build/extras/dosfstools.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-3.0.26}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget --no-check-certificate "http://daniel-baumann.ch/files/software/dosfstools/dosfstools-$VERSION.tar.gz"
+rm -rf dosfstools-$VERSION && tar -zxf "dosfstools-$VERSION.tar.gz"
+rm -f "dosfstools-$VERSION.tar.gz"
+cd dosfstools-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
