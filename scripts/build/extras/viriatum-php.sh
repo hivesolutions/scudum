c362ba7a2a82d83e3b50dbfb10db480ac9e93b67
joamag <joamag@gmail.com>
2014-09-01 17:15:25 +0100

    new cflags

--- scripts/build/extras/viriatum-php.sh
+++ scripts/build/extras/viriatum-php.sh
@@ -4,6 +4,11 @@ set -e +h
 
 source $DIR/common.sh
 
+export CFLAGS="-I/usr/local/include/php -I/usr/local/include/php/main\
+    -I/usr/local/include/php/TSRM -I/usr/local/include/php/Zend\
+    -I/opt/include/php -I/opt/include/php/main -I/opt/include/php/TSRM\
+    -I/opt/include/php/Zend"
+
 rm -rf viriatum && git clone --depth 1 https://github.com/hivesolutions/viriatum.git
 cd viriatum/modules/mod_php
 
