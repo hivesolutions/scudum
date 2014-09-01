c37bc337fc38a041a03bc4e74177abe7fc919f56
joamag <joamag@gmail.com>
2014-09-01 18:10:19 +0100

    new help

--- scripts/build/help.sh
+++ scripts/build/help.sh
@@ -12,7 +12,6 @@ The most commonly used git commands are:\n\
    help      Prints this help message\n\
    install   Creates a new Scudum instance from the most up-to-date remote Scudum\n\
    make.iso  Builds an ISO image with the current configuration\n\
-   pack      Packs the current Scudum deployment into tar.gz file (requires no configuration)\n\
-\n"
+   pack      Packs the current Scudum deployment into tar.gz file (requires no configuration)\n"
 
-echo -ne $MESSAGE
+echo -e $MESSAGE
