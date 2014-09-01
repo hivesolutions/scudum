b3ba7e3fd41592e30cce9a5be98e3a7c1446aafa
joamag <joamag@gmail.com>
2014-09-01 18:21:52 +0100

    new code change

--- scripts/build/all.sh
+++ scripts/build/all.sh
@@ -1,94 +1,9 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-# retrieves the reference to the current files directory
-# so that it's possible to "write" the scripts as relative
 DIR=$(dirname $(readlink -f $0))
 
-# sets the execution break on error so that if any
-# of the commands fails the execution is broken
 set -e +h
 
-# removes any previously existing build directory
-# and re-constructs the directory changing into it
-rm -rf build && mkdir build
-cd build
-
-# installs the dependencies for the various operations
-# that are going to be performed in the next steps
-$DIR/base/deps.sh
-
-# runs the cleanup operation, this should remove any
-# previous installation of scudum from the file system
-$DIR/base/cleanup.sh
-
-# loads the complete set of environment variables
-# that are going to be used in the build process
-source $DIR/base/config.sh
-source $DIR/base/config.tools.sh
-
-# changes the default remembering option and the
-# creation mask for the current user
-set +h
-umask 022
-
-# runs the complete set of package specific scripts
-# in order to build their source code properly
-$DIR/tools/binutils.pass1.sh
-$DIR/tools/gcc.pass1.sh
-$DIR/tools/linux-headers.sh
-$DIR/tools/glibc.sh
-$DIR/tools/libstdcpp.sh
-$DIR/tools/binutils.pass2.sh
-$DIR/tools/gcc.pass2.sh
-$DIR/tools/tcl.sh
-$DIR/tools/expect.sh
-$DIR/tools/dejagnu.sh
-$DIR/tools/check.sh
-$DIR/tools/ncurses.sh
-$DIR/tools/bash.sh
-$DIR/tools/bzip2.sh
-$DIR/tools/coreutils.sh
-$DIR/tools/diffutils.sh
-$DIR/tools/file.sh
-$DIR/tools/findutils.sh
-$DIR/tools/gawk.sh
-$DIR/tools/gettext.sh
-$DIR/tools/grep.sh
-$DIR/tools/gzip.sh
-$DIR/tools/m4.sh
-$DIR/tools/make.sh
-$DIR/tools/patch.sh
-$DIR/tools/perl.sh
-$DIR/tools/sed.sh
-$DIR/tools/tar.sh
-$DIR/tools/texinfo.sh
-$DIR/tools/xz.sh
-$DIR/tools/openssl.sh
-$DIR/tools/wget.sh
-
-# runs the strip operation on the complete set of tools
-# so that some disk space is spared by removing the debug
-# and the unneeded symbols from the libraries
-$DIR/tools/strip.sh
-
-# removes the directory where the building process has been done
-# so that no extra files leak to the final building stages, then
-# deletes also the dynamic link reference in tools (not required)
-cd .. && rm -rf build
-rm -f /tools
-
-# runs the sync command so that the current write operations are
-# flushed and further operations reflect the new system state,
-# note that the current bash hash state is also cleared
-hash -r && sync
-
-# updates the permissions of the tools directory and starts
-# the chroot operation in it so that a different execution
-# set is started from "now on" (as expected)
-chown -R root:root $SCUDUM/tools
-$DIR/base/chroot.sh /tools/repo/scripts/build/base/system.sh
-
-# runs the final strip operation on the generated files so
-# that some of the size for the files is spared
-$DIR/base/chroot.sh /tools/repo/scripts/build/system/strip.sh
+$DIR/install.sh
+$DIR/make.iso.sh
--- scripts/build/help.sh
+++ scripts/build/help.sh
@@ -4,7 +4,7 @@
 MESSAGE="usage: scudum <command>\n\
 \n\
 The most commonly used git commands are:\n\
-   all       Rebuild the Scudum system from the beginning (takes some time)\n\
+   all       Runs the installation process (from up-to-date image) and makes an ISO\n\
    cleanup   Cleans the current Scudum (/scudum) deployment, removing extra files\n\
    config    Reconfigures scudum according to local or global config files\n\
    deploy    Deploys the current Scudum instance to the remote repository\n\
@@ -12,6 +12,7 @@ The most commonly used git commands are:\n\
    help      Prints this help message\n\
    install   Creates a new Scudum instance from the most up-to-date remote Scudum\n\
    make.iso  Builds an ISO image with the current configuration\n\
-   pack      Packs the current Scudum deployment into tar.gz file (requires no configuration)\n"
+   pack      Packs the current Scudum deployment into tar.gz file (requires no configuration)\n
+   root      Rebuilds the Scudum system from the beginning (takes some time)\n"
 
 echo -e "$MESSAGE"
