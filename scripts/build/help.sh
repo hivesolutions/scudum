6ea524be594d56b684dafb166473c9ab0f730dc2
joamag <joamag@gmail.com>
2014-09-01 18:05:53 +0100

    new help support

---
+++ scripts/build/help.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+MESSAGE="all - Rebuild the Scudum system from the beginning (takes some time)\n\
+cleanup - Cleans the current Scudum (/scudum) deployment, removing extra files\n\
+config - Reconfigures scudum according to local or global config files\n\
+deploy - Deploys the current Scudum instance to the remote repository\n\
+extras - Re-creates the extras (packages) in the current deployment\n\
+help - Prints this help message\n\
+install - Creates a new Scudum instance from the most up-to-date remote Scudum\n\
+make.iso - Builds an ISO image with the current configuration\n\
+pack - Packs the current Scudum deployment into tar.gz file (requires no configuration)"
+
+echo -n $MESSAGE
