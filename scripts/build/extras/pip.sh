ff0661bc38c815da95fd6952729272e04ffb5cbe
joamag <joamag@gmail.com>
2014-09-02 18:46:23 +0100

    new conditional args

--- scripts/build/extras/pip.sh
+++ scripts/build/extras/pip.sh
@@ -4,5 +4,11 @@ set -e +h
 
 source $DIR/common.sh
 
+if [ "$PREFIX" == "/usr" ]; then
+    ARGS=""
+else
+    ARGS="--user"
+fi
+
 rm -f get-pip.py && wget "https://bootstrap.pypa.io/get-pip.py"
-python get-pip.py --user
+python get-pip.py $ARGS
