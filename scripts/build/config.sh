b241edb0e0130e8f343c720dd9721b915aa8b78a
joamag <joamag@gmail.com>
2014-09-02 17:26:22 +0100

    small change

--- scripts/build/config.sh
+++ scripts/build/config.sh
@@ -30,6 +30,7 @@ fi
 mkdir -p $SCUDUM/etc/scudum
 touch $SCUDUM/etc/scudum/DISTRIB
 touch $SCUDUM/etc/scudum/CONFIGURED
+
 echo $DISTRIB > $SCUDUM/etc/scudum/DISTRIB
 echo $EXTRAS > $SCUDUM/etc/scudum/CONFIGURED
 
