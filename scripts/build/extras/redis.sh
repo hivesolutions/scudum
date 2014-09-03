bd3f48b6a6b36b5c5d29914d6668e4c73489e20a
joamag <joamag@gmail.com>
2014-09-03 21:03:43 +0100

    new code

--- scripts/build/extras/redis.sh
+++ scripts/build/extras/redis.sh
@@ -6,6 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
+depends "tcl"
+
 wget "http://download.redis.io/releases/redis-$VERSION.tar.gz"
 rm -rf redis-$VERSION && tar -zxf "redis-$VERSION.tar.gz"
 rm -f "redis-$VERSION.tar.gz"
