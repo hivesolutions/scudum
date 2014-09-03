e35730770918769d7551fb545f786755ea519b38
joamag <joamag@gmail.com>
2014-09-03 20:54:14 +0100

    new redis

---
+++ scripts/build/extras/redis.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.8.14}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://download.redis.io/releases/redis-$VERSION.tar.gz"
+rm -rf redis-$VERSION && tar -zxf "redis-$VERSION.tar.gz"
+rm -f "redis-$VERSION.tar.gz"
+cd redis-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
