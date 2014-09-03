07d81e3884d496cc7bf85899dbc8edc3b57e3052
joamag <joamag@gmail.com>
2014-09-03 17:22:50 +0100

    small change

--- scripts/build/extras/mongodb.sh
+++ scripts/build/extras/mongodb.sh
@@ -13,4 +13,4 @@ rm -rf mongodb-src-r$VERSION && tar -zxf "mongodb-src-r$VERSION.tar.gz"
 rm -f "mongodb-src-r$VERSION.tar.gz"
 cd mongodb-src-r$VERSION
 
-scons mongod
+scons mongod -Wno-unused-local-typedefs
