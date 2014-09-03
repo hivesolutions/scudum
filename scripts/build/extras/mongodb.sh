0d6cac0bf38705193458eea58eb3070316fb8541
joamag <joamag@gmail.com>
2014-09-03 17:05:19 +0100

    initial mongodb script

---
+++ scripts/build/extras/mongodb.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-2.4.11}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "scons"
+
+wget "https://fastdl.mongodb.org/src/mongodb-src-r$VERSION.tar.gz"
+rm -rf mongodb-src-r$VERSION && tar -zxf "mongodb-src-r$VERSION.tar.gz"
+rm -f "mongodb-src-r$VERSION.tar.gz"
+cd mongodb-src-r$VERSION
+
+scons mongod
