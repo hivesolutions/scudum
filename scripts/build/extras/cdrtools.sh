bac4311de7b58afb56701f7e98340fc7cd7beada
joamag <joamag@gmail.com>
2014-09-04 11:45:39 +0100

    new code

--- scripts/build/extras/cdrtools.sh
+++ scripts/build/extras/cdrtools.sh
@@ -6,11 +6,11 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "schily"
-
 wget "http://prdownloads.sourceforge.net/cdrtools/cdrtools-$VERSION.tar.gz"
 rm -rf cdrtools-$VERSION && tar -zxf "cdrtools-$VERSION.tar.gz"
 rm -f "cdrtools-$VERSION.tar.gz"
 cd cdrtools-$VERSION
 
-smake && PREFIX=$PREFIX DEFMANBASE=. smake install
+make && make install PREFIX=$PREFIX
+
+ln -s ../schily/bin/mkisofs $PREFIX/bin/mkisofs
--- scripts/build/extras/schily.sh
+++ scripts/build/extras/schily.sh
@@ -11,5 +11,10 @@ rm -rf schily-$VERSION && tar -jxf "schily-$VERSION.tar.bz2"
 rm -f "schily-$VERSION.tar.bz2"
 cd schily-$VERSION
 
-./configure --prefix=$PREFIX
-make && make install
+make && make install PREFIX=$PREFIX
+
+ln -s ../schily/bin/smake $PREFIX/bin/smake
+ln -s ../schily/bin/mkisofs $PREFIX/bin/mkisofs
+ln -s ../schily/bin/isoinfo $PREFIX/bin/isoinfo
+ln -s ../schily/bin/isodump $PREFIX/bin/isodump
+ln -s ../schily/bin/isodebug $PREFIX/bin/isodebug
