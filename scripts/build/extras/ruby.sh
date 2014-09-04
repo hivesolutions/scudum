85ca7006029aa0c166b64df29c6bd7b1556f78a6
joamag <joamag@gmail.com>
2014-09-04 11:00:09 +0100

    new ruby script

---
+++ scripts/build/extras/ruby.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.1.2}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "sqlite3" "pcre"
+
+wget "http://cache.ruby-lang.org/pub/ruby/2.1/ruby-$VERSION.tar.gz"
+rm -rf ruby-$VERSION && tar -zxf "ruby-$VERSION.tar.gz"
+rm -f "ruby-$VERSION.tar.gz"
+cd ruby-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
