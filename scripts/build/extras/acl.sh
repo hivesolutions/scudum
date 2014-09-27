0138c25aba8648a6982ee37d5774393a277e40bd
joamag <joamag@gmail.com>
2014-09-27 12:49:16 +0100

    new acl package

---
+++ scripts/build/extras/acl.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.2.52}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://download.savannah.gnu.org/releases/acl/acl-$VERSION.src.tar.gz"
+rm -rf acl-$VERSION && tar -zxf "acl-$VERSION.src.tar.gz"
+rm -f "acl-$VERSION.src.tar.gz"
+cd attr-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install install-dev install-lib
