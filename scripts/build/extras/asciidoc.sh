840ca55837df9dbb049e6e287b791baa81be2864
joamag <joamag@gmail.com>
2014-09-27 12:55:19 +0100

    new ascii doc

---
+++ scripts/build/extras/asciidoc.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-8.6.9}
+
+DIR=$(dirname $(readlink -f $0))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://prdownloads.sourceforge.net/asciidoc/asciidoc-$VERSION.tar.gz"
+rm -rf asciidoc-$VERSION && tar -zxf "asciidoc-$VERSION.tar.gz"
+rm -f "asciidoc-$VERSION.tar.gz"
+cd asciidoc-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
