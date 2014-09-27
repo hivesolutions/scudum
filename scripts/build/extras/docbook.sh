6f7243ecc7694aadea8b292e093d38ca3ff97f26
joamag <joamag@gmail.com>
2014-09-27 13:41:20 +0100

    new options

--- scripts/build/extras/docbook.sh
+++ scripts/build/extras/docbook.sh
@@ -67,3 +67,26 @@ xmlcatalog --noout --add "rewriteURI"\
     "http://www.oasis-open.org/docbook/xml/4.5"\
     "file://$PREFIX/share/xml/docbook/xml-dtd-4.5"\
     /etc/xml/docbook
+
+for DTDVERSION in 4.1.2 4.2 4.3 4.4; do
+    xmlcatalog --noout --add "public"\
+        "-//OASIS//DTD DocBook XML V$DTDVERSION//EN"\
+        "http://www.oasis-open.org/docbook/xml/$DTDVERSION/docbookx.dtd"\
+        /etc/xml/docbook
+    xmlcatalog --noout --add "rewriteSystem"\
+        "http://www.oasis-open.org/docbook/xml/$DTDVERSION"\
+        "file://$PREFIX/share/xml/docbook/xml-dtd-4.5"\
+        /etc/xml/docbook
+    xmlcatalog --noout --add "rewriteURI"\
+        "http://www.oasis-open.org/docbook/xml/$DTDVERSION"\
+        "file://$PREFIX/share/xml/docbook/xml-dtd-4.5"\
+        /etc/xml/docbook
+    xmlcatalog --noout --add "delegateSystem"\
+        "http://www.oasis-open.org/docbook/xml/$DTDVERSION/"\
+        "file:///etc/xml/docbook"\
+        /etc/xml/catalog
+    xmlcatalog --noout --add "delegateURI"\
+        "http://www.oasis-open.org/docbook/xml/$DTDVERSION/"\
+        "file:///etc/xml/docbook"\
+        /etc/xml/catalog
+done
