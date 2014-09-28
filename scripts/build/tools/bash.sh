14cf683223cd6a4854c8ab9061439136a13be915
joamag <joamag@gmail.com>
2014-09-28 17:12:02 +0100

    new version

--- scripts/build/tools/bash.sh
+++ scripts/build/tools/bash.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-4.2}
+VERSION=${VERSION-4.3}
 
 set -e +h
 
@@ -6,9 +6,6 @@ wget "http://ftp.gnu.org/gnu/bash/bash-$VERSION.tar.gz"
 rm -f "bash-$VERSION" && tar -zxf "bash-$VERSION.tar.gz"
 rm -f "bash-$VERSION.tar.gz"
 cd bash-$VERSION
-
-wget "http://www.linuxfromscratch.org/patches/lfs/7.3/bash-$VERSION-fixes-11.patch"
-patch -Np1 -i bash-$VERSION-fixes-11.patch
 
 ./configure --prefix=$PREFIX --without-bash-malloc
 make && make install
