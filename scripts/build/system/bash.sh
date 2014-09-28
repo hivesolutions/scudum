1b3901776050db6f947c0f9611b68161d944a8dd
joamag <joamag@gmail.com>
2014-09-28 17:12:27 +0100

    new version

--- scripts/build/system/bash.sh
+++ scripts/build/system/bash.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-4.2}
+VERSION=${VERSION-4.3}
 
 set -e +h
 
@@ -6,9 +6,6 @@ wget --no-check-certificate "http://ftp.gnu.org/gnu/bash/bash-$VERSION.tar.gz"
 rm -rf bash-$VERSION && tar -zxf "bash-$VERSION.tar.gz"
 rm -f "bash-$VERSION.tar.gz"
 cd bash-$VERSION
-
-wget --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/bash-$VERSION-fixes-11.patch"
-patch -Np1 -i bash-$VERSION-fixes-11.patch
 
 ./configure\
     --prefix=/usr\
