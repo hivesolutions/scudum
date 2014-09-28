2e8d21506e3a7fa6c2fb28c18bc88fb0e1db59ba
joamag <joamag@gmail.com>
2014-09-28 17:10:28 +0100

    new pc target

--- scripts/build/tools/gcc.pass2.sh
+++ scripts/build/tools/gcc.pass2.sh
@@ -47,6 +47,7 @@ rm -rf gcc-build && mkdir gcc-build
 cd gcc-build
 
 CC=$SCUDUM_TARGET-gcc CXX=$SCUDUM_TARGET-g++ AR=$SCUDUM_TARGET-ar RANLIB=$SCUDUM_TARGET-ranlib ../gcc-$VERSION/configure\
+    --target=$PC_TARGET\
     --prefix=/tools\
     --with-local-prefix=/tools\
     --with-native-system-header-dir=/tools/include\
