fe6cfc8c2ed717c2dd542aca5e2ca30992054066
joamag <joamag@gmail.com>
2014-09-29 10:35:42 +0100

    new bash patches

--- scripts/build/tools/bash.sh
+++ scripts/build/tools/bash.sh
@@ -1,4 +1,6 @@
 VERSION=${VERSION-4.3}
+VERSION_L=${VERSION_L-43}
+PATCH_SEQ=${PATCH_SEQ-1 27}
 
 set -e +h
 
@@ -6,6 +8,11 @@ wget "http://ftp.gnu.org/gnu/bash/bash-$VERSION.tar.gz"
 rm -f "bash-$VERSION" && tar -zxf "bash-$VERSION.tar.gz"
 rm -f "bash-$VERSION.tar.gz"
 cd bash-$VERSION
+
+for index in $(seq -f "%03g" $PATCH_SEQ); do
+    wget http://ftp.gnu.org/gnu/bash/bash-$VERSION-patches/bash$VERSION_L-$index
+    patch -Np0 -i bash$VERSION_L-$index
+done
 
 ./configure --prefix=$PREFIX --without-bash-malloc
 make && make install
