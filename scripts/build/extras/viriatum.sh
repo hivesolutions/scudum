cdde0ef2326e8954daeb2f7ad36d63ffcce690a7
joamag <joamag@gmail.com>
2014-09-30 11:19:48 +0100

    new config path

--- scripts/build/extras/viriatum.sh
+++ scripts/build/extras/viriatum.sh
@@ -9,5 +9,5 @@ depends "pcre"
 rm -rf viriatum && git clone --depth 1 https://github.com/hivesolutions/viriatum.git
 cd viriatum
 
-./autogen.sh && ./configure --prefix=$PREFIX --sysconfdir=/etc/viriatum
+./autogen.sh && ./configure --prefix=$PREFIX --sysconfdir=/etc
 make && make install
