4b0fc80547b7d15ae9e3029bea363288bec5e9eb
joamag <joamag@gmail.com>
2014-09-30 10:06:29 +0100

    new version

--- scripts/build/extras/php.sh
+++ scripts/build/extras/php.sh
@@ -8,7 +8,7 @@ source $DIR/common.sh
 
 export CFLAGS="$CFLAGS -fpic"
 
-wget "http://downloads.php.net/stas/php-$VERSION.tar.gz" "--output-document=php-$VERSION.tar.gz"
+wget "http://php.net/get/php-$VERSION.tar.bz2/from/this/mirror" "--output-document=php-$VERSION.tar.gz"
 rm -rf php-$VERSION && tar -zxf "php-$VERSION.tar.gz"
 rm -f "php-$VERSION.tar.gz"
 cd php-$VERSION
