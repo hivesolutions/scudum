31ad70cd4130e8e134d9eaad688687afd223633c
joamag <joamag@gmail.com>
2014-10-01 16:39:58 +0100

    new which

---
+++ scripts/build/extras/which.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.20}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://ftp.gnu.org/gnu/which/which-$VERSION.tar.gz"
+rm -rf which-$VERSION && tar -zxf "which-$VERSION.tar.gz"
+rm -f "which-$VERSION.tar.gz"
+cd which-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
