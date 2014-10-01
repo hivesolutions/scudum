fdb8781bd522e3422d96ce6b858b910708036c76
joamag <joamag@gmail.com>
2014-10-01 17:02:09 +0100

    small code change

--- scripts/build/extras/gnu-efi.sh
+++ scripts/build/extras/gnu-efi.sh
@@ -1,4 +1,5 @@
 VERSION=${VERSION-3.0w}
+VERSION_L=${VERSION_L-3.0}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -7,9 +8,9 @@ set -e +h
 source $DIR/common.sh
 
 wget "http://prdownloads.sourceforge.net/gnu-efi/gnu-efi_$VERSION.orig.tar.gz"
-rm -rf gnu-efi-$VERSION && tar -zxf "gnu-efi_$VERSION.orig.tar.gz"
+rm -rf gnu-efi-$VERSION_L && tar -zxf "gnu-efi_$VERSION.orig.tar.gz"
 rm -f "gnu-efi_$VERSION.orig.tar.gz"
-cd gnu-efi-$VERSION
+cd gnu-efi-$VERSION_L
 
 make
 install -m 644 gnuefi/*.lds $PREFIX/lib
