36f43809cad1c0248b595644b6a45a09c0322852
joamag <joamag@gmail.com>
2014-10-01 11:21:08 +0100

    new deps

--- scripts/build/base/deps.sh
+++ scripts/build/base/deps.sh
@@ -1,4 +1,10 @@
 set -e +h
 
-apt-get -y install wget g++ make bison flex gawk gperf\
-    texinfo libncurses5 libncurses5-dev libssl-dev
+if hash apt-get 2> /dev/null; then
+    apt-get -y install wget g++ make bison flex gawk gperf\
+        texinfo libncurses5 libncurses5-dev libssl-dev
+elif hash scu 2> /dev/null; then
+    continue
+else
+    exit 1
+fi
