773e99ba8f82b35f362fedd87ca821116f62c7eb
joamag <joamag@gmail.com>
2014-10-01 23:41:41 +0100

    new file

--- scripts/build/extras/jpeg.sh
+++ scripts/build/extras/jpeg.sh
@@ -7,7 +7,7 @@ set -e +h
 source $DIR/common.sh
 
 wget "http://www.ijg.org/files/jpegsrc.v$VERSION.tar.gz"
-rm -rf jpeg-$VERSION && tar -jxf "jpegsrc.v$VERSION.tar.gz"
+rm -rf jpeg-$VERSION && tar -zxf "jpegsrc.v$VERSION.tar.gz"
 rm -f "jpegsrc.v$VERSION.tar.gz"
 cd jpeg-$VERSION
 
