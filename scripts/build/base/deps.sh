6cb41e5e0d41a6e6209e36ebe0eede3519cef67f
joamag <joamag@gmail.com>
2014-10-01 11:22:23 +0100

    new code

--- scripts/build/base/deps.sh
+++ scripts/build/base/deps.sh
@@ -4,7 +4,6 @@ if hash apt-get 2> /dev/null; then
     apt-get -y install wget g++ make bison flex gawk gperf\
         texinfo libncurses5 libncurses5-dev libssl-dev
 elif hash scu 2> /dev/null; then
-    continue
 else
     exit 1
 fi
