b90cb0a57c494a60629f595f595aecebffe8b2ea
joamag <joamag@gmail.com>
2014-10-01 15:10:15 +0100

    initial support for make usb

---
+++ scripts/build/make.usb.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+NAME=${NAME-scudum}
+VERSION=${VERSION-$(date +%Y%m%d)}
+LABEL=${LABEL-Scudum}
+BASE=${BASE-/mnt/builds}
+TARGET=${TARGET-$BASE/$NAME/iso}
+LOADER=${LOADER-isolinux}
+SCHEMA=${SCHEMA-transient}
+CONFIG=${CONFIG-1}
+CLEANUP=${CLEANUP-1}
+DEPLOY=${DEPLOY-0}
+SQUASH=${SQUASH-1}
+AUTORUN=${AUTORUN-1}
+
+CUR=$(pwd)
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/base/config.sh
+
+DISTRIB=${DISTRIB-$(cat $SCUDUM/etc/scudum/DISTRIB)}
+
+if [ "$DISTRIB" == "generic" ]; then
+    FILE=${FILE-$NAME-$VERSION.usb.img}
+else
+    FILE=${FILE-$NAME-$DISTRIB-$VERSION.usb.img}
+fi
+
+if type apt-get &> /dev/null; then
+    apt-get -y install squashfs-tools
+elif type scu &> /dev/null; then
+    scu install squashfs-tools
+else
+    exit 1
+fi
+
+if [ "$CONFIG" == "1" ]; then
+    SCHEMA=$SCHEMA $DIR/config.sh
+fi
+
+if [ ! -e $SCUDUM/etc/scudum/CONFIGURED ]; then
+    echo "Scudum not configured, not possible to make USB"
+    exit 1
+fi
+
+if [ "$CLEANUP" == "1" ]; then
+    $DIR/cleanup.sh
+fi
+
+cd $SCUDUM
+
+rm -rfv images && mkdir -pv images
+tar -zcf images/root.tar.gz root
+tar -zcf images/dev.tar.gz dev
+tar -zcf images/etc.tar.gz etc
+
+cd $CUR
+
+if [ "$SQUASH" == "1" ]; then
+    ISO_DIR=/tmp/$NAME.iso.dir
+    mksquashfs $SCUDUM $NAME.sqfs
+    mkdir -pv $ISO_DIR
+    cp -rp $SCUDUM/isolinux $ISO_DIR
+    mv $NAME.sqfs $ISO_DIR
+else
+    ISO_DIR=$SCUDUM
+fi
+
+if [ "$AUTORUN" == "1" ]; then
+    cp $ISO_DIR/isolinux/autorun.inf $ISO_DIR
+    cp $ISO_DIR/isolinux/scudum.ico $ISO_DIR
+fi
+
+mkisofs -r -J -R -U -joliet -joliet-long -o $FILE\
+    -b isolinux/isolinux.bin -c isolinux/boot.cat\
+    -no-emul-boot -boot-load-size 4 -boot-info-table\
+    -V $LABEL $ISO_DIR
+
+if [ "$AUTORUN" == "1" ]; then
+    rm -v $ISO_DIR/autorun.inf
+    rm -v $ISO_DIR/scudum.ico
+fi
+
+if [ "$SQUASH" == "1" ]; then
+    rm -rf $ISO_DIR
+fi
+
+if [ "$DEPLOY" == "1" ]; then
+    mv $FILE $TARGET
+fi
+
+rm -rv $SCUDUM/images
