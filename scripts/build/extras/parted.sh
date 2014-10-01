65f00a38bbff78102b2a10ab8ef775f0ae906004
joamag <joamag@gmail.com>
2014-10-01 15:36:37 +0100

    small code change

--- scripts/build/extras/parted.sh
+++ scripts/build/extras/parted.sh
@@ -3,7 +3,7 @@ VERSION=${VERSION-3.2}
 set -e +h
 
 wget --no-check-certificate "http://ftp.gnu.org/gnu/parted/parted-$VERSION.tar.xz"
-rm -rf parted-$VERSION && tar -jxf "parted-$VERSION.tar.xz"
+rm -rf parted-$VERSION && tar -Jxf "parted-$VERSION.tar.xz"
 rm -f "parted-$VERSION.tar.xz"
 cd parted-$VERSION
 
