0c040b21a868575795b5bd0760ab670a0936f8b7
joamag <joamag@gmail.com>
2014-10-01 10:25:27 +0100

    small code fix

--- scripts/build/extras/acl.sh
+++ scripts/build/extras/acl.sh
@@ -6,6 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
+depends "attr"
+
 wget "http://download.savannah.gnu.org/releases/acl/acl-$VERSION.src.tar.gz"
 rm -rf acl-$VERSION && tar -zxf "acl-$VERSION.src.tar.gz"
 rm -f "acl-$VERSION.src.tar.gz"
