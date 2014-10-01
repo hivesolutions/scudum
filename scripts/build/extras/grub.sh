353e8879e14e07215f12014a141e4ad750e2807a
joamag <joamag@gmail.com>
2014-10-01 13:38:07 +0100

    small code fix

--- scripts/build/extras/grub.sh
+++ scripts/build/extras/grub.sh
@@ -2,10 +2,10 @@ VERSION=${VERSION-2.00}
 
 set -e +h
 
-wget --no-check-certificate "http://ftp.gnu.org/gnu/grub/grub-$VERSION/gcc-$VERSION.tar.gz"
-rm -rf gcc-$VERSION && tar -zxf "gcc-$VERSION.tar.gz"
-rm -f "gcc-$VERSION.tar.gz"
-cd gcc-$VERSION
+wget --no-check-certificate "http://ftp.gnu.org/gnu/grub/grub-$VERSION/grub-$VERSION.tar.gz"
+rm -rf grub-$VERSION && tar -zxf "grub-$VERSION.tar.gz"
+rm -f "grub-$VERSION.tar.gz"
+cd grub-$VERSION
 
 ./configure --prefix=$PREFIX --sysconfdir=/etc
 make && make install
