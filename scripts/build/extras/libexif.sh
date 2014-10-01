17cb44b814d678232f33a8b0d1744facf7bf7176
joamag <joamag@gmail.com>
2014-10-01 23:44:53 +0100

    new libexif

---
+++ scripts/build/extras/libexif.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-0.6.21}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+export CFLAGS="$CFLAGS -fpic"
+
+wget wget "http://prdownloads.sourceforge.net/libexif/libexif-$VERSION.tar.gz"
+rm -rf libexif-$VERSION && tar -zxf "libexif-$VERSION.tar.gz"
+rm -f "libexif-$VERSION.tar.gz"
+cd libexif-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
