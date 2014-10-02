3e48a4c2edf562b889035bded791b5473ad45b88
joamag <joamag@gmail.com>
2014-10-02 11:35:46 +0100

    new extra

---
+++ scripts/build/extras/lynx.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.8.7}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "ftp://artfiles.org/lynx.isc.org/lynx$VERSION/lynx$VERSION.tar.gz"
+rm -rf lynx$VERSION && tar -jxf "lynx$VERSION.tar.gz"
+rm -f "lynx$VERSION.tar.gz"
+cd lynx$VERSION.tar.gz
+
+./configure --prefix=$PREFIX
+make && make install
