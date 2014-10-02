afc4b4fc15a6e0e22b1bb75f426a1c29ae5c7d43
joamag <joamag@gmail.com>
2014-10-02 23:53:43 +0100

    new kpartx support

---
+++ scripts/build/extras/kpartx.sh
@@ -0,0 +1,12 @@
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "lvm2"
+
+rm -rf multipath-tools && git clone http://git.opensvc.com/multipath-tools/.git
+cd multipath-tools/kpartx
+
+make && make install DESTDIR=$PREFIX
