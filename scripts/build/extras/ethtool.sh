40224ef5362d449c868735b89b520bfa717d440b
joamag <joamag@gmail.com>
2014-10-02 15:36:44 +0100

    new ethtool support

---
+++ scripts/build/extras/ethtool.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.6.36}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://prdownloads.sourceforge.net/gkernel/ethtool-$VERSION.tar.gz"
+rm -rf ethtool-$VERSION && tar -jxf "ethtool-$VERSION.tar.gz"
+rm -f "ethtool-$VERSION.tar.gz"
+cd ethtool-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
