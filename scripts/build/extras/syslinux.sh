950fd22c28c6e388cd305de43465773be0a61626
joamag <joamag@gmail.com>
2014-10-02 14:09:30 +0100

    new make flags unset

--- scripts/build/extras/syslinux.sh
+++ scripts/build/extras/syslinux.sh
@@ -1,7 +1,5 @@
 VERSION=${VERSION-6.02}
 
-unset MAKEFLAGS
-
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 set -e +h
@@ -9,6 +7,8 @@ set -e +h
 source $DIR/common.sh
 
 depends "nasm" "upx" "which"
+
+unset MAKEFLAGS
 
 wget "https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-$VERSION.tar.xz"
 rm -rf syslinux-$VERSION && tar -Jxf "syslinux-$VERSION.tar.xz"
--- scripts/build/system/openssl.sh
+++ scripts/build/system/openssl.sh
@@ -1,8 +1,8 @@
 VERSION=${VERSION-1.0.1i}
 
-unset MAKEFLAGS
-
 set -e +h
+
+unset MAKEFLAGS
 
 wget --no-check-certificate "https://www.openssl.org/source/openssl-$VERSION.tar.gz"
 rm -rf openssl-$VERSION && tar -zxf "openssl-$VERSION.tar.gz"
