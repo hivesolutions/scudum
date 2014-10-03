61814c75275ca9c7b2a996a1ac6749117d37a730
joamag <joamag@gmail.com>
2014-10-03 11:57:13 +0100

    new scudum system

---
+++ scripts/build/extras/scudum-system.sh
@@ -0,0 +1,11 @@
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+rm -rf scudum && git clone --depth 1 https://github.com/hivesolutions/scudum.git
+cd scudum
+
+mkdir -p $PREFIX/bin
+cp -rp system/bin/* $PREFIX/bin
