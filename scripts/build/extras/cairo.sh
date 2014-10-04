cca7d6bd1fb441d8d4dab4a4266112de4c645af0
joamag <joamag@gmail.com>
2014-10-04 14:05:19 +0100

    initial cairo support

---
+++ scripts/build/extras/cairo.sh
@@ -0,0 +1,22 @@
+VERSION=${VERSION-1.12.16}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "freetype" "pixman"
+
+wget "http://cairographics.org/releases/cairo-$VERSION.tar.xz"
+rm -rf cairo-$VERSION && tar -Jxf "cairo-$VERSION.tar.xz"
+rm -f "cairo-$VERSION.tar.xz"
+cd cairo-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --disable-static\
+    --enable-drm\
+    --enable-tee
+
+make && make install
