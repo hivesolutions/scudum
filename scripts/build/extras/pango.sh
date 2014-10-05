5db187cd29b7a194bf884fbbf8da9f2ecfef45fd
joamag <joamag@gmail.com>
2014-10-05 22:41:15 +0100

    new code

--- scripts/build/extras/pango.sh
+++ scripts/build/extras/pango.sh
@@ -8,10 +8,10 @@ source $DIR/common.sh
 
 depends "cairo" "harfbuzz" "fontconfig"
 
-wget "http://ftp.gnome.org/pub/gnome/sources/pango/VERSION/pango-VERSION.tar.xz"
-rm -rf pango-VERSION && tar -Jxf "pango-VERSION.tar.xz"
-rm -f "pango-VERSION.tar.xz"
-cd pango-VERSION
+wget "http://ftp.gnome.org/pub/gnome/sources/pango/$VERSION/pango-$VERSION.tar.xz"
+rm -rf pango-$VERSION && tar -Jxf "pango-$VERSION.tar.xz"
+rm -f "pango-$VERSION.tar.xz"
+cd pango-$VERSION
 
 ./configure --prefix=$PREFIX --sysconfdir=/etc
 make && make install
