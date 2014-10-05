83104cef12f2e71b75e42d99438d70f995f8b117
joamag <joamag@gmail.com>
2014-10-05 22:43:26 +0100

    new plymouth

---
+++ scripts/build/extras/plymouth.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-0.9.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libpng" "pango"
+
+wget "http://www.freedesktop.org/software/plymouth/releases/plymouth-$VERSION.tar.bz2"
+rm -rf plymouth-$VERSION && tar -jxf "plymouth-$VERSION.tar.bz2"
+rm -f "plymouth-$VERSION.tar.bz2"
+cd plymouth-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
