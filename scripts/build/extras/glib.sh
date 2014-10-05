dfe66828e4e5f1f91788836cce9450b2d6f628ea
joamag <joamag@gmail.com>
2014-10-05 21:04:15 +0100

    new glib

---
+++ scripts/build/extras/glib.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-2.42.0}
+VERSION_L=${VERSION_L-2.42}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libffi" "python"
+
+wget "http://ftp.gnome.org/pub/gnome/sources/glib/$VERSION_L/glib-$VERSION.tar.xz"
+rm -rf glib-$VERSION && tar -Jxf "glib-$VERSION.tar.xz"
+rm -f "glib-$VERSION.tar.xz"
+cd glib-$VERSION
+
+./configure --prefix=$PREFIX --with-pcre=system
+make && make install
