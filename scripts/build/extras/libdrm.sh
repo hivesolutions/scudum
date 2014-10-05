c2af847e26fbcd013584df7087ddca962b920562
joamag <joamag@gmail.com>
2014-10-06 00:20:58 +0100

    new lib drm

---
+++ scripts/build/extras/libdrm.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.4.58}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://dri.freedesktop.org/libdrm/libdrm-$VERSION.tar.bz2"
+rm -rf libdrm-$VERSION && tar -jxf "libdrm-$VERSION.tar.bz2"
+rm -f "libdrm-$VERSION.tar.bz2"
+cd libdrm-$VERSION
+
+./configure --prefix=$PREFIX --enable-udev
+make && make install
