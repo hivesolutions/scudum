7546ffc556c1858066810f484f21eb737f25cb1c
joamag <joamag@gmail.com>
2014-10-06 18:12:51 +0100

    new xkeyboard config

---
+++ scripts/build/extras/xkeyboard-config.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.13}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "intltool"
+
+wget "http://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-$VERSION.tar.bz2"
+rm -rf xkeyboard-config-$VERSION && tar -jxf "xkeyboard-config-$VERSION.tar.bz2"
+rm -f "xkeyboard-config-$VERSION.tar.bz2"
+cd xkeyboard-config-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
