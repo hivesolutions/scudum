2a38668993544806ac54f6415d6eb46fb5f4851d
joamag <joamag@gmail.com>
2014-10-06 14:27:49 +0100

    new expat

---
+++ scripts/build/extras/expat.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.1.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://prdownloads.sourceforge.net/expat/expat-$VERSION.tar.gz"
+rm -rf expat-$VERSION && tar -zxf "expat-$VERSION.tar.gz"
+rm -f "expat-$VERSION.tar.gz"
+cd expat-$VERSION
+
+./configure --prefix=$PREFIX --disable-static
+make && make install
