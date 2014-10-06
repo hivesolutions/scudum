8d7fabaff127d5ed1d4e4b8ab17a549ad66a2501
joamag <joamag@gmail.com>
2014-10-06 13:32:14 +0100

    new libxcb

---
+++ scripts/build/extras/libxcb.sh
@@ -0,0 +1,22 @@
+VERSION=${VERSION-1.11}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libxau" "xcb-proto"
+
+wget "http://xcb.freedesktop.org/dist/libxcb-$VERSION.tar.bz2"
+rm -rf libxcb-$VERSION && tar -jxf "libxcb-$VERSION.tar.bz2"
+rm -f "libxcb-$VERSION.tar.bz2"
+cd libxcb-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --sysconfdir=/etc\
+    --disable-static\
+    --enable-xinput
+
+make && make install
