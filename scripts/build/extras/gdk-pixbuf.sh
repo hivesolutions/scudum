c7911316f4adb7ade6d1fa3bd25cce01555c2af8
joamag <joamag@gmail.com>
2014-10-06 12:43:59 +0100

    new support

---
+++ scripts/build/extras/gdk-pixbuf.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-2.30.4}
+VERSION_L=${VERSION_L-2.30}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "glib" "libpng"
+
+wget "http://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/$VERSION_L/gdk-pixbuf-$VERSION.tar.xz"
+rm -rf gdk-pixbuf-$VERSION && tar -Jxf "gdk-pixbuf-$VERSION.tar.xz"
+rm -f "gdk-pixbuf-$VERSION.tar.xz"
+cd gdk-pixbuf-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
