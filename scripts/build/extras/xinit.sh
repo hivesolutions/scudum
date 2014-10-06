3d5ff20bdc62fe0c746b03ca4c7a7ca9369d0d9e
joamag <joamag@gmail.com>
2014-10-06 18:00:54 +0100

    new xinit

---
+++ scripts/build/extras/xinit.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-1.3.4}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://xorg.freedesktop.org/releases/individual/app/xinit-$VERSION.tar.bz2"
+rm -rf xinit-$VERSION && tar -jxf "xinit-$VERSION.tar.bz2"
+rm -f "xinit-$VERSION.tar.bz2"
+cd xinit-$VERSION
+
+./configure --prefix=$PREFIX --with-xinitdir=/etc/X11/app-defaults
+make && make install
