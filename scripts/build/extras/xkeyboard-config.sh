0534815ecb9c7a68a0cf7a647b8558fba6362fbb
joamag <joamag@gmail.com>
2014-10-06 18:36:16 +0100

    new support

--- scripts/build/extras/xkeyboard-config.sh
+++ scripts/build/extras/xkeyboard-config.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "intltool"
+depends "intltool" "xorg-libs"
 
 wget "http://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-$VERSION.tar.bz2"
 rm -rf xkeyboard-config-$VERSION && tar -jxf "xkeyboard-config-$VERSION.tar.bz2"
