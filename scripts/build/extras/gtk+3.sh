301a0283df521891506b7de2dfc5cbe2bc0be1ce
joamag <joamag@gmail.com>
2014-10-06 16:00:45 +0100

    new naming

--- scripts/build/extras/gtk+3.sh
+++ scripts/build/extras/gtk+3.sh
@@ -7,7 +7,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "pango" "atk" "gdk-pixbuf"
+depends "pango" "atk" "gdk-pixbuf" "xorg-libs"
 
 wget "http://ftp.gnome.org/pub/gnome/sources/gtk+/$VERSION_L/gtk+-$VERSION.tar.xz"
 rm -rf gtk+-$VERSION && tar -Jxf "gtk+-$VERSION.tar.xz"
