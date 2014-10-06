fd2c4b9275b5d79f75ad547991b70d108774e203
joamag <joamag@gmail.com>
2014-10-06 19:26:15 +0100

    new xclock

---
+++ scripts/build/extras/xclock.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-1.0.7}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "xorg-libs"
+
+wget "http://xorg.freedesktop.org/releases/individual/app/xclock-$VERSION.tar.bz2"
+rm -rf xclock-$VERSION && tar -jxf "xclock-$VERSION.tar.bz2"
+rm -f "xclock-$VERSION.tar.bz2"
+cd xclock-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
