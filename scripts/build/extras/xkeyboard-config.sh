f0eaaff031f6e41f78f5299ec8623db935fbf5d3
joamag <joamag@gmail.com>
2014-10-06 18:36:03 +0100

    new code support

--- scripts/build/extras/xkeyboard-config.sh
+++ scripts/build/extras/xkeyboard-config.sh
@@ -13,5 +13,5 @@ rm -rf xkeyboard-config-$VERSION && tar -jxf "xkeyboard-config-$VERSION.tar.bz2"
 rm -f "xkeyboard-config-$VERSION.tar.bz2"
 cd xkeyboard-config-$VERSION
 
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --with-xkb-rules-symlink=xorg
 make && make install
