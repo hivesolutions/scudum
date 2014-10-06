848a82606051cb189d7ceb82ff54b64c6ebe7bcb
joamag <joamag@gmail.com>
2014-10-06 13:07:43 +0100

    new dependency

--- scripts/build/extras/xcb-proto.sh
+++ scripts/build/extras/xcb-proto.sh
@@ -6,6 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
+depends "python"
+
 wget "http://xcb.freedesktop.org/dist/xcb-proto-$VERSION.tar.bz2"
 rm -rf xcb-proto-$VERSION && tar -jxf "xcb-proto-$VERSION.tar.bz2"
 rm -f "xcb-proto-$VERSION.tar.bz2"
