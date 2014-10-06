3a62464d0f982a79e4c6e45f4047e5d71de238cc
joamag <joamag@gmail.com>
2014-10-06 16:23:52 +0100

    new xorg server

---
+++ scripts/build/extras/xorg-server.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-1.15.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://xorg.freedesktop.org/archive/individual/xserver/xorg-server-$VERSION.tar.bz2"
+rm -rf xorg-server-$VERSION && tar -jxf "xorg-server-$VERSION.tar.bz2"
+rm -f "xorg-server-$VERSION.tar.bz2"
+cd xorg-server-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
