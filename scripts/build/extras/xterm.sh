191c0ac1415a346b75bf9cc7d29f4ab26145f729
joamag <joamag@gmail.com>
2014-10-06 19:24:41 +0100

    new xterm support

---
+++ scripts/build/extras/xterm.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-312}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "xorg-applications"
+
+wget "ftp://invisible-island.net/xterm/xterm-$VERSION.tgz"
+rm -rf xterm-$VERSION && tar -zxf "xterm-$VERSION.tgz"
+rm -f "xterm-$VERSION.tgz"
+cd xterm-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
