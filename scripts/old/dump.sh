2cf1722cbd07eef57575b420387eaba35dfa04f5
joamag <joamag@gmail.com>
2014-10-06 12:29:39 +0100

    removed old scirpts

--- scripts/old/dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-
-DEV_NAME=${DEV_NAME-/dev/null}
-FILE=${FILE-scudum.img}
-BUFFER=${BUFFER-1M}
-
-dd bs=$BUFFER if=$FILE of=$DEV_NAME
--- scripts/old/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -*- coding: utf-8 -*-
-
-DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-export PATH=$DIR:$PATH
-bash
-exit $?
