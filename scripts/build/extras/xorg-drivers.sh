607ff7f9b2c66f1a4579391b045b96551dde4515
joamag <joamag@gmail.com>
2014-10-07 12:55:00 +0100

    new vesa support

--- scripts/build/extras/xorg-drivers.sh
+++ scripts/build/extras/xorg-drivers.sh
@@ -1,5 +1,6 @@
 VERSION_EVDEV=${VERSION_EVDEV-2.9.0}
-VERSION_SYNAPTICS-=${VERSION_SYNAPTICS-1.8.1}
+VERSION_SYNAPTICS=${VERSION_SYNAPTICS-1.8.1}
+VERSION_VESA=${VERSION_VESA-2.3.3}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -21,6 +22,14 @@ wget "http://xorg.freedesktop.org/archive/individual/driver/xf86-input-synaptics
 rm -rf xf86-input-synaptics-$VERSION_SYNAPTICS && tar -jxf "xf86-input-synaptics-$VERSION_SYNAPTICS.tar.bz2"
 rm -f "xf86-input-synaptics-$VERSION_SYNAPTICS.tar.bz2"
 cd xf86-input-synaptics-$VERSION_SYNAPTICS
+
+./configure --prefix=$PREFIX
+make && make install
+
+wget "http://xorg.freedesktop.org/archive/individual/driver/xf86-video-vesa-$VERSION_VESA.tar.bz2"
+rm -rf xf86-video-vesa-$VERSION_VESA && tar -jxf "xf86-video-vesa-$VERSION_VESA.tar.bz2"
+rm -f "xf86-video-vesa-$VERSION_VESA.tar.bz2"
+cd xf86-video-vesa-$VERSION_VESA
 
 ./configure --prefix=$PREFIX
 make && make install
