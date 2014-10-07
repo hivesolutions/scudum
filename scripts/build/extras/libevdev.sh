cfba9b7211b8d2f271313c4607dbb9eb6ff75023
joamag <joamag@gmail.com>
2014-10-07 11:56:39 +0100

    new libevdev

---
+++ scripts/build/extras/libevdev.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-1.3}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://www.freedesktop.org/software/libevdev/libevdev-$VERSION.tar.xz"
+rm -rf libevdev-$VERSION && tar -Jxf "libevdev-$VERSION.tar.xz"
+rm -f "libevdev-$VERSION.tar.xz"
+cd libevdev-$VERSION.tar.xz
+
+./configure --prefix=$PREFIX
+make && make install
