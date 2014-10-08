330edcfa49bde3949f8726dcc00509ad44bad41f
joamag <joamag@gmail.com>
2014-10-08 10:30:07 +0100

    new libva

---
+++ scripts/build/extras/libva.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-1.4.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "mesa"
+
+wget "http://www.freedesktop.org/software/vaapi/releases/libva/libva-$VERSION.tar.bz2"
+rm -rf libva-$VERSION && tar -jxf "libva-$VERSION.tar.bz2"
+rm -f "libva-$VERSION.tar.bz2"
+cd libva-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
