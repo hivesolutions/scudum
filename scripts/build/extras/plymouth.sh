b2fbb6f7d00764936fedf15477123d0075576d62
joamag <joamag@gmail.com>
2014-10-14 19:14:33 +0100

    new plymouth

--- scripts/build/extras/plymouth.sh
+++ scripts/build/extras/plymouth.sh
@@ -13,9 +13,6 @@ rm -rf plymouth-$VERSION && tar -jxf "plymouth-$VERSION.tar.bz2"
 rm -f "plymouth-$VERSION.tar.bz2"
 cd plymouth-$VERSION
 
-./configure\
-    --prefix=$PREFIX\
-    --sysconfdir=/etc\
-    --disable-documentation
+./configure --prefix=$PREFIX --sysconfdir=/etc --disable-documentation
 
 make && make install
