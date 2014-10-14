98b6ae9db03cd6fb8c80b4f1f52ce3571636e5a7
joamag <joamag@gmail.com>
2014-10-14 17:14:44 +0100

    new gparted

---
+++ scripts/build/extras/gparted.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-0.19.1}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "parted"
+
+wget "http://downloads.sourceforge.net/gparted/gparted-$VERSION.tar.bz2"
+rm -rf gparted-$VERSION && tar -jxf "gparted-$VERSION.tar.bz2"
+rm -f "gparted-$VERSION.tar.bz2"
+cd gparted-$VERSION
+
+./configure --prefix=$PREFIX --disable-doc --disable-static
+make && make install
