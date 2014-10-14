9648aa01abfcc31d3d7c0a0d38dc658f849cf49d
joamag <joamag@gmail.com>
2014-10-14 16:26:29 +0100

    new qemu

--- scripts/build/extras/qemu.sh
+++ scripts/build/extras/qemu.sh
@@ -1,4 +1,5 @@
 VERSION=${VERSION-2.1.2}
+TARGET_LIST=${TARGET_LIST-x86_64-softmmu,x86_64-linux-user}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -13,5 +14,5 @@ rm -rf qemu-$VERSION && tar -jxf "qemu-$VERSION.tar.bz2"
 rm -f "qemu-$VERSION.tar.bz2"
 cd qemu-$VERSION
 
-./configure --prefix=$PREFIX --sysconfdir=/etc
+./configure --prefix=$PREFIX --target-list=$TARGET_LIST --sysconfdir=/etc 
 make && make install
