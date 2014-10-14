f8e418fdcc3014f3f5956ee64bbb983b006ab877
joamag <joamag@gmail.com>
2014-10-14 22:38:49 +0100

    new library

---
+++ scripts/build/extras/dbus-glib.sh
@@ -0,0 +1,21 @@
+VERSION=${VERSION-0.102}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "dbus" "glib"
+
+wget "http://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-$VERSION.tar.gz"
+rm -rf dbus-glib-$VERSION.tar.gz && tar -zxf "dbus-glib-$VERSION.tar.gz"
+rm -f "dbus-glib-$VERSION.tar.gz"
+cd dbus-glib-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --sysconfdir=/etc\
+    --disable-static
+
+make && make install
