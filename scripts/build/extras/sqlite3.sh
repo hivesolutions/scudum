433e9ae749fe94ca0642844fcd192e7dd4188923
joamag <joamag@gmail.com>
2014-10-14 17:44:38 +0100

    new sqlite compilation

--- scripts/build/extras/sqlite3.sh
+++ scripts/build/extras/sqlite3.sh
@@ -6,6 +6,11 @@ set -e +h
 
 source $DIR/common.sh
 
+export CFLAGS="$CFLAGS -DSQLITE_ENABLE_FTS3=1\
+    -DSQLITE_ENABLE_COLUMN_METADATA=1\
+    -DSQLITE_ENABLE_UNLOCK_NOTIFY=1\
+    -DSQLITE_SECURE_DELETE=1"
+
 wget "http://www.sqlite.org/2014/sqlite-autoconf-$VERSION.tar.gz"
 rm -rf sqlite-autoconf-$VERSION && tar -zxf "sqlite-autoconf-$VERSION.tar.gz"
 rm -f "sqlite-autoconf-$VERSION.tar.gz"
