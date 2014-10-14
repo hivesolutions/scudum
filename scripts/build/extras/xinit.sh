f6b1bab4353e9ebb0e2f51912ea4d19d1fa7025b
joamag <joamag@gmail.com>
2014-10-14 13:29:45 +0100

    new xinit

--- scripts/build/extras/xinit.sh
+++ scripts/build/extras/xinit.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "xorg-server" "xterm" "xclock" "twm"
+depends "xorg-server" "xorg-drivers" "xterm" "xclock" "twm"
 
 wget "http://xorg.freedesktop.org/releases/individual/app/xinit-$VERSION.tar.bz2"
 rm -rf xinit-$VERSION && tar -jxf "xinit-$VERSION.tar.bz2"
