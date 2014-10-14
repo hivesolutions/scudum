4ceb6aa2fa632f2ed417f96321d88ee020b86b04
joamag <joamag@gmail.com>
2014-10-14 23:25:55 +0100

    new gstreamer plugins

---
+++ scripts/build/extras/gstreamer-plugins.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-1.4.3}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "gstreamer" "pango"
+
+wget "http://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-$VERSION.tar.xz"
+rm -rf gst-plugins-base-$VERSION && tar -Jxf "gst-plugins-base-$VERSION.tar.xz"
+rm -f "gst-plugins-base-$VERSION.tar.xz"
+cd gst-plugins-base-$VERSION
+
+./configure --prefix=$PREFIX --disable-static
+make && make install
