041e004f8c530b515ce67a9ab07d60aeba05a85d
joamag <joamag@gmail.com>
2014-10-14 15:47:14 +0100

    initial qemu support

---
+++ scripts/build/extras/qemu.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.1.2}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "glib" "python" "xorg-server"
+
+wget "http://wiki.qemu.org/download/qemu-$VERSION.tar.bz2"
+rm -rf qemu-$VERSION && tar -jxf "qemu-$VERSION.tar.bz2"
+rm -f "qemu-$VERSION.tar.bz2"
+cd qemu-$VERSION
+
+./configure --prefix=$PREFIX --sysconfdir=/etc --target-list=x86_64-softmmu
+make && make install
