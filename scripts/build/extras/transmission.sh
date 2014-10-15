0292464003152fd138eabde4686b2a4a5161046d
joamag <joamag@gmail.com>
2014-10-15 10:48:51 +0100

    new transmission

---
+++ scripts/build/extras/transmission.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.84}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libevent"
+
+wget "http://download.transmissionbt.com/files/transmission-$VERSION.tar.xz"
+rm -rf transmission-$VERSION && tar -zxf "transmission-$VERSION.tar.xz"
+rm -f "transmission-$VERSION.tar.xz"
+cd transmission-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
