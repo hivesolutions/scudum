9e8fe63e73e5341a2183a57e73d8afcf70f0d0a2
joamag <joamag@gmail.com>
2014-10-15 10:46:39 +0100

    new libevent

---
+++ scripts/build/extras/libevent.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.0.21}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "https://github.com/downloads/libevent/libevent/libevent-$VERSION-stable.tar.gz"
+rm -rf libevent-$VERSION && tar -zxf "libevent-$VERSION-stable.tar.gz"
+rm -f "libevent-$VERSION-stable.tar.gz"
+cd libevent-$VERSION
+
+./configure --prefix=$PREFIX --disable-static
+make && make install
