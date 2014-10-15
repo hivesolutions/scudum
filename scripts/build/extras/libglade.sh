c37e36706b84ea417c6539e774770ff2a164450c
joamag <joamag@gmail.com>
2014-10-15 10:28:49 +0100

    new libgladde

---
+++ scripts/build/extras/libglade.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-1.6.13}
+VERSION_L=${VERSION_L-2.6}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://ftp.gnome.org/pub/gnome/sources/libglade/$VERSION_L/libglade-$VERSION.tar.bz2"
+rm -rf libglade-$VERSION && tar -jxf "libglade-$VERSION.tar.bz2"
+rm -f "libglade-$VERSION.tar.bz2"
+cd libglade-$VERSION
+
+./configure --prefix=$PREFIX --disable-static
+make && make install
