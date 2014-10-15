941e1e12267aadcf8ed49cc339378e940af653de
joamag <joamag@gmail.com>
2014-10-15 10:47:16 +0100

    new code

--- scripts/build/extras/libevent.sh
+++ scripts/build/extras/libevent.sh
@@ -7,9 +7,9 @@ set -e +h
 source $DIR/common.sh
 
 wget "https://github.com/downloads/libevent/libevent/libevent-$VERSION-stable.tar.gz"
-rm -rf libevent-$VERSION && tar -zxf "libevent-$VERSION-stable.tar.gz"
+rm -rf libevent-$VERSION-stable && tar -zxf "libevent-$VERSION-stable.tar.gz"
 rm -f "libevent-$VERSION-stable.tar.gz"
-cd libevent-$VERSION
+cd libevent-$VERSION-stable
 
 ./configure --prefix=$PREFIX --disable-static
 make && make install
