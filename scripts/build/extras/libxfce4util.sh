9d4a6b03df2ff43beb7a20b2d46a5c45828e9072
joamag <joamag@gmail.com>
2014-10-15 12:11:30 +0100

    new util library

---
+++ scripts/build/extras/libxfce4util.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-4.10.1}
+VERSION_L=${VERSION_L-4.10}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "glib"
+
+wget "http://archive.xfce.org/src/xfce/libxfce4ui/$VERSION_L/libxfce4util-$VERSION.tar.bz2"
+rm -rf libxfce4util-$VERSION && tar -jxf "libxfce4util-$VERSION.tar.bz2"
+rm -f "libxfce4util-$VERSION.tar.bz2"
+cd libxfce4util-$VERSION
+
+./configure --prefix=$PREFIX --sysconfdir=/etc
+make && make install
