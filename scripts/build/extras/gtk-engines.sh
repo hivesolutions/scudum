4d9391df9f1162ad420692caacc2e79491cb5a1d
joamag <joamag@gmail.com>
2014-10-15 11:48:39 +0100

    new gtk engines

---
+++ scripts/build/extras/gtk-engines.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-2.20.2}
+VERSION_L=${VERSION_L-2.20}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "gtk+2" "lua"
+
+wget "http://ftp.gnome.org/pub/gnome/sources/gtk-engines/$VERSION_L/gtk-engines-$VERSION.tar.bz2"
+rm -rf gtk-engines-$VERSION && tar -jxf "gtk-engines-$VERSION.tar.bz2"
+rm -f "gtk-engines-$VERSION.tar.bz2"
+cd gtk-engines-$VERSION
+
+./configure --prefix=$PREFIX --enable-lua
+make && make install
