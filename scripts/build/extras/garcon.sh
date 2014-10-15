1ede0d1ab29d05cafb3679e3c37d592b4e138c39
joamag <joamag@gmail.com>
2014-10-15 12:34:44 +0100

    new garcon lib

---
+++ scripts/build/extras/garcon.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-0.3.0}
+VERSION_L=${VERSION_L-0.3}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libxfce4ui" "libxfce4util" "uri"
+
+wget "http://archive.xfce.org/src/xfce/garcon/$VERSION_L/garcon-$VERSION.tar.bz2"
+rm -rf garcon-$VERSION && tar -jxf "garcon-$VERSION.tar.bz2"
+rm -f "garcon-$VERSION.tar.bz2"
+cd garcon-$VERSION
+
+./configure --prefix=$PREFIX --sysconfdir=/etc
+make && make install
