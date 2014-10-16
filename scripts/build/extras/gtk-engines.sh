d1c9dddb40eda8ee8d6e1efc86452b286fb1dd94
joamag <joamag@gmail.com>
2014-10-16 13:12:10 +0100

    new code

--- scripts/build/extras/gtk-engines.sh
+++ scripts/build/extras/gtk-engines.sh
@@ -7,7 +7,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "gtk+2" "lua"
+depends "gtk+2" "intltool" "lua"
 
 wget "http://ftp.gnome.org/pub/gnome/sources/gtk-engines/$VERSION_L/gtk-engines-$VERSION.tar.bz2"
 rm -rf gtk-engines-$VERSION && tar -jxf "gtk-engines-$VERSION.tar.bz2"
