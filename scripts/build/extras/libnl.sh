7f0c28b7dad35a809143f631bdaa330c0e3164e7
joamag <joamag@gmail.com>
2014-10-20 12:08:25 +0100

    new libnl

---
+++ scripts/build/extras/libnl.sh
@@ -0,0 +1,19 @@
+VERSION=${VERSION-3.2.25}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://www.carisma.slowglass.com/~tgr/libnl/files/libnl-$VERSION.tar.gz"
+rm -rf libnl-$VERSION && tar -zxf "libnl-$VERSION.tar.gz"
+rm -f "libnl-$VERSION.tar.gz"
+cd libnl-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --sysconfdir=/etc\
+    --disable-static
+
+make && make install
