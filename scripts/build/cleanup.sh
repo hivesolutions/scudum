8afb8129ece8d0b86e6acad66cab29a78e1a451f
joamag <joamag@gmail.com>
2014-11-11 13:51:40 +0000

    new umount

--- scripts/build/cleanup.sh
+++ scripts/build/cleanup.sh
@@ -7,10 +7,11 @@ set -e +h
 
 source $DIR/base/config.sh
 
-mountpoint -q $SCUDUM/sys && umount $SCUDUM/sys
-mountpoint -q $SCUDUM/proc && umount $SCUDUM/proc
-mountpoint -q $SCUDUM/dev/pts && umount $SCUDUM/dev/pts
-mountpoint -q $SCUDUM/dev && umount $SCUDUM/dev
+mountpoint -q $SCUDUM/sys && umount -v $SCUDUM/sys
+mountpoint -q $SCUDUM/proc && umount -v $SCUDUM/proc
+mountpoint -q $SCUDUM/dev/shm && umount -v $SCUDUM/dev/shm
+mountpoint -q $SCUDUM/dev/pts && umount -v $SCUDUM/dev/pts
+mountpoint -q $SCUDUM/dev && umount -v $SCUDUM/dev
 
 cd $SCUDUM/root
 find . ! -name ".bashrc" -delete
