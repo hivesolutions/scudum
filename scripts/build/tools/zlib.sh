915c2105c6e5393ee72eb2ad9c964e6e9fcbdca1
joamag <joamag@gmail.com>
2014-11-11 15:34:49 +0000

    new zlib config

---
+++ scripts/build/tools/zlib.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-1.2.7}
+
+set -e +h
+
+wget --no-check-certificate "http://zlib.net/fossils/zlib-$VERSION.tar.gz"
+rm -rf zlib-$VERSION && tar -zxf "zlib-$VERSION.tar.gz"
+rm -f "zlib-$VERSION.tar.gz"
+cd zlib-$VERSION
+
+./configure --prefix=$PREFIX
+
+make && make install
