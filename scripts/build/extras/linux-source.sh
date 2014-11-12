60a4e11b2ed7a5de944096e3914dcb56634e66d6
joamag <joamag@gmail.com>
2014-11-12 12:19:52 +0000

    new linux source support

---
+++ scripts/build/extras/linux-source.sh
@@ -0,0 +1,10 @@
+VERSION=${VERSION-3.17.1}
+
+set -e +h
+
+wget --no-check-certificate "https://www.kernel.org/pub/linux/kernel/v3.x/linux-$VERSION.tar.xz"
+rm -rf linux-$VERSION && tar -Jxf "linux-$VERSION.tar.xz"
+rm -f "linux-$VERSION.tar.xz"
+
+mkdir -p $PREFIX/src
+mv linux-$VERSION $PREFIX/src/$(uname -r)
