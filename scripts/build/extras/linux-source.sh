1990bffdfcdc99ff8e9ccec4bb524869f8502e42
joamag <joamag@gmail.com>
2014-11-12 12:38:35 +0000

    new linux source package

--- scripts/build/extras/linux-source.sh
+++ scripts/build/extras/linux-source.sh
@@ -5,6 +5,14 @@ set -e +h
 wget --no-check-certificate "https://www.kernel.org/pub/linux/kernel/v3.x/linux-$VERSION.tar.xz"
 rm -rf linux-$VERSION && tar -Jxf "linux-$VERSION.tar.xz"
 rm -f "linux-$VERSION.tar.xz"
+cd linux-$VERSION
+
+cp /boot/config .config
+
+make mrproper
+make modules_install INSTALL_MOD_PATH=modules_install
+
+cd ..
 
 mkdir -p $PREFIX/src
 mv linux-$VERSION $PREFIX/src/linux
