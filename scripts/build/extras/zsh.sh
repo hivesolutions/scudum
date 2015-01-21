5b3cfa57fd2e5bcf83fb435cff1c86bc68152a64
joamag <joamag@gmail.com>
2015-01-21 13:58:20 +0000

    new link

--- scripts/build/extras/zsh.sh
+++ scripts/build/extras/zsh.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "http://www.zsh.org/pub/zsh-$VERSION.tar.bz2"
+wget "http://www.zsh.org/pub/old/zsh-$VERSION.tar.bz2"
 rm -rf zsh-$VERSION && tar -jxf "zsh-$VERSION.tar.bz2"
 rm -f "zsh-$VERSION.tar.bz2"
 cd zsh-$VERSION
