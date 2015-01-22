a3ae2e7c071a7b77518b638c7be386b85bf7203c
joamag <joamag@gmail.com>
2015-01-22 08:52:39 +0000

    new version of m4

--- scripts/build/tools/m4.sh
+++ scripts/build/tools/m4.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.4.16}
+VERSION=${VERSION-1.4.17}
 
 set -e +h
 
@@ -7,6 +7,5 @@ rm -rf m4-$VERSION && tar -jxf "m4-$VERSION.tar.bz2"
 rm -f "m4-$VERSION.tar.bz2"
 cd m4-$VERSION
 
-sed -i -e '/gets is a/d' lib/stdio.in.h
 ./configure --prefix=$PREFIX
 make && make install
