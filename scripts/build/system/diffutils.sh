9132b14d770189f4bcd04c2df4306a3b6d5842ed
joamag <joamag@gmail.com>
2015-01-22 09:18:39 +0000

    new file packing

--- scripts/build/system/diffutils.sh
+++ scripts/build/system/diffutils.sh
@@ -2,9 +2,9 @@ VERSION=${VERSION-3.3}
 
 set -e +h
 
-wget --no-check-certificate "http://ftp.gnu.org/gnu/diffutils/diffutils-$VERSION.tar.gz"
-rm -rf diffutils-$VERSION && tar -zxf "diffutils-$VERSION.tar.gz"
-rm -f "diffutils-$VERSION.tar.gz"
+wget --no-check-certificate "http://ftp.gnu.org/gnu/diffutils/diffutils-$VERSION.tar.xz"
+rm -rf diffutils-$VERSION && tar -Jxf "diffutils-$VERSION.tar.xz"
+rm -f "diffutils-$VERSION.tar.xz"
 cd diffutils-$VERSION
 
 ./configure --prefix=/usr
--- scripts/build/tools/diffutils.sh
+++ scripts/build/tools/diffutils.sh
@@ -2,9 +2,9 @@ VERSION=${VERSION-3.3}
 
 set -e +h
 
-wget "http://ftp.gnu.org/gnu/diffutils/diffutils-$VERSION.tar.gz"
-rm -f "diffutils-$VERSION" && tar -zxf "diffutils-$VERSION.tar.gz"
-rm -f "diffutils-$VERSION.tar.gz"
+wget "http://ftp.gnu.org/gnu/diffutils/diffutils-$VERSION.tar.xz"
+rm -f "diffutils-$VERSION" && tar -Jxf "diffutils-$VERSION.tar.xz"
+rm -f "diffutils-$VERSION.tar.xz"
 cd diffutils-$VERSION
 
 ./configure --prefix=$PREFIX
