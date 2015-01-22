1633ac515863191866f65413324090dd1e97aba4
joamag <joamag@gmail.com>
2015-01-22 08:49:11 +0000

    new check version

--- scripts/build/tools/check.sh
+++ scripts/build/tools/check.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-0.9.9}
+VERSION=${VERSION-0.9.14}
 
 set -e +h
 
@@ -7,5 +7,5 @@ rm -f "check-$VERSION" && tar -zxf "check-$VERSION.tar.gz"
 rm -f "check-$VERSION.tar.gz"
 cd check-$VERSION
 
-CFLAGS="-L/STAGE1/lib -lpthread" ./configure --prefix=$PREFIX
+PKG_CONFIG= ./configure --prefix=$PREFIX
 make && make install
