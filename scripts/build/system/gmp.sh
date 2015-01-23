cb886d92c4712f685bb3ee5cb4f13f9f970acf7f
joamag <joamag@gmail.com>
2015-01-23 23:17:29 +0000

    reverted gmp version

--- scripts/build/system/gmp.sh
+++ scripts/build/system/gmp.sh
@@ -1,13 +1,12 @@
-VERSION=${VERSION-6.0.0}
-VERSION_F=${VERSION_F-a}
+VERSION=${VERSION-5.1.3}
 
 set -e +h
 
 export CFLAGS="$CFLAGS -O2 -pedantic -fomit-frame-pointer"
 
-wget --no-check-certificate "http://ftp.gnu.org/gnu/gmp/gmp-$VERSION$VERSION_F.tar.xz"
-rm -rf gmp-$VERSION && tar -Jxf "gmp-$VERSION$VERSION_F.tar.xz"
-rm -f "gmp-$VERSION$VERSION_F.tar.xz"
+wget --no-check-certificate "http://ftp.gnu.org/gnu/gmp/gmp-$VERSION.tar.xz"
+rm -rf gmp-$VERSION && tar -Jxf "gmp-$VERSION.tar.xz"
+rm -f "gmp-$VERSION.tar.xz"
 cd gmp-$VERSION
 
 ./configure --prefix=/usr --enable-cxx\
