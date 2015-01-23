83750836d94b1f2a9adfdaba1296e3bc145ccb0c
joamag <joamag@gmail.com>
2015-01-23 15:46:58 +0000

    removed extra spaces

--- scripts/build/base/config.sh
+++ scripts/build/base/config.sh
@@ -31,7 +31,7 @@ export MAKEFLAGS="-j $(nproc)"
 
 # exprts the flags theat define the deault optimization flags
 # for both the base c compiler and the c++ compiler so that the
-# resulting build is generic enough for proper handling 
+# resulting build is generic enough for proper handling
 export CFLAGS="-O2 -m64 -mtune=generic -march=x86-64"
 
 # exports the various gcc related flags that will be used to
