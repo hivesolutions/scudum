415e24501ac3f4b2ee4ba31ce54890e40f86cefc
joamag <joamag@gmail.com>
2015-02-03 19:09:56 +0000

    new pypy pip

---
+++ scripts/build/extras/pypy-pip.sh
@@ -0,0 +1,16 @@
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "pypy"
+
+if [ "$PREFIX" == "/usr" ]; then
+    ARGS=""
+else
+    ARGS="--user"
+fi
+
+rm -f get-pip.py && wget "https://bootstrap.pypa.io/get-pip.py"
+pypy get-pip.py $ARGS
