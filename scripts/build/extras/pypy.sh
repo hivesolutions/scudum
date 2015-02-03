29f4a2a34b260effc6ad9e1600ff3014a335bf81
joamag <joamag@gmail.com>
2015-02-03 19:06:48 +0000

    better pypy

--- scripts/build/extras/pypy.sh
+++ scripts/build/extras/pypy.sh
@@ -16,9 +16,12 @@ cd pypy-$VERSION-src
 cd pypy/goal
 python ../../rpython/bin/rpython -Ojit targetpypystandalone
 cd ../tool/release
-python package.py --targetdir pypy-$VERSION.tar.gz --archive-name pypy --without-tk
+python package.py --targetdir pypy-$VERSION.tar.bz2 --archive-name pypy --without-tk
 
 mv pypy-$VERSION.tar.gz $PREFIX/lib
 cd $PREFIX/lib
 
-rm -rf pypy && tar -zxvf pypy-$VERSION.tar.gz
+rm -rf pypy && tar -jxvf pypy-$VERSION.tar.bz2
+rm -f pypy-$VERSION.tar.gz
+
+ln -s ../lib/pypy/bin/pypy /usr/bin/pypy
