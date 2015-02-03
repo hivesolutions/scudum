793577e5ce761c34d6387627161e56894bc21962
joamag <joamag@gmail.com>
2015-02-03 16:56:59 +0000

    new lib ev

---
+++ scripts/build/extras/libev.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-4.19}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://dist.schmorp.de/libev/Attic/libev-$VERSION.tar.gz"
+rm -rf libev-$VERSION && tar -zxf "libev-$VERSION.tar.gz"
+rm -f "libev-$VERSION.tar.gz"
+cd libev-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
