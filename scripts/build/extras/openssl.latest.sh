02a41213493842c36f91d7fdf805baaa0bde9e13
joamag <joamag@gmail.com>
2015-02-04 22:17:59 +0000

    new openssl latest

---
+++ scripts/build/extras/openssl.latest.sh
@@ -0,0 +1,23 @@
+VERSION=${VERSION-1.0.1l}
+
+set -e +h
+
+unset MAKEFLAGS
+
+wget --no-check-certificate "https://www.openssl.org/source/openssl-$VERSION.tar.gz"
+rm -rf openssl-$VERSION && tar -zxf "openssl-$VERSION.tar.gz"
+rm -f "openssl-$VERSION.tar.gz"
+cd openssl-$VERSION
+
+cat > openssl.ld  << "EOF"
+OPENSSL_1.0.0 {
+    global:
+    *;
+};
+EOF
+
+./config shared --prefix=/usr --openssldir=/usr/ssl\
+    -Wl,--version-script=openssl.ld -Wl,-Bsymbolic-functions
+make && make install
+
+ln -sv /usr/ssl /etc/ssl
