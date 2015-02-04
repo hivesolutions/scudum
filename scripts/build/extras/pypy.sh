6469cea5603039d22b27c553b3009b4eadfd1bf2
joamag <joamag@gmail.com>
2015-02-04 14:00:34 +0000

    new bzip support

--- scripts/build/extras/pypy.sh
+++ scripts/build/extras/pypy.sh
@@ -18,10 +18,10 @@ python ../../rpython/bin/rpython -Ojit targetpypystandalone
 cd ../tool/release
 python package.py --targetdir pypy-$VERSION.tar.bz2 --archive-name pypy --without-tk || true
 
-mv -v pypy-$VERSION.tar.gz $PREFIX/lib
+mv -v pypy-$VERSION.tar.bz2 $PREFIX/lib
 cd $PREFIX/lib
 
 rm -rvf pypy && tar -jxvf pypy-$VERSION.tar.bz2
-rm -vf pypy-$VERSION.tar.gz
+rm -vf pypy-$VERSION.tar.bz2
 
 ln -sv ../lib/pypy/bin/pypy $PREFIX/bin/pypy
