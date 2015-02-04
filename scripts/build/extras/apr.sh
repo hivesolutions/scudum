ec1891b66678f66b0952e6ab53e0dfe64cbf61c2
joamag <joamag@gmail.com>
2015-02-04 16:29:46 +0000

    older version

--- scripts/build/extras/apr.sh
+++ scripts/build/extras/apr.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.5.1}
+VERSION=${VERSION-1.5.0}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -12,6 +12,4 @@ rm -f "apr-$VERSION.tar.gz"
 cd apr-$VERSION
 
 ./configure --prefix=$PREFIX
-make
-chmod +x tools/gen_test_char
-make install
+make && make install
