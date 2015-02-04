e820581d782d8b6662749432a0e4b2326590f797
joamag <joamag@gmail.com>
2015-02-04 21:45:26 +0000

    new ntp installation

---
+++ scripts/build/extras/ntp.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-4.2.8p1}
+VERSION_M=${VERSION_M-4.2}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://archive.ntp.org/ntp4/ntp-$VERSION_M/ntp-$VERSION.tar.gz"
+rm -rf ntp-$VERSION && tar -zxf "ntp-$VERSION.tar.gz"
+rm -f "ntp-$VERSION.tar.gz"
+cd ntp-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
