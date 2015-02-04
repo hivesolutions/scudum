fb1fafbad1cc6c91f746ca2e2ac2a35e745c215c
joamag <joamag@gmail.com>
2015-02-04 22:43:10 +0000

    new built pypy

---
+++ scripts/build/extras/pypy.built.sh
@@ -0,0 +1,20 @@
+VERSION=${VERSION-2.4.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "expat" "ffi"
+
+wget "https://bitbucket.org/pypy/pypy/downloads/pypy-$VERSION-linux64.tar.bz2"
+rm -rf pypy-$VERSION-linux64 && tar -jxf "pypy-$VERSION-linux64.tar.bz2"
+
+mv -v pypy-$VERSION-linux64.tar.bz2 $PREFIX/lib
+cd $PREFIX/lib
+
+rm -rvf pypy && tar -jxf pypy-$VERSION-linux64.tar.bz2
+rm -vf pypy-$VERSION-linux64.tar.bz2
+
+ln -sv ../lib/pypy/bin/pypy $PREFIX/bin/pypy
