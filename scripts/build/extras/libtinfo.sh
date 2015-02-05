d84552b7ae833413c5a7a98d874c97af0e958c39
joamag <joamag@gmail.com>
2015-02-05 09:53:37 +0000

    initial support for libtinfo

---
+++ scripts/build/extras/libtinfo.sh
@@ -0,0 +1,22 @@
+VERSION=${VERSION-5.9}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+wget --no-check-certificate "ftp://ftp.gnu.org/gnu/ncurses/ncurses-$VERSION.tar.gz"
+rm -rf ncurses-$VERSION && tar -zxf "ncurses-$VERSION.tar.gz"
+rm -f "ncurses-$VERSION.tar.gz"
+cd ncurses-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --mandir=$PREFIX/share/man\
+    --with-shared\
+    --with-termlib\
+    --without-debug\
+    --enable-pc-files
+
+make
+    
+cp -av lib/libtinfo*.so.5* /usr/lib
