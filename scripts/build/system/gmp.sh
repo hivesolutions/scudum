55ee877ed3aa1850c708f2ca65044280a3584e29
joamag <joamag@gmail.com>
2015-02-06 16:13:40 +0000

    new code

--- scripts/build/system/gmp.sh
+++ scripts/build/system/gmp.sh
@@ -3,6 +3,7 @@ VERSION=${VERSION-6.0.0}
 set -e +h
 
 export CFLAGS="$CFLAGS -O2 -pedantic -fomit-frame-pointer -march=$SCUDUM_MARCH -mtune=generic"
+export GMP_CPU_TYPE="$SCUDUM_MARCH"
 
 wget --no-check-certificate "https://gmplib.org/download/gmp/gmp-$VERSION.tar.xz"
 rm -rf gmp-$VERSION && tar -Jxf "gmp-$VERSION.tar.xz"
@@ -10,7 +11,7 @@ rm -f "gmp-$VERSION.tar.xz"
 cd gmp-$VERSION
 
 ./configure --prefix=/usr --enable-cxx\
-    --docdir=/usr/share/doc/gmp-$VERSION
+    --enable-fake-cpuid --docdir=/usr/share/doc/gmp-$VERSION
 
 make
 
