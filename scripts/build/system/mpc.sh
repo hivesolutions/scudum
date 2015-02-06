33d01ab61df252f0956799bdd41c88f345fcfa3d
joamag <joamag@gmail.com>
2015-02-06 20:44:33 +0000

    removed extra cflags

--- scripts/build/system/mpc.sh
+++ scripts/build/system/mpc.sh
@@ -2,8 +2,6 @@ VERSION=${VERSION-1.0.2}
 
 set -e +h
 
-export CFLAGS="$CFLAGS -O2 -pedantic -fomit-frame-pointer -march=$SCUDUM_MARCH -mtune=generic"
-
 wget --no-check-certificate "http://www.multiprecision.org/mpc/download/mpc-$VERSION.tar.gz"
 rm -rf mpc-$VERSION && tar -zxf "mpc-$VERSION.tar.gz"
 rm -f "mpc-$VERSION.tar.gz"
