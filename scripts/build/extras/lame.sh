d5998c2772137bf411dbaf16b34afd17559cd489
joamag <joamag@gmail.com>
2015-02-07 15:55:06 +0000

    new lame encoder

---
+++ scripts/build/extras/lame.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-3.99.5}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "nasm"
+
+wget "http://downloads.sourceforge.net/lame/lame-$VERSION.tar.gz"
+rm -rf lame-$VERSION && tar -zxf "lame-$VERSION.tar.gz"
+rm -f "lame-$VERSION.tar.gz"
+cd lame-$VERSION
+
+./configure --prefix=$PREFIX --enable-mp3rtp --disable-static
+make && make install
