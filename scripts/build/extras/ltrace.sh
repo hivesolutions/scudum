b9108c34644bb00a9ed2cc5df6793d1d4d3bfa85
joamag <joamag@gmail.com>
2015-02-07 18:37:27 +0000

    small code fix

--- scripts/build/extras/ltrace.sh
+++ scripts/build/extras/ltrace.sh
@@ -12,7 +12,7 @@ export CXXFLAGS="-Wno-unused-local-typedefs"
 depends "libelf"
 
 wget "http://ltrace.org/ltrace_$VERSION.orig.tar.bz2"
-rm -rf ltrace_$VERSION && tar -zxf "ltrace_$VERSION.orig.tar.bz2"
+rm -rf ltrace_$VERSION && tar -jxf "ltrace_$VERSION.orig.tar.bz2"
 rm -f "ltrace_$VERSION.orig.tar.bz2"
 cd ltrace_$VERSION
 
