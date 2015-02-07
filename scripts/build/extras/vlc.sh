d65f26b73fd0ace911316d6cbb22060f575bc705
joamag <joamag@gmail.com>
2015-02-07 16:26:56 +0000

    new vlc

---
+++ scripts/build/extras/vlc.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.1.5}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "alsa" "ffmpeg" "liba52" "libgcrypt" "lua" "x11"
+
+wget "http://download.videolan.org/vlc/$VERSION/vlc-$VERSION.tar.xz"
+rm -rf vlc-$VERSION && tar -Jxf "vlc-$VERSION.tar.xz"
+rm -f "vlc-$VERSION.tar.xz"
+cd vlc-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
