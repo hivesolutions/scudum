fb49d5761d02daf32e362d3b0e54c1a4cd499be0
joamag <joamag@gmail.com>
2015-02-07 15:51:40 +0000

    new libass

---
+++ scripts/build/extras/libass.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-0.12.1}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "yasm"
+
+wget "https://github.com/libass/libass/releases/download/$VERSION/libass-$VERSION.tar.xz"
+rm -rf libass-$VERSION && tar -Jxf "libass-$VERSION.tar.xz"
+rm -f "libass-$VERSION.tar.xz"
+cd libass-$VERSION
+
+./configure --prefix=$PREFIX --disable-static
+make && make install
