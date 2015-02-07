2e14dd1ea8de2c284cb9e58a7e8a0570bd924c23
joamag <joamag@gmail.com>
2015-02-07 15:58:43 +0000

    small code fix

--- scripts/build/extras/freebidi.sh
+++ scripts/build/extras/freebidi.sh
@@ -9,7 +9,7 @@ source $DIR/common.sh
 depends "glib"
 
 wget "http://fribidi.org/download/fribidi-$VERSION.tar.bz2"
-rm -rf fribidi-$VERSION && tar -zxf "fribidi-$VERSION.tar.bz2"
+rm -rf fribidi-$VERSION && tar -jxf "fribidi-$VERSION.tar.bz2"
 rm -f "fribidi-$VERSION.tar.bz2"
 cd fribidi-$VERSION
 
