0d96b846df1b697f4cc7ec27e25da53c6102dae3
joamag <joamag@gmail.com>
2015-02-07 16:16:59 +0000

    new liba52

---
+++ scripts/build/extras/liba52.sh
@@ -0,0 +1,20 @@
+VERSION=${VERSION-0.7.4}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://liba52.sourceforge.net/files/a52dec-$VERSION.tar.gz"
+rm -rf a52dec-$VERSION && tar -zxf "a52dec-$VERSION.tar.gz"
+rm -f "a52dec-$VERSION.tar.gz"
+cd a52dec-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --mandir=$PREFIX/share/man\
+    --enable-shared\
+    --disable-static
+
+make && make install
