ea555a841e2e716db55f923c562bcd5a38faaba9
joamag <joamag@gmail.com>
2015-02-07 16:03:30 +0000

    new dependency

--- scripts/build/extras/libass.sh
+++ scripts/build/extras/libass.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "freetype" "freebidi"
+depends "freetype" "freebidi" "fontconfig"
 
 wget "https://github.com/libass/libass/releases/download/$VERSION/libass-$VERSION.tar.xz"
 rm -rf libass-$VERSION && tar -Jxf "libass-$VERSION.tar.xz"
