05d37629bae567b48dff74f58bff36b916039c2c
joamag <joamag@gmail.com>
2015-02-07 17:15:41 +0000

    new enable as root

--- scripts/build/extras/vlc.sh
+++ scripts/build/extras/vlc.sh
@@ -6,12 +6,12 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "alsa" "ffmpeg" "liba52" "libgcrypt" "libmad" "lua" "x11"
+depends "alsa" "ffmpeg" "liba52" "libgcrypt" "libmad" "x11"
 
 wget "http://download.videolan.org/vlc/$VERSION/vlc-$VERSION.tar.xz"
 rm -rf vlc-$VERSION && tar -Jxf "vlc-$VERSION.tar.xz"
 rm -f "vlc-$VERSION.tar.xz"
 cd vlc-$VERSION
 
-./configure --prefix=$PREFIX --disable-lua
+./configure --prefix=$PREFIX --enable-run-as-root --disable-lua
 make && make install
