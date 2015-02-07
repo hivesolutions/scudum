8f87ae682aaad68f063f871045cb68a89859541c
joamag <joamag@gmail.com>
2015-02-07 19:49:02 +0000

    new dependency

--- scripts/build/extras/crosstool-ng.sh
+++ scripts/build/extras/crosstool-ng.sh
@@ -6,6 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
+depends "gperf"
+
 wget "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-$VERSION.tar.bz2"
 rm -rf crosstool-ng-$VERSION && tar -jxf "crosstool-ng-$VERSION.tar.bz2"
 rm -f "crosstool-ng-$VERSION.tar.bz2"
