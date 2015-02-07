cb1083e99beb461b2c4c85ca676a9c48473b3ad8
joamag <joamag@gmail.com>
2015-02-07 15:41:45 +0000

    new ffmpeg

---
+++ scripts/build/extras/ffmpeg.sh
@@ -0,0 +1,32 @@
+VERSION=${VERSION-2.5.3}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://ffmpeg.org/releases/ffmpeg-$VERSION.tar.bz2"
+rm -rf ffmpeg-$VERSION && tar -jxf "ffmpeg-$VERSION.tar.bz2"
+rm -f "ffmpeg-$VERSION.tar.bz2"
+cd ffmpeg-$VERSION
+
+./configure\
+    --prefix=$PREFIX
+    --enable-gpl\
+    --enable-version3\
+    --enable-nonfree\
+    --disable-static\
+    --enable-shared\
+    --disable-debug\
+    --enable-libass\
+    --enable-libfdk-aac\
+    --enable-libmp3lame\
+    --enable-libtheora\
+    --enable-libvorbis\
+    --enable-libvpx\
+    --enable-libx264\
+    --enable-x11grab\
+    --docdir=$PREFIX/share/doc/ffmpeg-$VERSION
+
+make && make install
