47aecaba27743162b2f48cbddc4be07a2c7e0a9a
joamag <joamag@gmail.com>
2015-02-09 13:35:24 +0000

    new glew

---
+++ scripts/build/extras/glew.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-1.12.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "x11"
+
+wget "http://downloads.sourceforge.net/glew/glew/glew-$VERSION.tgz"
+rm -rf glew-$VERSION && tar -zxf "glew-$VERSION.tgz"
+rm -f "glew-$VERSION.tar.xz"
+cd glew-$VERSION
+
+GLEW_DEST=$PREFIX make && GLEW_DEST=$PREFIX make install
