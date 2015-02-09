956f48b2f8b61d0de15cf1672bdd536cd446ee71
joamag <joamag@gmail.com>
2015-02-09 13:39:51 +0000

    new glu lib

---
+++ scripts/build/extras/glu.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-9.0.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "mesa"
+
+wget "ftp://ftp.freedesktop.org/pub/mesa/glu/glu-$VERSION.tar.gz"
+rm -rf glu-$VERSION && tar -zxf "glu-$VERSION.tar.gz"
+rm -f "glu-$VERSION.tar.gz"
+cd glu-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
