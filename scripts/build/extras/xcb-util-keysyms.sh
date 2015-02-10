85dfcc99a34c4fd9022555170b7867b2c0bb4f5a
joamag <joamag@gmail.com>
2015-02-10 13:12:03 +0000

    new keysms

---
+++ scripts/build/extras/xcb-util-keysyms.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-0.4.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libxcb"
+
+wget "http://xcb.freedesktop.org/dist/xcb-util-keysyms-$VERSION.tar.bz2"
+rm -rf xcb-util-keysyms-$VERSION && tar -zxf "xcb-util-keysyms-$VERSION.tar.bz2"
+rm -f "xcb-util-keysyms-$VERSION.tar.bz2"
+cd xcb-util-keysyms-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
