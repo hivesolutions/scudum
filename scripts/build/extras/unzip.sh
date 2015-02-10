b73b9e0f8f6629620c6f775d65ba39bcd489d552
joamag <joamag@gmail.com>
2015-02-10 15:51:46 +0000

    temp change

--- scripts/build/extras/unzip.sh
+++ scripts/build/extras/unzip.sh
@@ -6,8 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "http://downloads.sourceforge.net/infozip/unzip$VERSION.tar.gz"\
-    "http://fossies.org/linux/misc/unzip$VERSION.tar.gz"
+rget "http://fossies.org/linux/misc/unzip$VERSION.tar.gz"
 rm -rf unzip$VERSION && tar -zxf "unzip$VERSION.tar.gz"
 rm -f "unzip$VERSION.tar.gz"
 cd unzip$VERSION
