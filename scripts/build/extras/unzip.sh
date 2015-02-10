6a24fdbabd031024f6af4f3121c7d450c751625d
joamag <joamag@gmail.com>
2015-02-10 15:52:11 +0000

    new reverted change

--- scripts/build/extras/unzip.sh
+++ scripts/build/extras/unzip.sh
@@ -6,7 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "http://fossies.org/linux/misc/unzip$VERSION.tar.gz"
+rget "http://downloads.sourceforge.net/infozip/unzip$VERSION.tar.gz"\
+    "http://fossies.org/linux/misc/unzip$VERSION.tar.gz"
 rm -rf unzip$VERSION && tar -zxf "unzip$VERSION.tar.gz"
 rm -f "unzip$VERSION.tar.gz"
 cd unzip$VERSION
