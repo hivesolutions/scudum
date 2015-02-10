c750f19705da4c5d2dbcc6c0e5d07400a086d747
joamag <joamag@gmail.com>
2015-02-10 16:32:02 +0000

    new rget

--- scripts/build/extras/libmad.sh
+++ scripts/build/extras/libmad.sh
@@ -6,8 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "ftp://ftp.be.netbsd.org/pub/pkgsrc/distfiles/libmad-$VERSION.tar.gz"\
-    "http://downloads.sourceforge.net/mad/libmad-$VERSION.tar.gz"
+rget "http://downloads.sourceforge.net/mad/libmad-$VERSION.tar.gz"\
+    "ftp://ftp.be.netbsd.org/pub/pkgsrc/distfiles/libmad-$VERSION.tar.gz"
 rm -rf libmad-$VERSION && tar -zxf "libmad-$VERSION.tar.gz"
 rm -f "libmad-$VERSION.tar.gz"
 cd libmad-$VERSION
