974338aded8b5fafe36c49dbf9c1678116eaf4af
joamag <joamag@gmail.com>
2015-02-10 10:05:18 +0000

    new gstreamer lib av

---
+++ scripts/build/extras/gstreamer-libav.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-1.4.5}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "gstreamer-plugins"
+
+wget "http://gstreamer.freedesktop.org/src/gst-libav/gst-libav-$VERSION.tar.xz"
+rm -rf gst-libav-$VERSION && tar -Jxf "gst-libav-$VERSION.tar.xz"
+rm -f "gst-libav-$VERSION.tar.xz"
+cd gst-libav-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
