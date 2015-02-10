6ec60ae74937f75d26f1aeeb043d740f5661605d
joamag <joamag@gmail.com>
2015-02-10 16:30:39 +0000

    new code

--- scripts/build/extras/libmad.sh
+++ scripts/build/extras/libmad.sh
@@ -6,8 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "http://downloads.sourceforge.net/mad/libmad-$VERSION.tar.gz"\
-    "ftp://ftp.fi.netbsd.org/ftp/index/FreeBSD/distfiles/libmad-$VERSION"
+rget "ftp://ftp.fi.netbsd.org/ftp/index/FreeBSD/distfiles/libmad-$VERSION"\
+    "http://downloads.sourceforge.net/mad/libmad-$VERSION.tar.gz"
 rm -rf libmad-$VERSION && tar -zxf "libmad-$VERSION.tar.gz"
 rm -f "libmad-$VERSION.tar.gz"
 cd libmad-$VERSION
