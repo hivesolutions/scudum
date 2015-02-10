29a036cbe2a045d23bb52920bf3bad0ed32e90c7
joamag <joamag@gmail.com>
2015-02-10 09:55:05 +0000

    new gstreamer plugins

--- scripts/build/extras/gstreamer-plugins.sh
+++ scripts/build/extras/gstreamer-plugins.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.4.3}
+VERSION=${VERSION-1.4.5}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -13,5 +13,13 @@ rm -rf gst-plugins-base-$VERSION && tar -Jxf "gst-plugins-base-$VERSION.tar.xz"
 rm -f "gst-plugins-base-$VERSION.tar.xz"
 cd gst-plugins-base-$VERSION
 
-./configure --prefix=$PREFIX --disable-static
+./configure --prefix=$PREFIX
+make && make install
+
+wget "http://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-$VERSION.tar.xz"
+rm -rf gst-plugins-good-$VERSION && tar -Jxf "gst-plugins-good-$VERSION.tar.xz"
+rm -f "gst-plugins-good-$VERSION.tar.xz"
+cd gst-plugins-good-$VERSION
+
+./configure --prefix=$PREFIX
 make && make install
