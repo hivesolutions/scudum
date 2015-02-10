ecb7fa05479073baf2cdd52c5cd8f0245f9bda23
joamag <joamag@gmail.com>
2015-02-10 16:34:59 +0000

    reverted change

--- scripts/build/extras/docbook-xsl.sh
+++ scripts/build/extras/docbook-xsl.sh
@@ -6,8 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "https://distfiles.macports.org/docbook-xsl/docbook-xsl-$VERSION.tar.bz2"\
-    "http://downloads.sourceforge.net/docbook/docbook-xsl-$VERSION.tar.bz2"
+rget "http://downloads.sourceforge.net/docbook/docbook-xsl-$VERSION.tar.bz2"\
+    "https://distfiles.macports.org/docbook-xsl/docbook-xsl-$VERSION.tar.bz2"
 rm -rf docbook-xsl-$VERSION && tar -jxf "docbook-xsl-$VERSION.tar.bz2"
 rm -f "docbook-xsl-$VERSION.tar.bz2"
 cd docbook-xsl-$VERSION
