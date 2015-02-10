c6e66533b0e5c4fe50e52ab6900f41d14c72daa5
joamag <joamag@gmail.com>
2015-02-10 13:03:30 +0000

    new xorg-macros

---
+++ scripts/build/extras/xorg-macros.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-1.19.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://xorg.freedesktop.org/releases/individual/util/util-macros-$VERSION.tar.bz2"
+rm -rf util-macros-$VERSION && tar -jxf "util-macros-$VERSION.tar.bz2"
+rm -f "util-macros-$VERSION.tar.bz2"
+cd util-macros-$VERSION
+
+./configure--prefix=$PREFIX
+make && make install
