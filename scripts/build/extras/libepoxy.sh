88f99379732896befda5e4357d2c64f1d937bb28
joamag <joamag@gmail.com>
2015-02-10 12:59:02 +0000

    new libepoxy

---
+++ scripts/build/extras/libepoxy.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-1.2}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "mesa"
+
+wget "http://crux.nu/files/libepoxy-$VERSION.tar.gz"
+rm -rf libepoxy-$VERSION && tar -zxf "libepoxy-$VERSION.tar.gz"
+rm -f "libepoxy-$VERSION.tar.gz"
+cd libepoxy-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
