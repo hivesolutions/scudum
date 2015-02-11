5d15ab1959c7bb358e7269814fdf811ad9f98509
joamag <joamag@gmail.com>
2015-02-11 14:15:49 +0000

    new pkg support

---
+++ scripts/build/make.pkg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+NAME=${NAME-scudum}
+BASE=${BASE-/mnt/builds}
+TARGET=${TARGET-$BASE/$NAME/pkg}
+SCHEMA=${SCHEMA-transient}
+KVARIANT=${KVARIANT-default}
+CONFIG=${CONFIG-1}
+CLEANUP=${CLEANUP-1}
+DEPLOY=${DEPLOY-0}
+
+CUR=$(pwd)
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/base/config.sh
+
+DISTRIB=${DISTRIB-$(cat $SCUDUM/etc/scudum/DISTRIB)}
+FILE=${FILE-$NAME-$VERSION.tar.gz}
+
+if [ "$CONFIG" == "1" ]; then
+    SCHEMA=$SCHEMA KVARIANT=$KVARIANT $DIR/config.sh
+fi
+
+if [ ! -e $SCUDUM/etc/scudum/CONFIGURED ]; then
+    echo "make.iso: scudum not configured, not possible to make PKG"
+    exit 1
+fi
+
+if [ "$CLEANUP" == "1" ]; then
+    $DIR/cleanup.sh
+fi
+
+tar -cxvf $SCUDUM $FILE
+
+if [ "$DEPLOY" == "1" ]; then
+    mkdir -pv $TARGET && mv -v $FILE $TARGET
+fi
