5420d2dc9612749e999a529cfeb6225ea9d4a6ea
joamag <joamag@gmail.com>
2015-02-12 11:54:13 +0000

    new code

--- scripts/build/extras/cdrtools.latest.sh
+++ scripts/build/extras/cdrtools.latest.sh
@@ -1,4 +1,5 @@
 VERSION=${VERSION-3.01a27}
+VERSION_L=${VERSION_L-3.01}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -7,9 +8,9 @@ set -e +h
 source $DIR/common.sh
 
 wget "http://downloads.sourceforge.net/cdrtools/alpha/cdrtools-$VERSION.tar.gz"
-rm -rf cdrtools-$VERSION && tar -zxf "cdrtools-$VERSION.tar.gz"
+rm -rf cdrtools-$VERSION_L && tar -zxf "cdrtools-$VERSION.tar.gz"
 rm -f "cdrtools-$VERSION.tar.gz"
-cd cdrtools-$VERSION
+cd cdrtools-$VERSION_L
 
 make clean GMAKE_NOWARN=true || true
 make GMAKE_NOWARN=true && make install INS_BASE=$PREFIX GMAKE_NOWARN=true
