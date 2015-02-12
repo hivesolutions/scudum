5e006260a8f4d8aef6c1b1bfc11c3f4d6c6978ac
joamag <joamag@gmail.com>
2015-02-12 19:09:04 +0000

    new uhci

--- scripts/build/make.efi.sh
+++ scripts/build/make.efi.sh
@@ -33,11 +33,11 @@ grub-mkimage\
     --compression=xz\
     --prefix=/EFI/BOOT\
     part_gpt part_msdos fat ext2 ntfs hfs hfsplus iso9660 udf ufs1 ufs2\
-    zfs chain linux boot appleldr ahci configfile normal regexp\
+    zfs chain linux boot appleldr ahci uhci configfile normal regexp\
     minicmd reboot halt search search_fs_file search_fs_uuid\
     search_label gfxterm gfxmenu efi_gop efi_uga all_video loadbios\
     gzio echo true probe loadenv bitmap_scale font cat help ls png\
-    jpeg tga test at_keyboard usb_keyboard sleep
+    jpeg tga test at_keyboard usb_keyboard sleep usbms
 
 dd if=/dev/zero of=$EFI_NAME bs=1K count=1440
 mkdosfs -F 12 $EFI_NAME
