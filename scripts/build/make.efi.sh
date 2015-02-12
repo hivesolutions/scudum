5612e87000d6b6975f5460897bc5997eaba605b3
joamag <joamag@gmail.com>
2015-02-12 18:49:11 +0000

    removed uhci

--- scripts/build/make.efi.sh
+++ scripts/build/make.efi.sh
@@ -33,8 +33,8 @@ grub-mkimage\
     --compression=xz\
     --prefix=/EFI/BOOT\
     part_gpt part_msdos fat ext2 ntfs hfs hfsplus iso9660 udf ufs1 ufs2\
-    zfs chain linux boot appleldr ahci ahci ehci ohci uhci configfile\
-    normal regexp minicmd reboot halt search search_fs_file search_fs_uuid\
+    zfs chain linux boot appleldr ahci ahci ehci ohci configfile normal regexp\
+    minicmd reboot halt search search_fs_file search_fs_uuid\
     search_label gfxterm gfxmenu efi_gop efi_uga all_video loadbios\
     gzio echo true probe loadenv bitmap_scale font cat help ls png\
     jpeg tga test at_keyboard usb_keyboard usbms sleep
