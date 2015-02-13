40161f045b30229e3baca8d5f4854ccf0dfe3ea7
joamag <joamag@gmail.com>
2015-02-13 09:28:04 +0000

    new code

--- scripts/build/extras/pci-utils.sh
+++ scripts/build/extras/pci-utils.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "https://ftp.kernel.org/pub/software/utils/pciutils/pciutils-$VERSION.tar.xz"
+wget "https://www.kernel.org/pub/software/utils/pciutils/pciutils-$VERSION.tar.xz"
 rm -rf pciutils-$VERSION && tar -Jxf "pciutils-$VERSION.tar.xz"
 rm -f "pciutils-$VERSION.tar.xz"
 cd pciutils-$VERSION
