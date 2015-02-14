665fb6f95e3fd52a43090b6a16e76bcfcc933bdf
joamag <joamag@gmail.com>
2015-02-14 15:10:39 +0000

    small fix

--- scripts/build/tools/tcl.sh
+++ scripts/build/tools/tcl.sh
@@ -2,15 +2,13 @@ VERSION=${VERSION-8.6.3}
 
 set -e +h
 
-export tcl_cv_strtod_buggy=1
-
 wget "http://downloads.sourceforge.net/tcl/tcl$VERSION-src.tar.gz"
 rm -f "tcl$VERSION" && tar -zxf "tcl$VERSION-src.tar.gz"
 rm -f "tcl$VERSION-src.tar.gz"
 cd tcl$VERSION
 
 cd unix
-./configure --host=$ARCH_TARGET --prefix=$PREFIX
+tcl_cv_strtod_buggy=1 ./configure --host=$ARCH_TARGET --prefix=$PREFIX
 make && make install
 
 chmod -v u+w $PREFIX/lib/libtcl8.6.so
