dd7f1e6feec40e328c29c9b3cc5e925afdd393ed
joamag <joamag@gmail.com>
2015-02-14 14:47:21 +0000

    new code support

--- scripts/build/base/config.sh
+++ scripts/build/base/config.sh
@@ -109,6 +109,15 @@ case "$GCC_FLAVOUR" in
         ;;
 esac
 
+case "$SCUDUM_ARCH" in
+    arm)
+        export CC="$ARCH_TARGET-gcc"
+        export CXX="$ARCH_TARGET-g++"
+        export AR="$ARCH_TARGET-ar"
+        export RANLIB="$ARCH_TARGET-ranlib"
+        ;;
+esac
+
 print_scudum() {
     echo "PERSIST := $PERSIST"
     echo "SCUDUM := $SCUDUM"
