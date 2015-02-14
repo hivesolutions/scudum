1dcf39a87e58ea9cd49f2c616d4e36fbbc656fb6
joamag <joamag@gmail.com>
2015-02-14 16:21:43 +0000

    new code support

--- scripts/build/tools/expect.sh
+++ scripts/build/tools/expect.sh
@@ -7,11 +7,13 @@ rm -f "expect$VERSION" && tar -zxf "expect$VERSION.tar.gz"
 rm -f "expect$VERSION.tar.gz"
 cd expect$VERSION
 
-cp -v configure{,.orig}
-sed 's:/usr/local/bin:/bin:' configure.orig > configure
-
 wget https://raw.githubusercontent.com/maximeh/buildroot/master/package/expect/expect-0001-enable-cross-compilation.patch
 patch -Np1 -i expect-0001-enable-cross-compilation.patch
+
+autoreconf -f -i
+
+cp -v configure{,.orig}
+sed 's:/usr/local/bin:/bin:' configure.orig > configure
 
 ./configure --host=$ARCH_TARGET --prefix=$PREFIX --with-tcl=$PREFIX/lib\
     --with-tclinclude=$PREFIX/include
