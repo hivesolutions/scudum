f9dbb60fe15cfb85ac14cde4bc5e728bdb905cd5
joamag <joamag@gmail.com>
2015-02-14 15:40:23 +0000

    new tools

--- scripts/build/tools/dejagnu.sh
+++ scripts/build/tools/dejagnu.sh
@@ -7,5 +7,5 @@ rm -f "dejagnu-$VERSION" && tar -zxf "dejagnu-$VERSION.tar.gz"
 rm -f "dejagnu-$VERSION.tar.gz"
 cd dejagnu-$VERSION
 
-./configure --prefix=$PREFIX
+./configure --host=$ARCH_TARGET --prefix=$PREFIX
 make && make install
