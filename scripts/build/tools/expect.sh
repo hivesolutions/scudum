f1880a3142a5594dc44f4489b406376280fdd577
joamag <joamag@gmail.com>
2015-02-14 15:03:28 +0000

    new code

--- scripts/build/tools/expect.sh
+++ scripts/build/tools/expect.sh
@@ -10,6 +10,6 @@ cd expect$VERSION
 cp -v configure{,.orig}
 sed 's:/usr/local/bin:/bin:' configure.orig > configure
 
-./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib\
+./configure --host=$ARCH_TARGET --prefix=$PREFIX --with-tcl=$PREFIX/lib\
     --with-tclinclude=$PREFIX/include
 make && make SCRIPTS="" install
--- scripts/build/tools/tcl.sh
+++ scripts/build/tools/tcl.sh
@@ -3,7 +3,6 @@ VERSION=${VERSION-8.6.3}
 set -e +h
 
 export ac_cv_func_strtod=yes
-export tcl_cv_strtod_buggy=1
 
 wget "http://downloads.sourceforge.net/tcl/tcl$VERSION-src.tar.gz"
 rm -f "tcl$VERSION" && tar -zxf "tcl$VERSION-src.tar.gz"
@@ -11,7 +10,7 @@ rm -f "tcl$VERSION-src.tar.gz"
 cd tcl$VERSION
 
 cd unix
-./configure --host="$ARCH_TARGET" --prefix=$PREFIX
+./configure --host=$ARCH_TARGET --prefix=$PREFIX
 make && make install
 
 chmod -v u+w $PREFIX/lib/libtcl8.6.so
