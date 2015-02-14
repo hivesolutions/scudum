9bfa73f8ed09e6fdf1f2ea72f632c71f580d6473
joamag <joamag@gmail.com>
2015-02-14 16:56:10 +0000

    removed extra patch

--- scripts/build/tools/expect.sh
+++ scripts/build/tools/expect.sh
@@ -7,11 +7,6 @@ rm -f "expect$VERSION" && tar -zxf "expect$VERSION.tar.gz"
 rm -f "expect$VERSION.tar.gz"
 cd expect$VERSION
 
-wget https://raw.githubusercontent.com/maximeh/buildroot/master/package/expect/expect-0001-enable-cross-compilation.patch
-patch -Np1 -i expect-0001-enable-cross-compilation.patch
-
-autoreconf -f -i
-
 cp -v configure{,.orig}
 sed 's:/usr/local/bin:/bin:' configure.orig > configure
 
