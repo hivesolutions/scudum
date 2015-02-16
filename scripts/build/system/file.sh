06cc41500c58d2137d8db18edbc5a2daa5548939
joamag <joamag@gmail.com>
2015-02-16 23:23:58 +0000

    new host value

--- scripts/build/system/file.sh
+++ scripts/build/system/file.sh
@@ -7,7 +7,7 @@ rm -rf file-$VERSION && tar -zxf "file-$VERSION.tar.gz"
 rm -f "file-$VERSION.tar.gz"
 cd file-$VERSION
 
-./configure --prefix=/usr
+./configure --host=$ARCH_TARGET --prefix=/usr
 
 make
 test $TEST && make check
