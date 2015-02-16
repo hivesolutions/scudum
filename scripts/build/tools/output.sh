866c7c5263094cc3cd391a533d84a59148aa3822
joamag <joamag@gmail.com>
2015-02-16 22:29:10 +0000

    new output strategy

--- scripts/build/tools/output.sh
+++ scripts/build/tools/output.sh
@@ -1,7 +1,6 @@
 set -e +h
 
-cat > $SCUDUM/tools/config << "EOF"
-"export SCUDUM=$SCUDUM
+variables="export SCUDUM=$SCUDUM
 export SCUDUM_HOST=$SCUDUM_HOST
 export SCUDUM_ARCH=$SCUDUM_ARCH
 export SCUDUM_VENDOR=$SCUDUM_VENDOR
@@ -9,7 +8,8 @@ export SCUDUM_SYSTEM=$SCUDUM_SYSTEM
 export SCUDUM_SYSTEM_H=$SCUDUM_SYSTEM_H
 export SCUDUM_MARCH=$SCUDUM_MARCH
 export SCUDUM_TARGET=$SCUDUM_TARGET
-export ARCH_TARGET=$ARCH_TARGET
-EOF"
+export ARCH_TARGET=$ARCH_TARGET"
+
+echo $variables > $SCUDUM/tools/config
 
 chmod +x $SCUDUM/tools/config
