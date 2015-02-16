f498866eba44e316131a4ab35e2ca10667be147a
joamag <joamag@gmail.com>
2015-02-16 18:26:58 +0000

    new url

--- scripts/build/extras/ntfsprogs.sh
+++ scripts/build/extras/ntfsprogs.sh
@@ -6,7 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "http://downloads.sourceforge.net/linux-ntfs/ntfsprogs-$VERSION.tar.gz"
+rget "ftp://ftp.mirrorservice.org/sites/distfiles.macports.org/ntfsprogs/ntfsprogs-$VERSION.tar.gz"\
+    "http://downloads.sourceforge.net/linux-ntfs/ntfsprogs-$VERSION.tar.gz"
 rm -rf ntfsprogs-$VERSION && tar -zxf "ntfsprogs-$VERSION.tar.gz"
 rm -f "ntfsprogs-$VERSION.tar.gz"
 cd ntfsprogs-$VERSION
