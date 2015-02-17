2b36aeaa28ef4ac4c0e2057af93a4f3ee377576c
joamag <joamag@gmail.com>
2015-02-17 22:27:30 +0000

    new package config and ld

--- scripts/build/base/config.system.sh
+++ scripts/build/base/config.system.sh
@@ -4,7 +4,9 @@ case "$SCUDUM_ARCH" in
         export CXX="$ARCH_TARGET-g++"
         export AR="$ARCH_TARGET-ar"
         export RANLIB="$ARCH_TARGET-ranlib"
+        export LD="$ARCH_TARGET-ld"
         export LD_LIBRARY_PATH="/usr/lib:/lib"
+        export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/lib/pkgconfig"
         export CFLAGS="-I/usr/include -I/include -L/usr/lib -L/lib -R/usr/lib -R/lib"
         export CXXFLAGS="-I/usr/include -I/include -L/usr/lib -L/lib -R/usr/lib -R/lib"
         ;;
