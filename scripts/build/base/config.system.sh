ad01aad96663edcf0f4ec784ff918db950f70d44
joamag <joamag@gmail.com>
2015-02-17 00:26:30 +0000

    new include and lib paths

--- scripts/build/base/config.system.sh
+++ scripts/build/base/config.system.sh
@@ -4,5 +4,8 @@ case "$SCUDUM_ARCH" in
         export CXX="$ARCH_TARGET-g++"
         export AR="$ARCH_TARGET-ar"
         export RANLIB="$ARCH_TARGET-ranlib"
+        export LD_LIBRARY_PATH="/usr/lib:/lib"
+        export CFLAGS="-I/usr/include -I/include -L/usr/lib -L/lib -R/usr/lib -R/lib"
+        export CXXFLAGS="-I/usr/include -I/include -L/usr/lib -L/lib -R/usr/lib -R/lib"
         ;;
 esac
