eed8ad793e93b4bd8767ca61368b354dfdf3459e
joamag <joamag@gmail.com>
2015-02-17 12:21:12 +0000

    fixed ps misc cross compile

--- scripts/build/system/psmisc.sh
+++ scripts/build/system/psmisc.sh
@@ -2,6 +2,9 @@ VERSION=${VERSION-22.20}
 
 set -e +h
 
+export ac_cv_func_malloc_0_nonnull=yes
+export ac_cv_func_realloc_0_nonnull=yes
+
 wget --no-check-certificate "http://downloads.sourceforge.net/psmisc/psmisc-$VERSION.tar.gz"
 rm -rf psmisc-$VERSION && tar -zxf "psmisc-$VERSION.tar.gz"
 rm -f "psmisc-$VERSION.tar.gz"
