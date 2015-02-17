32e9cb1af4ba1bdfbdcc2d4bf68eda3aeb182b8f
joamag <joamag@gmail.com>
2015-02-17 17:34:01 +0000

    new definition

--- scripts/build/extras/crosstool-rasp.sh
+++ scripts/build/extras/crosstool-rasp.sh
@@ -16,6 +16,7 @@ CT_PREFIX_DIR="/opt/${CT_TARGET}"
 CT_ARCH_FPU="vfp"
 CT_ARCH_FLOAT_HW=y
 CT_ARCH_arm=y
+CT_ARCH_ARCH="armv6zk"
 CT_ARCH_CPU="arm1176jzf-s"
 CT_ARCH_TUNE="arm1176jzf-s"
 CT_TARGET_VENDOR="rasp"
