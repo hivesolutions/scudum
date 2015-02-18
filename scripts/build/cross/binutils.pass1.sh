6dfda691cb3d3f1aaa682d11dab3b74baa5b184f
joamag <joamag@gmail.com>
2015-02-18 17:37:30 +0000

    new binutils

---
+++ scripts/build/cross/binutils.pass1.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-2.25}
+
+set -e +h
+
+wget "http://ftp.gnu.org/gnu/binutils/binutils-$VERSION.tar.bz2"
+rm -rf binutils-$VERSION && tar -jxf "binutils-$VERSION.tar.bz2"
+rm -f "binutils-$VERSION.tar.bz2"
+cd binutils-$VERSION
+
+./configure --prefix=$PREFIX --with-sysroot=$SCUDUM --with-lib-path=$PREFIX/lib\
+    --target=$SCUDUM_ARCH --disable-nls --disable-werror
+
+make
+case $SCUDUM_ARCH in
+    x86_64) mkdir -v $PREFIX/lib && ln -sv lib $PREFIX/lib64 ;;
+esac
+make install
