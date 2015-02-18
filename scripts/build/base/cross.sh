4a996bb6b057d7c7c05ba8776bdcda2efddf3477
joamag <joamag@gmail.com>
2015-02-18 18:34:43 +0000

    new binutils

--- scripts/build/base/cross.sh
+++ scripts/build/base/cross.sh
@@ -8,11 +8,10 @@ DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 if [ "$SCUDUM_CROSS_COPY" == "1" ]; then
     cp -rpn /opt/$ARCH_TARGET/* $SCUDUM/cross
 else
-    $DIR/../cross/binutils.pass1.sh
+    $DIR/../cross/binutils.sh
     $DIR/../cross/$GCC_BUILD_BINARY.pass1.sh
     $DIR/../cross/linux-headers.sh
     $DIR/../cross/glibc.sh
     $DIR/../cross/libstdc++.sh
-    $DIR/../cross/binutils.pass2.sh
     $DIR/../cross/$GCC_BUILD_BINARY.pass2.sh
 fi
