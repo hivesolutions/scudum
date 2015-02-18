60b7ed157169516e11d35698e5610ae3e463b415
joamag <joamag@gmail.com>
2015-02-18 18:01:07 +0000

    new linux headers

---
+++ scripts/build/cross/linux-headers.sh
@@ -0,0 +1,14 @@
+VERSION=${VERSION-3.19}
+
+set -e +h
+
+wget "https://www.kernel.org/pub/linux/kernel/v3.x/linux-$VERSION.tar.xz"
+rm -rf linux-$VERSION && tar -Jxf "linux-$VERSION.tar.xz"
+rm -f "linux-$VERSION.tar.xz"
+cd linux-$VERSION
+
+make mrproper
+make ARCH=$SCUDUM_ARCH headers_check
+make ARCH=$SCUDUM_ARCH INSTALL_HDR_PATH=dest headers_install
+
+cp -rv dest/include/* $PREFIX_CROSS/include
