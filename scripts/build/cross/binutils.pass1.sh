5ce1870b0fa00e85348a23ad7b2c1390ffcb24cf
joamag <joamag@gmail.com>
2015-02-18 17:48:11 +0000

    new arch target fix

--- scripts/build/cross/binutils.pass1.sh
+++ scripts/build/cross/binutils.pass1.sh
@@ -8,7 +8,7 @@ rm -f "binutils-$VERSION.tar.bz2"
 cd binutils-$VERSION
 
 ./configure --prefix=$PREFIX_CROSS --with-sysroot=$SCUDUM --with-lib-path=$PREFIX_CROSS/lib\
-    --target=$SCUDUM_ARCH --disable-nls --disable-werror
+    --target=$ARCH_TARGET --disable-nls --disable-werror
 
 make
 case $SCUDUM_ARCH in
