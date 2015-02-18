d23df25447f1613a6e0ba8f1455d29fda4fa418d
joamag <joamag@gmail.com>
2015-02-18 18:18:51 +0000

    new cross compilation support

--- scripts/build/cross/gcc.latest.pass1.sh
+++ scripts/build/cross/gcc.latest.pass1.sh
@@ -25,12 +25,12 @@ mv mpc-$VERSION_MPC mpc
 for file in $(find gcc/config -name linux64.h -o -name linux.h -o -name sysv4.h)
 do
     cp -uv $file{,.orig}
-    sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g'\
-        -e 's@/usr@/tools@g' $file.orig > $file
+    sed -e 's@/lib\(64\)\?\(32\)\?/ld@/cross&@g'\
+        -e 's@/usr@/cross@g' $file.orig > $file
     echo '
 #undef STANDARD_STARTFILE_PREFIX_1
 #undef STANDARD_STARTFILE_PREFIX_2
-#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
+#define STANDARD_STARTFILE_PREFIX_1 "/cross/lib/"
 #define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
     touch $file.orig
 done
