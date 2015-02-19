1f713d5141314d42d90a27e9c6cca22842e8591a
joamag <joamag@gmail.com>
2015-02-19 16:27:58 +0000

    new target

--- scripts/build/system/autoconf.sh
+++ scripts/build/system/autoconf.sh
@@ -7,7 +7,7 @@ rm -rf autoconf-$VERSION && tar -Jxf "autoconf-$VERSION.tar.xz"
 rm -f "autoconf-$VERSION.tar.xz"
 cd autoconf-$VERSION
 
-./configure --prefix=/usr
+./configure --host=$ARCH_TARGET --prefix=/usr
 
 make
 test $TEST && make check
