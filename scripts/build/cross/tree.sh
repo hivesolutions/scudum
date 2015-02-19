789856156798c75d61dd380d619d4671c35bb383
joamag <joamag@gmail.com>
2015-02-19 09:39:55 +0000

    small code fix

--- scripts/build/cross/tree.sh
+++ scripts/build/cross/tree.sh
@@ -1,18 +1,18 @@
 set -e +h
 
-mkdir -p $CROSS_PREFIX/lib
-mkdir -p $CROSS_PREFIX/sysroot
-mkdir -p $CROSS_PREFIX/sysroot/lib
-mkdir -p $CROSS_PREFIX/sysroot/usr/lib
-mkdir -p $CROSS_PREFIX/sysroot/usr/include
-mkdir -p $CROSS_PREFIX/$ARCH_TARGET
+mkdir -p $PREFIX_CROSS/lib
+mkdir -p $PREFIX_CROSS/sysroot
+mkdir -p $PREFIX_CROSS/sysroot/lib
+mkdir -p $PREFIX_CROSS/sysroot/usr/lib
+mkdir -p $PREFIX_CROSS/sysroot/usr/include
+mkdir -p $PREFIX_CROSS/$ARCH_TARGET
 
-ln -svf lib $CROSS_PREFIX/lib32
-ln -svf lib $CROSS_PREFIX/lib64
-ln -svf lib $CROSS_PREFIX/sysroot/lib32
-ln -svf lib $CROSS_PREFIX/sysroot/lib64
-ln -svf lib $CROSS_PREFIX/sysroot/usr/lib32
-ln -svf lib $CROSS_PREFIX/sysroot/usr/lib64
-ln -svf ../sysroot/lib $CROSS_PREFIX/$ARCH_TARGET/lib
-ln -svf lib $CROSS_PREFIX/$ARCH_TARGET/lib32
-ln -svf lib $CROSS_PREFIX/$ARCH_TARGET/lib64
+ln -svf lib $PREFIX_CROSS/lib32
+ln -svf lib $PREFIX_CROSS/lib64
+ln -svf lib $PREFIX_CROSS/sysroot/lib32
+ln -svf lib $PREFIX_CROSS/sysroot/lib64
+ln -svf lib $PREFIX_CROSS/sysroot/usr/lib32
+ln -svf lib $PREFIX_CROSS/sysroot/usr/lib64
+ln -svf ../sysroot/lib $PREFIX_CROSS/$ARCH_TARGET/lib
+ln -svf lib $PREFIX_CROSS/$ARCH_TARGET/lib32
+ln -svf lib $PREFIX_CROSS/$ARCH_TARGET/lib64
