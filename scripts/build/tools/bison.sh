49744e0d4d5a66e7b4c7f5f46029d08ede2d0644
joamag <joamag@gmail.com>
2015-02-24 14:59:46 +0000

    new bison and flex scripts

---
+++ scripts/build/tools/bison.sh
@@ -0,0 +1,14 @@
+VERSION=${VERSION-2.7}
+
+set -e +h
+
+wget "http://ftp.gnu.org/gnu/bison/bison-$VERSION.tar.xz"
+rm -rf bison-$VERSION && tar -Jxf "bison-$VERSION.tar.xz"
+rm -f "bison-$VERSION.tar.xz"
+cd bison-$VERSION
+
+./configure --prefix=$PREFIX
+
+echo '#define YYENABLE_NLS 1' >> lib/config.h
+
+make && make install
---
+++ scripts/build/tools/flex.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.5.37}
+
+set -e +h
+
+wget "http://downloads.sourceforge.net/flex/flex-$VERSION.tar.bz2"
+rm -rf flex-$VERSION && tar -jxf "flex-$VERSION.tar.bz2"
+rm -f "flex-$VERSION.tar.bz2"
+cd flex-$VERSION
+
+wget  --no-check-certificate "http://www.linuxfromscratch.org/patches/lfs/7.3/flex-$VERSION-bison-2.6.1-1.patch"
+patch -Np1 -i flex-$VERSION-bison-2.6.1-1.patch
+
+./configure --prefix=$PREFIX
+
+make && make install
