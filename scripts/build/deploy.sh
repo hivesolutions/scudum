6f25f350faea76e6ba5f490a1aa31a81e82332f3
joamag <joamag@gmail.com>
2015-02-25 12:23:29 +0000

    new version of the deploy infra-structure

--- scripts/build/deploy.sh
+++ scripts/build/deploy.sh
@@ -3,7 +3,6 @@
 
 NAME=${NAME-scudum}
 VERSION=${VERSION-$(date +%Y%m%d)}
-FILE=${FILE-$NAME-$VERSION.tar.gz}
 TARGET=${TARGET-/mnt/builds/$NAME}
 
 LATEST=$NAME-latest.tar.gz
@@ -12,6 +11,16 @@ DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 set -e +h
 
 source $DIR/base/config.sh
+
+if [ -e $SCUDUM/config ]; then
+    source $SCUDUM/config
+fi
+
+if [ "$SCUDUM_ARCH" == "" ]; then
+    FILE=${FILE-$NAME-$VERSION.tar.gz}
+else
+    FILE=${FILE-$NAME-$VERSION-$SCUDUM_ARCH.tar.gz}
+fi
 
 NAME=$NAME VERSION=$VERSION FILE=$FILE $DIR/pack.sh
 
