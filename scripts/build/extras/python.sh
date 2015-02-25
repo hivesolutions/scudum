f9a59dc218435ee770f9d7ffaf6d3cdd3daadff1
joamag <joamag@gmail.com>
2015-02-25 18:36:31 +0000

    new code

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -14,7 +14,7 @@ rm -f "Python-$VERSION.tgz"
 cd Python-$VERSION
 
 if [ "$SCUDUM_CROSS" == "1" ]; then
-    CC=gcc ./configure
+    CC=gcc CFLAGS="" ./configure
     make python Parser/pgen
     mv python python_for_build
     mv Parser/pgen Parser/pgen_for_build
