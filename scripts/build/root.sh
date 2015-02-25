799690f70efdca7b3d7c0ef6a52a5340142c34da
joamag <joamag@gmail.com>
2015-02-25 23:17:54 +0000

    new bc support

--- scripts/build/root.sh
+++ scripts/build/root.sh
@@ -93,6 +93,7 @@ $DIR/tools/pkg-config.sh
 $DIR/tools/util-linux.sh
 $DIR/tools/shadow.sh
 $DIR/tools/e2fsprogs.sh
+$DIR/tools/bc.sh
 $DIR/tools/openssl.sh
 $DIR/tools/bison.sh
 $DIR/tools/flex.sh
---
+++ scripts/build/tools/bc.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-1.06}
+
+set -e +h
+
+wget "http://ftp.gnu.org/gnu/bc/bc-$VERSION.tar.gz"
+rm -rf bc-$VERSION && tar -zxf "bc-$VERSION.tar.gz"
+rm -f "bc-$VERSION.tar.gz"
+cd bc-$VERSION
+
+./configure --prefix=$PREFIX
+
+make && make install
