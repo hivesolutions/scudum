793cf33a31cd2dda7220c4e5d977ed2fd80a13bd
joamag <joamag@gmail.com>
2015-02-26 13:06:32 +0000

    new kmod support

---
+++ scripts/build/tools/kmod.sh
@@ -0,0 +1,19 @@
+VERSION=${VERSION-12}
+
+set -e +h
+
+wget "http://www.kernel.org/pub/linux/utils/kernel/kmod/kmod-$VERSION.tar.xz"
+rm -rf kmod-$VERSION && tar -Jxf "kmod-$VERSION.tar.xz"
+rm -f "kmod-$VERSION.tar.xz"
+cd kmod-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --bindir=$PREFIX/bin\
+    --libdir=$PREFIX/lib\
+    --sysconfdir=/etc\
+    --disable-manpages\
+    --with-xz\
+    --with-zlib
+
+make && make pkgconfigdir=$PREFIX/lib/pkgconfig install
