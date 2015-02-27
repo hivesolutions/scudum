9345e26ecff4b014a78dff41c447c774c1b6f527
joamag <joamag@gmail.com>
2015-02-27 16:28:21 +0000

    better strip support

--- scripts/build/system/strip.sh
+++ scripts/build/system/strip.sh
@@ -1,2 +1,7 @@
-/tools/bin/find /{,usr/}{bin,lib,sbin} -type f\
-    -exec /tools/bin/strip --strip-debug '{}' ';'
+if [ "$SCUDUM_CROSS" == "1" ]; then
+    strip=/cross/bin/$ARCH_TARGET-strip
+else
+    strip=/tools/bin/strip
+fi
+
+/tools/bin/find /{,usr/}{bin,lib,sbin} -type f -exec $strip --strip-debug '{}' ';'
