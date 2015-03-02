26dcb5998448b8073a98d9fcef8642dd7569e336
joamag <joamag@gmail.com>
2015-03-02 18:10:50 +0000

    new cflags

--- scripts/build/extras/wpa-supplicant.sh
+++ scripts/build/extras/wpa-supplicant.sh
@@ -45,7 +45,7 @@ cd wpa_supplicant
 
 if [ "$SCUDUM_CROSS" == "1" ]; then
     if [ -z "$CFLAGS" ]; then export CFLAGS="-O2"; fi
-    make CC=$CC CFLAGS=$CFLAGS BINDIR=$PREFIX/bin LIBDIR=$PREFIX/lib C_INCLUDE_PATH=$C_INCLUDE_PATH:$PREFIX/include/libnl3
+    make CC=$CC CFLAGS="$CFLAGS" BINDIR=$PREFIX/bin LIBDIR=$PREFIX/lib C_INCLUDE_PATH=$C_INCLUDE_PATH:$PREFIX/include/libnl3
 else
     make BINDIR=$PREFIX/bin LIBDIR=$PREFIX/lib C_INCLUDE_PATH=$C_INCLUDE_PATH:$PREFIX/include/libnl3
 fi
