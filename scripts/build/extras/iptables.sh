c588bb86404e759e5de6e9e007d35965d990873d
joamag <joamag@gmail.com>
2015-03-02 18:02:36 +0000

    new code

--- scripts/build/extras/iptables.sh
+++ scripts/build/extras/iptables.sh
@@ -6,14 +6,11 @@ set -e +h
 
 source $DIR/common.sh
 
-export ac_cv_func_malloc_0_nonnull=yes
-export ac_cv_func_realloc_0_nonnull=yes
-
 wget "http://www.netfilter.org/projects/iptables/files/iptables-$VERSION.tar.bz2"
 rm -rf iptables-$VERSION && tar -jxf "iptables-$VERSION.tar.bz2"
 rm -f "iptables-$VERSION.tar.bz2"
 cd iptables-$VERSION
 
-./configure --host=$ARCH_TARGET --prefix=$PREFIX
+ac_cv_func_malloc_0_nonnull=yes ./configure --host=$ARCH_TARGET --prefix=$PREFIX
 
 make && make install
