c63c652d07f5d66a363a078a303099a1a43d85b7
joamag <joamag@gmail.com>
2015-03-02 13:31:45 +0000

    new arm target configuration

--- scripts/build/system/openssl.sh
+++ scripts/build/system/openssl.sh
@@ -4,20 +4,20 @@ set -e +h
 
 unset MAKEFLAGS
 
-export SSL_FLAGS="-DOPENSSL_PIC -DZLIB -DOPENSSL_THREADS -D_REENTRANT -DDSO_DLFCN -DHAVE_DLFCN_H -DL_ENDIAN -DTERMIO\
-    -DOPENSSL_BN_ASM_MONT -DOPENSSL_BN_ASM_GF2m -DSHA1_ASM -DSHA256_ASM -DSHA512_ASM -DAES_ASM -DGHASH_ASM"
-
-if [ -z "$CC" ]; then export CC="gcc"; fi
-if [ -z "$CFLAGS" ]; then export CFLAGS="-O2"; fi
-export CC="$CC $CFLAGS $SSL_FLAGS"
-
 wget --no-check-certificate "https://www.openssl.org/source/openssl-$VERSION.tar.gz"
 rm -rf openssl-$VERSION && tar -zxf "openssl-$VERSION.tar.gz"
 rm -f "openssl-$VERSION.tar.gz"
 cd openssl-$VERSION
 
 if [ "$SCUDUM_CROSS" == "1" ]; then
-    ./config shared --prefix=/usr --openssldir=/usr/ssl os/compiler:$ARCH_TARGET-
+    case "$SCUDUM_ARCH" in
+        arm)
+            ./Configure linux-armv4 shared --prefix=/usr --openssldir=/usr/ssl
+            ;;
+        *)
+            ./config shared --prefix=/usr --openssldir=/usr/ssl os/compiler:$ARCH_TARGET-gcc
+            ;;
+    esac
 else
     ./config shared --prefix=/usr --openssldir=/usr/ssl
 fi
