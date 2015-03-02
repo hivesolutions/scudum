277def7650b6b6b45201f3ed36144093c30f1dc2
joamag <joamag@gmail.com>
2015-03-02 12:51:34 +0000

    new ssl source

--- scripts/build/system/openssl.sh
+++ scripts/build/system/openssl.sh
@@ -4,9 +4,12 @@ set -e +h
 
 unset MAKEFLAGS
 
+export SSL_FLAGS="-DOPENSSL_PIC -DZLIB -DOPENSSL_THREADS -D_REENTRANT -DDSO_DLFCN -DHAVE_DLFCN_H -DL_ENDIAN -DTERMIO\
+    -DOPENSSL_BN_ASM_MONT -DOPENSSL_BN_ASM_GF2m -DSHA1_ASM -DSHA256_ASM -DSHA512_ASM -DAES_ASM -DGHASH_ASM"
+
 if [ -z "$CC" ]; then export CC="gcc"; fi
 if [ -z "$CFLAGS" ]; then export CFLAGS="-O2"; fi
-export CC="$CC $CFLAGS"
+export CC="$CC $CFLAGS $SSL_FLAGS"
 
 wget --no-check-certificate "https://www.openssl.org/source/openssl-$VERSION.tar.gz"
 rm -rf openssl-$VERSION && tar -zxf "openssl-$VERSION.tar.gz"
