2774cec998e39ad659f2d827af7a7f17d5ed3eba
joamag <joamag@gmail.com>
2015-03-02 19:02:25 +0000

    new functions and common support for system

---
+++ scripts/build/base/functions.sh
@@ -0,0 +1,12 @@
+depends() {
+    for package in $@; do
+        FORCE=0 REMOVE=0 REFRESH=0 scu.install $package
+    done
+}
+
+rget() {
+    for url in "$@"; do
+        wget --tries=1 --timeout=20 $url && return 0
+    done
+    return 1
+}
--- scripts/build/system/common.sh
+++ scripts/build/system/common.sh
@@ -1,6 +1,3 @@
-export PREFIX=${PREFIX-/usr}
-export MAKEFLAGS=${MAKEFLAGS--j $(nproc)}
-
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 source $DIR/../base/functions.sh
