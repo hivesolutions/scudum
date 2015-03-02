968c349c629ce2e2d236e5a98759b5ec09c46483
joamag <joamag@gmail.com>
2015-03-02 17:25:29 +0000

    new code

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -14,6 +14,9 @@ rm -f "Python-$VERSION.tgz"
 cd Python-$VERSION
 
 if [ "$SCUDUM_CROSS" == "1" ]; then
+    cp ../Python-$VERSION for_build
+    cd for_build
+
     CC=gcc\
     RANLIB=ranlib\
     CFLAGS="-I/tools/include"\
@@ -24,16 +27,18 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     C_INCLUDE_PATH="/tools/include"\
     LD_LIBRARY_PATH="/tools/lib"\
     LIBRARY_PATH="/tools/lib" make python Parser/pgen sharedmods
+    
+    cd ..
 
-    export PATH="binaries:$PATH"
-    mkdir binaries && cp -p python binaries
-    cp -p python python_for_build
-    cp -p Parser/pgen Parser/pgen_for_build
-    make distclean
+    export PATH="for_build:$PATH"
+    #cp -p python python_for_build
+    cp -p for_build/Parser/pgen Parser/pgen_for_build
+    #make distclean
 
     ac_cv_file__dev_ptmx=no\
     ac_cv_file__dev_ptc=no\
     ac_cv_have_long_long_format=yes\
+    PYTHON_FOR_BUILD=for_build/python\
     ./configure --build=$SCUDUM_ARCH --host=$ARCH_TARGET --prefix=$PREFIX --enable-shared --disable-ipv6
 else
     ./configure --prefix=$PREFIX --enable-shared
