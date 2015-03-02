1f14258e11637b11c2e4b33c2b5da04b6cf47fdb
joamag <joamag@gmail.com>
2015-03-02 17:47:32 +0000

    new code

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -47,11 +47,13 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     C_INCLUDE_PATH="/tools/include" ./configure
     C_INCLUDE_PATH="/tools/include"\
     LD_LIBRARY_PATH="/tools/lib"\
-    LIBRARY_PATH="/tools/lib" make Parser/pgen
+    LIBRARY_PATH="/tools/lib" make python Parser/pgen
+
+    mkdir binaries && cp -p python binaries
+    mv python python_for_build
+    mv Parser/pgen Parser/pgen_for_build
     make distclean
-
-    cp -p Parser/pgen Parser/pgen_for_build
-
+    
     ac_cv_file__dev_ptmx=no\
     ac_cv_file__dev_ptc=no\
     ac_cv_have_long_long_format=yes\
