15869104a1ed400464ebe0305f31b82b0b593456
joamag <joamag@gmail.com>
2015-03-03 15:57:42 +0000

    reverted to original

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -18,7 +18,6 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     cp -rp Python-$VERSION Python-$VERSION-build
     cd Python-$VERSION-build
     PYTHON_BUILD_DIR=$(pwd)
-    PYTHON_TARGET_DIR=$(mktemp -d)
 
     CC=gcc\
     RANLIB=ranlib\
@@ -26,38 +25,39 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     LDFLAGS="-L/tools/lib"\
     LD_LIBRARY_PATH="/tools/lib"\
     LIBRARY_PATH="/tools/lib"\
-    C_INCLUDE_PATH="/tools/include" ./configure --prefix=$PYTHON_TARGET_DIR
+    C_INCLUDE_PATH="/tools/include" ./configure
     C_INCLUDE_PATH="/tools/include"\
     LD_LIBRARY_PATH="/tools/lib"\
-    LIBRARY_PATH="/tools/lib" make
-    make install
+    LIBRARY_PATH="/tools/lib" make python Parser/pgen sharedmods
 
     cp -p python python_for_build
     cp -p Parser/pgen Parser/pgen_for_build
 
     cd ../Python-$VERSION
-    export PATH="$PYTHON_TARGET_DIR:$PATH"
-    cp -p ../Python-$VERSION-build/python python
-    cp -p ../Python-$VERSION-build/python python_for_build
+    export PATH="$PYTHON_BUILD_DIR:$PATH"
     cp -p ../Python-$VERSION-build/Parser/pgen Parser/pgen
     cp -p ../Python-$VERSION-build/Parser/pgen Parser/pgen_for_build
-    mkdir binaries && cp -p python binaries
 
     CC=gcc\
-    RANLIB=ranlib ./configure
+    RANLIB=ranlib\
+    CFLAGS="-I/tools/include"\
+    LDFLAGS="-L/tools/lib"\
+    LD_LIBRARY_PATH="/tools/lib"\
+    LIBRARY_PATH="/tools/lib"\
+    C_INCLUDE_PATH="/tools/include" ./configure
     C_INCLUDE_PATH="/tools/include"\
     LD_LIBRARY_PATH="/tools/lib"\
     LIBRARY_PATH="/tools/lib" make python Parser/pgen
 
-#    mkdir binaries && cp -p python binaries
-#    mv python python_for_build
+    mkdir binaries && cp -p python binaries
+    mv python python_for_build
     mv Parser/pgen Parser/pgen_for_build
     make distclean
 
     ac_cv_file__dev_ptmx=no\
     ac_cv_file__dev_ptc=no\
     ac_cv_have_long_long_format=yes\
-    PYTHON_FOR_BUILD=$PYTHON_TARGET_DIR/bin/python\
+    PYTHON_FOR_BUILD=$PYTHON_BUILD_DIR/python\
     ./configure --build=$SCUDUM_BARCH --host=$ARCH_TARGET --prefix=$PREFIX --enable-shared --disable-ipv6
 else
     ./configure --prefix=$PREFIX --enable-shared
