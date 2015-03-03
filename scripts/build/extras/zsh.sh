d3ca86f18e7ce3e50e1cb0c924562bb74a0291b6
joamag <joamag@gmail.com>
2015-03-03 12:51:45 +0000

    new zsh

--- scripts/build/extras/zsh.sh
+++ scripts/build/extras/zsh.sh
@@ -6,7 +6,8 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "http://www.zsh.org/pub/old/zsh-$VERSION.tar.bz2"
+rget "http://downloads.sourceforge.net/zsh/zsh-$VERSION.tar.bz2"\
+    "http://www.zsh.org/pub/old/zsh-$VERSION.tar.bz2"
 rm -rf zsh-$VERSION && tar -jxf "zsh-$VERSION.tar.bz2"
 rm -f "zsh-$VERSION.tar.bz2"
 cd zsh-$VERSION
