af5f1d33da465a2dc11d57000dafbcbc99fda505
joamag <joamag@gmail.com>
2015-03-03 17:26:18 +0000

    better code

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -58,9 +58,11 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     C_INCLUDE_PATH="/tools/include"\
     LD_LIBRARY_PATH="/tools/lib"\
     LIBRARY_PATH="/tools/lib" make python Parser/pgen
-
+    
     cp -p python python_for_build
     cp -p Parser/pgen Parser/pgen_for_build
+    
+    make distclean
     
     wget "https://raw.githubusercontent.com/hivesolutions/patches/master/python/Python-$VERSION-xcompile.patch"
     patch -Np1 -i Python-$VERSION-xcompile.patch
@@ -69,7 +71,9 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     ac_cv_file__dev_ptc=no\
     ac_cv_have_long_long_format=yes\
     PYTHON_FOR_BUILD=$PYTHON_WORK_DIR/python_for_build\
-    PYTHON_FOR_BUILD=$PYTHON_WORK_DIR/Parser/pgen_for_build\
+    PGEN_FOR_BUILD=$PYTHON_WORK_DIR/Parser/pgen_for_build\
+    HOSTPYTHON=$PYTHON_WORK_DIR/python_for_build\
+    HOSTPGEN=$PYTHON_WORK_DIR/Parser/pgen_for_build\
     ./configure --build=$SCUDUM_HOST --host=$ARCH_TARGET --prefix=$PREFIX --enable-shared --disable-ipv6
     make HOSTPYTHON=$PYTHON_WORK_DIR/python_for_build HOSTPGEN=$PYTHON_WORK_DIR/Parser/pgen_for_build
     make HOSTPYTHON=$PYTHON_WORK_DIR/python_for_build HOSTPGEN=$PYTHON_WORK_DIR/Parser/pgen_for_build install
