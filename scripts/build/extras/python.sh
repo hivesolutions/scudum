822c6600f0f6871aff272b2a294886db835cd2ad
joamag <joamag@gmail.com>
2015-03-03 15:45:01 +0000

    new code for python

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -18,6 +18,7 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     cp -rp Python-$VERSION Python-$VERSION-build
     cd Python-$VERSION-build
     PYTHON_BUILD_DIR=$(pwd)
+    PYTHON_TARGET_DIR=$(mktemp -d)
 
     CC=gcc\
     RANLIB=ranlib\
@@ -25,16 +26,17 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     LDFLAGS="-L/tools/lib"\
     LD_LIBRARY_PATH="/tools/lib"\
     LIBRARY_PATH="/tools/lib"\
-    C_INCLUDE_PATH="/tools/include" ./configure
+    C_INCLUDE_PATH="/tools/include" ./configure --prefix=$PYTHON_TARGET_DIR
     C_INCLUDE_PATH="/tools/include"\
     LD_LIBRARY_PATH="/tools/lib"\
-    LIBRARY_PATH="/tools/lib" make python Parser/pgen sharedmods
+    LIBRARY_PATH="/tools/lib" make
+    make install
 
     cp -p python python_for_build
     cp -p Parser/pgen Parser/pgen_for_build
 
     cd ../Python-$VERSION
-    export PATH="$PYTHON_BUILD_DIR:$PATH"
+    export PATH="$PYTHON_TARGET_DIR:$PATH"
     cp -p ../Python-$VERSION-build/Parser/pgen Parser/pgen
     cp -p ../Python-$VERSION-build/Parser/pgen Parser/pgen_for_build
 
@@ -57,6 +59,7 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     ac_cv_file__dev_ptmx=no\
     ac_cv_file__dev_ptc=no\
     ac_cv_have_long_long_format=yes\
+    PYTHON_FOR_BUILD=$PYTHON_TARGET_DIR/bin/python\
     ./configure --build=$SCUDUM_BARCH --host=$ARCH_TARGET --prefix=$PREFIX --enable-shared --disable-ipv6
 else
     ./configure --prefix=$PREFIX --enable-shared
