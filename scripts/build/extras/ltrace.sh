9f2edd243621bf27bfb8b1562241afe7a3d37578
joamag <joamag@gmail.com>
2015-03-04 00:40:50 +0000

    new flags

--- scripts/build/extras/ltrace.sh
+++ scripts/build/extras/ltrace.sh
@@ -8,8 +8,10 @@ source $DIR/common.sh
 
 depends "libelf"
 
-export CFLAGS="-O2 -Wno-unused-local-typedefs"
-export CXXFLAGS="-O2 -Wno-unused-local-typedefs"
+if [ -z "$CFLAGS" ]; then export CFLAGS="-O2"; fi
+if [ -z "$CXXFLAGS" ]; then export CXXFLAGS="-O2"; fi
+export CFLAGS="$CFLAGS -Wno-unused-local-typedefs"
+export CXXFLAGS="$CXXFLAGS -Wno-unused-local-typedefs"
 
 wget "http://ltrace.org/ltrace_$VERSION.orig.tar.bz2"
 rm -rf ltrace-$VERSION && tar -jxf "ltrace_$VERSION.orig.tar.bz2"
