1e77f19bf2858a77f4d3f05f709735f052b37199
joamag <joamag@gmail.com>
2015-03-04 18:11:49 +0000

    new scudum cross support

--- scripts/build/extras/pip.sh
+++ scripts/build/extras/pip.sh
@@ -12,5 +12,9 @@ else
     ARGS="--user"
 fi
 
+if [ "$SCUDUM_CROSS" == "1" ]; then
+    ARGS="$ARGS --target /usr/lib/python2.7/site-packages -–install-option=\"–-install-scripts=/usr/bin\" -–install-option=\"–-prefix=/usr\""
+fi
+
 rm -f get-pip.py && wget "https://bootstrap.pypa.io/get-pip.py"
 python get-pip.py $ARGS
