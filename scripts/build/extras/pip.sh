016f57f948b251437523685851fcd59712eb555a
joamag <joamag@gmail.com>
2015-03-05 15:55:11 +0000

    new pip code

--- scripts/build/extras/pip.sh
+++ scripts/build/extras/pip.sh
@@ -13,6 +13,11 @@ rm -rf pip-6.0.8 && tar -zxf "pip-6.0.8.tar.gz"
 rm -f "pip-6.0.8.tar.gz"
 cd pip-6.0.8
 
+if [ "$SCUDUM_CROSS" == "1" ]; then
+    rm -f get-pip.py && wget "https://bootstrap.pypa.io/get-pip.py"
+    python get-pip.py $ARGS
+fi
+
 if [ "$PREFIX" == "/usr" ]; then
     if [ "$SCUDUM_CROSS" == "1" ]; then
         PYTHON_VERSION=$(python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";)
