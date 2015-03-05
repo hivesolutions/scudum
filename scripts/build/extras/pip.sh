0ec756dc90e8d86418b654d410a6959d4a4b6d1c
joamag <joamag@gmail.com>
2015-03-05 16:40:23 +0000

    better pip structure

--- scripts/build/extras/pip.sh
+++ scripts/build/extras/pip.sh
@@ -20,13 +20,15 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     PYTHON_LIB=/usr/lib/python$PYTHON_VERSION/site-packages
     PYTHON_TOOLS_LIB=/tools/lib/python$PYTHON_VERSION/site-packages
 
-    rm -rf $PYTHON_LIB/setuptools*
-    rm -rf $PYTHON_LIB/pip*
-
-    mv $PYTHON_TOOLS_LIB/setuptools* $PYTHON_LIB
-    mv $PYTHON_TOOLS_LIB/pip* $PYTHON_LIB
+    rm -rf $PYTHON_LIB/pkg_resources* && mv $PYTHON_TOOLS_LIB/pkg_resources* $PYTHON_LIB
+    rm -rf $PYTHON_LIB/easy_install* && mv $PYTHON_TOOLS_LIB/easy_install* $PYTHON_LIB
+    rm -rf $PYTHON_LIB/setuptools* && mv $PYTHON_TOOLS_LIB/setuptools* $PYTHON_LIB
+    rm -rf $PYTHON_LIB/_markerlib* && mv $PYTHON_TOOLS_LIB/_markerlib* $PYTHON_LIB
+    rm -rf $PYTHON_LIB/pip* && mv $PYTHON_TOOLS_LIB/pip* $PYTHON_LIB
 
     mv /tools/bin/pip{,2*} /usr/bin
+    mv /tools/bin/easy_install{,2*} /usr/bin
 
     sed -i 's/\/tools\/bin/\/usr\/bin/g' /usr/bin/pip{,2*}
+    sed -i 's/\/tools\/bin/\/usr\/bin/g' /usr/bin/easy_install{,2*}
 fi
