e8231805699dfb370aab2458b0eb1551a48fe472
joamag <joamag@gmail.com>
2015-03-05 18:53:34 +0000

    small code fix

--- util/armord/src/armord/__init__.py
+++ util/armord/src/armord/__init__.py
@@ -34,10 +34,10 @@ __copyright__ = "Copyright (c) 2008-2015 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+from . import controllers
 from . import models
-from . import views
 from . import main
 
+from .controllers import *
 from .models import *
-from .views import *
 from .main import app
