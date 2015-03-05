c4c7785ab4c836381773e9e7c5e223e77b83b86a
joamag <joamag@gmail.com>
2015-03-05 15:29:04 +0000

    new pip install support

--- scripts/build/extras/pip.sh
+++ scripts/build/extras/pip.sh
@@ -1,3 +1,5 @@
+VERSION=${VERSION-6.0.8}
+
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 set -e +h
@@ -6,15 +8,20 @@ source $DIR/common.sh
 
 depends "python"
 
+wget "https://pypi.python.org/packages/source/p/pip/pip-6.0.8.tar.gz"
+rm -rf pip-6.0.8 && tar -zxf "pip-6.0.8.tar.gz"
+rm -f "pip-6.0.8.tar.gz"
+cd pip-6.0.8
+
 if [ "$PREFIX" == "/usr" ]; then
-    ARGS=""
+    if [ "$SCUDUM_CROSS" == "1" ]; then
+        PYTHON_VERSION=$(python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";)
+        ARGS="–-install-scripts=/usr/bin --install-purelib=/usr/lib/python$PYTHON_VERSION/site-packages"
+    else
+        ARGS=""
+    fi
 else
     ARGS="--user"
 fi
 
-if [ "$SCUDUM_CROSS" == "1" ]; then
-    ARGS="$ARGS --target /usr/lib/python2.7/site-packages -–install-option=\"–-install-scripts=/usr/bin\" -–install-option=\"–-prefix=/usr\""
-fi
-
-rm -f get-pip.py && wget "https://bootstrap.pypa.io/get-pip.py"
-python get-pip.py $ARGS
+python setup.py install $ARGS
