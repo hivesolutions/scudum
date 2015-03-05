a03f1d65554f7732a95e2d1117eebbb6940b9731
joamag <joamag@gmail.com>
2015-03-05 11:53:19 +0000

    new alternatives for nano

--- scripts/build/system/nano.sh
+++ scripts/build/system/nano.sh
@@ -1,8 +1,13 @@
 VERSION=${VERSION-2.2.6}
 
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
 set -e +h
 
-wget --no-check-certificate "http://www.nano-editor.org/dist/v2.2/nano-$VERSION.tar.gz"
+source $DIR/../base/functions.sh
+
+rgeti "http://ftp.gnu.org/pub/gnu/nano/nano-$VERSION.tar.gz"\
+    "http://www.nano-editor.org/dist/v2.2/nano-$VERSION.tar.gz"
 rm -rf nano-$VERSION && tar -zxf "nano-$VERSION.tar.gz"
 rm -f "nano-$VERSION.tar.gz"
 cd nano-$VERSION
