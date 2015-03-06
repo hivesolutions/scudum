a749512d9749d9f92e9ddc8a6a68d1c16b025424
joamag <joamag@gmail.com>
2015-03-06 20:04:04 +0000

    older version

--- scripts/build/extras/mongodb.built.sh
+++ scripts/build/extras/mongodb.built.sh
@@ -6,9 +6,9 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "https://fastdl.mongodb.org/src/mongodb-linux-x86_64-$VERSION.tar.gz"
-rm -rf mongodb-linux-x86_64-$VERSION && tar -zxf "mongodb-linux-x86_64-$VERSION.tar.gz"
-rm -f "mongodb-linux-x86_64-$VERSION.tar.gz"
+wget "https://fastdl.mongodb.org/src/mongodb-linux-x86_64-$VERSION.tgz"
+rm -rf mongodb-linux-x86_64-$VERSION && tar -zxf "mongodb-linux-x86_64-$VERSION.tgz"
+rm -f "mongodb-linux-x86_64-$VERSION.tgz"
 cd mongodb-linux-x86_64-$VERSION
 
 mv bin/* $PREFIX/bin
