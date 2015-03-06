1dad287c745f1beb8e6d5440d7d26c41ec96c1ce
joamag <joamag@gmail.com>
2015-03-06 14:46:08 +0000

    fixed issue in unmounting cifs

--- util/armorc/src/armorc/base.py
+++ util/armorc/src/armorc/base.py
@@ -130,12 +130,7 @@ class ArmorClient(object):
         pipe.wait()
 
     def unmount_cifs(self):
-        cifs_path = self.domain_info["cifs_path"]
-        cifs_username = self.domain_info["cifs_username"]
-        cifs_password = self.domain_info["cifs_password"]
-        if not cifs_path: return
-        if not cifs_username: return
-        if not cifs_password: return
+        if not os.path.ismount(self.data_path): return
         print("Unmounting CIFS share from '%s'" % self.data_path)
         pipe = subprocess.Popen(["umount", self.data_path])
         pipe.wait()
