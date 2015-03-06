083a17aa0564f190e092fbc79d28f5571b02384f
joamag <joamag@gmail.com>
2015-03-06 16:13:32 +0000

    new version

--- util/armorc/setup.py
+++ util/armorc/setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "armorc",
-    version = "0.1.26",
+    version = "0.1.27",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Armor Client",
