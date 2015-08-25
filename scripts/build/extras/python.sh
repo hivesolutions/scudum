193932a67e049f0fa13f4bcc2030297e799b5893
joamag <joamag@gmail.com>
2015-08-25 12:51:54 +0100

    new python version

--- scripts/build/extras/python.sh
+++ scripts/build/extras/python.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-2.7.9}
+VERSION=${VERSION-2.7.10}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
--- scripts/build/extras/python3.sh
+++ scripts/build/extras/python3.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-3.4.2}
+VERSION=${VERSION-3.4.3}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
