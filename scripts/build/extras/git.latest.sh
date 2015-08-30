2bff0a28db739aeeb82c9c23c56d891987188adb
joamag <joamag@gmail.com>
2015-08-30 20:07:32 +0100

    new git latest support

---
+++ scripts/build/extras/git.latest.sh
@@ -0,0 +1,22 @@
+VERSION=${VERSION-2.5.1}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget --no-check-certificate "https://www.kernel.org/pub/software/scm/git/git-$VERSION.tar.xz"
+rm -rf git-$VERSION && tar -Jxf "git-$VERSION.tar.xz"
+rm -f "git-$VERSION.tar.xz"
+cd git-$VERSION
+
+sed -i 's/as_fn_error ()/as_fn_error ()\n{\nreturn 0\n}\nold_as_fn_error ()\n/' configure
+
+if [ "$SCUDUM_CROSS" == "1" ]; then
+    ./configure --host=$ARCH_TARGET --prefix=/usr --with-perl=/tools/bin/perl
+else
+    ./configure --host=$ARCH_TARGET --prefix=/usr
+fi
+
+make && make install
