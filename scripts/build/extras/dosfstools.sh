4a467340f772130dc4a51f722eb2604fd811ac8b
joamag <joamag@hive.pt>
2015-11-12 17:25:58 +0000

    new version

--- scripts/build/extras/dosfstools.sh
+++ scripts/build/extras/dosfstools.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-3.0.26}
+VERSION=${VERSION-3.0.28}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "http://daniel-baumann.ch/files/software/dosfstools/dosfstools-$VERSION.tar.gz"
+wget "https://github.com/dosfstools/dosfstools/releases/download/v$VERSION/dosfstools-$VERSION.tar.gz"
 rm -rf dosfstools-$VERSION && tar -zxf "dosfstools-$VERSION.tar.gz"
 rm -f "dosfstools-$VERSION.tar.gz"
 cd dosfstools-$VERSION
