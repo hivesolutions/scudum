1bb6983061b27454f27cb6c3542ceeb9d96b7c16
joamag <joamag@gmail.com>
2015-12-13 19:04:13 +0000

    new iperf support

---
+++ scripts/build/extras/iperf3.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-3.1.1}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "unzip"
+
+wget "https://github.com/esnet/iperf/archive/$VERSION.zip"
+rm -rf iperf-$VERSION && unzip "iperf-$VERSION.zip"
+rm -f "iperf-$VERSION.zip"
+cd iperf-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
