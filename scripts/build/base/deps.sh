3c9d07d999a9386373ed04552155a7f850cc7709
joamag <joamag@hive.pt>
2015-12-23 14:06:19 +0000

    new dependencies

--- scripts/build/base/deps.sh
+++ scripts/build/base/deps.sh
@@ -2,7 +2,7 @@ set -e +h
 
 if type apt-get &> /dev/null; then
     apt-get -y install wget g++ make bison flex gawk gperf\
-        texinfo libncurses5 libncurses5-dev libssl-dev
+        xz-utils texinfo libncurses5 libncurses5-dev libssl-dev
 elif type scu &> /dev/null; then
     echo "scudum: dependencies already installed"
 else
