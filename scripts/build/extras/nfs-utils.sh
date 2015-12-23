a0072f1a9ec3f9995b2d9c0d7090b0909b377261
joamag <joamag@hive.pt>
2015-12-23 13:10:13 +0000

    new nfs utils

---
+++ scripts/build/extras/nfs-utils.sh
@@ -0,0 +1,22 @@
+VERSION=${VERSION-1.3.3}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "libtirpc"
+
+wget "http://downloads.sourceforge.net/nfs/nfs-utils-$VERSION.tar.bz2"
+rm -rf nfs-utils-$VERSION && tar -jxf "nfs-utils-$VERSION.tar.bz2"
+rm -f "nfs-utils-$VERSION.tar.bz2"
+cd nfs-utils-$VERSION
+
+./configure\
+    --prefix=$PREFIX\
+    --without-tcp-wrappers\
+    --disable-nfsv4\
+    --disable-gss
+
+make && make install
