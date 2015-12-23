0fae3778ff0652cbe3c71e47d098d5c3256a8b42
joamag <joamag@gmail.com>
2015-12-23 09:41:26 +0000

    new url

--- scripts/build/extras/freetype.sh
+++ scripts/build/extras/freetype.sh
@@ -8,7 +8,8 @@ source $DIR/common.sh
 
 depends "libpng"
 
-rget "http://download.savannah.gnu.org/releases/freetype/freetype-$VERSION.tar.gz"\
+rget "http://downloads.sourceforge.net/freetype2/freetype-$VERSION.tar.gz"\
+    "http://download.savannah.gnu.org/releases/freetype/freetype-$VERSION.tar.gz"\
     "http://mirrors.zerg.biz/nongnu/freetype/freetype-$VERSION.tar.gz"
 rm -rf freetype-$VERSION && tar -zxf "freetype-$VERSION.tar.gz"
 rm -f "freetype-$VERSION.tar.gz"
