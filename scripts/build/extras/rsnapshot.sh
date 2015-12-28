429bbaefc399f752bf4c5f33df1f0112f6663a57
joamag <joamag@gmail.com>
2015-12-28 10:34:10 +0000

    new rsnapshort dependency

---
+++ scripts/build/extras/rsnapshot.sh
@@ -0,0 +1,17 @@
+VERSION=${VERSION-1.4.1}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "rsync"
+
+wget "http://rsnapshot.org/downloads/rsnapshot-$VERSION.tar.gz"
+rm -rf rsnapshot-$VERSION && tar -zxf "rsnapshot-$VERSION.tar.gz"
+rm -f "rsnapshot-$VERSION.tar.gz"
+cd rsnapshot-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
