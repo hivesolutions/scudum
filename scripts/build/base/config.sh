f3fa431f1260af1e0bed6b8a3e069646819b031d
joamag <joamag@gmail.com>
2016-02-27 16:55:06 +0000

    new latest version

--- scripts/build/base/config.sh
+++ scripts/build/base/config.sh
@@ -131,7 +131,7 @@ fi
 case "$GCC_FLAVOUR" in
     latest)
         export GCC_BUILD_BINARY="gcc.latest"
-        export GCC_BUILD_VERSION="4.9.3"
+        export GCC_BUILD_VERSION="5.3.0"
         ;;
     normal)
         export GCC_BUILD_BINARY="gcc"
