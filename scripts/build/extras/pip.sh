b54a4510798101419c01fb47532d4ec1c3afd044
joamag <joamag@hive.pt>
2016-02-29 18:41:03 +0000

    small code fix

--- scripts/build/extras/pip.sh
+++ scripts/build/extras/pip.sh
@@ -20,19 +20,19 @@ if [ "$SCUDUM_CROSS" == "1" ]; then
     PYTHON_LIB=/usr/lib/python$PYTHON_VERSION/site-packages
     PYTHON_TOOLS_LIB=/tools/lib/python$PYTHON_VERSION/site-packages
 
-    if [ -e $PYTHON_TOOLS_LIB/pkg_resources* ]; then
+    if [ -e $PYTHON_TOOLS_LIB/pkg_resources ]; then
         rm -rf $PYTHON_LIB/pkg_resources* && mv $PYTHON_TOOLS_LIB/pkg_resources* $PYTHON_LIB
     fi
-    if [ -e $PYTHON_TOOLS_LIB/easy_install* ]; then
+    if [ -e $PYTHON_TOOLS_LIB/easy_install ]; then
         rm -rf $PYTHON_LIB/easy_install* && mv $PYTHON_TOOLS_LIB/easy_install* $PYTHON_LIB
     fi
-    if [ -e $PYTHON_TOOLS_LIB/setuptools* ]; then
+    if [ -e $PYTHON_TOOLS_LIB/setuptools ]; then
         rm -rf $PYTHON_LIB/setuptools* && mv $PYTHON_TOOLS_LIB/setuptools* $PYTHON_LIB
     fi
-    if [ -e $PYTHON_TOOLS_LIB/_markerlib* ]; then
+    if [ -e $PYTHON_TOOLS_LIB/_markerlib ]; then
         rm -rf $PYTHON_LIB/_markerlib* && mv $PYTHON_TOOLS_LIB/_markerlib* $PYTHON_LIB
     fi
-    if [ -e $PYTHON_TOOLS_LIB/pip* ]; then
+    if [ -e $PYTHON_TOOLS_LIB/pip ]; then
         rm -rf $PYTHON_LIB/pip* && mv $PYTHON_TOOLS_LIB/pip* $PYTHON_LIB
     fi
 
