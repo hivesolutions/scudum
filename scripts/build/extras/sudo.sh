0f43e5374ad02b4312dbeb347b505f0264650556
joamag <joamag@hive.pt>
2016-02-29 21:00:23 +0000

    new data source

--- scripts/build/extras/sudo.sh
+++ scripts/build/extras/sudo.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "http://www.tux.org/pub/security/sudo/sudo-$VERSION.tar.gz"\
+rget "http://ftp3.usa.openbsd.org/pub/sudo/sudo-$VERSION.tar.gz"\
     "http://www.sudo.ws/sudo/dist/sudo-$VERSION.tar.gz"
 rm -rf sudo-$VERSION && tar -zxf "sudo-$VERSION.tar.gz"
 rm -f "sudo-$VERSION.tar.gz"
