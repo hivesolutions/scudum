ef4fcb23266244e866901d5fcee4e9abe84298a0
joamag <joamag@gmail.com>
2016-02-29 01:07:45 +0000

    new patch

--- scripts/build/tools/ncurses.sh
+++ scripts/build/tools/ncurses.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-6.0}
+VERSION=${VERSION-5.9}
 
 set -e +h
 
@@ -6,6 +6,9 @@ wget "ftp://ftp.gnu.org/gnu/ncurses/ncurses-$VERSION.tar.gz"
 rm -rf ncurses-$VERSION && tar -zxf "ncurses-$VERSION.tar.gz"
 rm -f "ncurses-$VERSION.tar.gz"
 cd ncurses-$VERSION
+
+wget "http://archive.hive.pt/files/lfs/patches/ncurses-$VERSION-gcc_5-1.patch"
+patch -Np1 -i ncurses-$VERSION-gcc_5-1.patch
 
 ./configure --prefix=$PREFIX --with-shared\
     --without-debug --without-ada --enable-widec\
