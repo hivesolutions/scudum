7ba605836d6f38239d1eafeb1e55c71342995422
joamag <joamag@hive.pt>
2016-03-24 12:14:30 +0000

    new owner support for postgres

--- util/armorc/src/armorc/base.py
+++ util/armorc/src/armorc/base.py
@@ -161,7 +161,7 @@ class ArmorClient(object):
         pipe = subprocess.Popen(
             [
                 "mount", "-t", "cifs", "-o",
-                "nosetuids,noperm,noacl,dir_mode=0700,file_mode=0700,username=" + cifs_username + ",password=" + cifs_password,
+                "nosetuids,noperm,noacl,forceuid,forcegid,uid=999,gid=999,dir_mode=0700,file_mode=0700,username=" + cifs_username + ",password=" + cifs_password,
                 cifs_path, self.owner_path
             ]
         )
