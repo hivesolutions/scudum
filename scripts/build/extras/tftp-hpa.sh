41a917e20aa3023f6651005de1571d02fe743ac2
joamag <joamag@hive.pt>
2016-06-09 19:27:31 +0100

    new version

--- scripts/build/extras/tftp-hpa.sh
+++ scripts/build/extras/tftp-hpa.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-0.40}
+VERSION=${VERSION-5.2}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "https://www.kernel.org/pub/software/network/tftp/tftp-hpa-$VERSION.tar.gz"
+wget "https://www.kernel.org/pub/software/network/tftp/tftp-hpa/tftp-hpa-$VERSION.tar.gz"
 rm -rf tftp-hpa-$VERSION && tar -zxf "tftp-hpa-$VERSION.tar.gz"
 rm -f "tftp-hpa-$VERSION.tar.gz"
 cd tftp-hpa-$VERSION
