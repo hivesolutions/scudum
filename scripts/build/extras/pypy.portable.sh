3dbfe6b55d9220d42a60da56288d87ed1b11bb11
joamag <joamag@hive.pt>
2016-07-06 17:39:26 +0100

    new portable pyyp support

---
+++ scripts/build/extras/pypy.portable.sh
@@ -0,0 +1,21 @@
+VERSION=${VERSION-5.3.1}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "expat" "libffi" "libtinfo"
+
+wget "https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-$VERSION-linux_x86_64-portable.tar.bz2"
+rm -rf pypy-$VERSION-linux_x86_64-portable && tar -jxf "pypy-$VERSION-linux_x86_64-portable.tar.bz2"
+
+mv -v pypy-$VERSION-linux_x86_64-portable.tar.bz2 $PREFIX/lib
+cd $PREFIX/lib
+
+rm -rvf pypy && tar -jxf pypy-$VERSION-linux_x86_64-portable.tar.bz2
+mv pypy-$VERSION-linux_x86_64-portable pypy
+rm -vf pypy-$VERSION-linux_x86_64-portable.tar.bz2
+
+ln -svf ../lib/pypy/bin/pypy $PREFIX/bin/pypy
