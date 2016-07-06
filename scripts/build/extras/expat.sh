d6c3640c040c71cbc4502213b8299aa7d4abc852
joamag <joamag@hive.pt>
2016-07-06 16:00:30 +0100

    new file version

--- scripts/build/extras/expat.sh
+++ scripts/build/extras/expat.sh
@@ -6,9 +6,9 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "http://downloads.sourceforge.net/expat/expat-$VERSION.tar.gz"
-rm -rf expat-$VERSION && tar -zxf "expat-$VERSION.tar.gz"
-rm -f "expat-$VERSION.tar.gz"
+wget "http://downloads.sourceforge.net/expat/expat-$VERSION.tar.bz2"
+rm -rf expat-$VERSION && tar -jxf "expat-$VERSION.tar.bz2"
+rm -f "expat-$VERSION.tar.bz2"
 cd expat-$VERSION
 
 ./configure --prefix=$PREFIX --disable-static
