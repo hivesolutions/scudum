1c688a0679825ad45d2d3774d6866a71e6b3b100
joamag <joamag@hive.pt>
2016-07-06 16:03:03 +0100

    small code change

--- scripts/build/extras/pypy.sh
+++ scripts/build/extras/pypy.sh
@@ -8,20 +8,20 @@ source $DIR/common.sh
 
 depends "python" "expat" "libffi"
 
-wget "https://bitbucket.org/pypy/pypy/downloads/pypy2-$VERSION-src.tar.bz2"
-rm -rf pypy2-$VERSION-src && tar -jxf "pypy2-$VERSION-src.tar.bz2"
-rm -f "pypy2-$VERSION-src.tar.bz2"
-cd pypy2-$VERSION-src
+wget "https://bitbucket.org/pypy/pypy/downloads/pypy2-v$VERSION-src.tar.bz2"
+rm -rf pypy2-v$VERSION-src && tar -jxf "pypy2-v$VERSION-src.tar.bz2"
+rm -f "pypy2-v$VERSION-src.tar.bz2"
+cd pypy2-v$VERSION-src
 
 cd pypy/goal
 python ../../rpython/bin/rpython -Ojit targetpypystandalone
 cd ../tool/release
-python package.py --targetdir pypy2-$VERSION.tar.bz2 --archive-name pypy --without-tk || true
+python package.py --targetdir pypy2-v$VERSION.tar.bz2 --archive-name pypy --without-tk || true
 
-mv -v pypy2-$VERSION.tar.bz2 $PREFIX/lib
+mv -v pypy2-v$VERSION.tar.bz2 $PREFIX/lib
 cd $PREFIX/lib
 
-rm -rvf pypy && tar -jxf pypy2-$VERSION.tar.bz2
-rm -vf pypy2-$VERSION.tar.bz2
+rm -rvf pypy && tar -jxf pypy2-v$VERSION.tar.bz2
+rm -vf pypy2-v$VERSION.tar.bz2
 
 ln -sv ../lib/pypy/bin/pypy $PREFIX/bin/pypy
