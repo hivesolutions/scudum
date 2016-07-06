c6a9c17690a6141240fac389ad3fb89e468020fb
joamag <joamag@hive.pt>
2016-07-06 16:02:16 +0100

    new value

--- scripts/build/extras/pypy.sh
+++ scripts/build/extras/pypy.sh
@@ -8,20 +8,20 @@ source $DIR/common.sh
 
 depends "python" "expat" "libffi"
 
-wget "https://bitbucket.org/pypy/pypy/downloads/pypy-$VERSION-src.tar.bz2"
-rm -rf pypy-$VERSION-src && tar -jxf "pypy-$VERSION-src.tar.bz2"
-rm -f "pypy-$VERSION-src.tar.bz2"
-cd pypy-$VERSION-src
+wget "https://bitbucket.org/pypy/pypy/downloads/pypy2-$VERSION-src.tar.bz2"
+rm -rf pypy2-$VERSION-src && tar -jxf "pypy2-$VERSION-src.tar.bz2"
+rm -f "pypy2-$VERSION-src.tar.bz2"
+cd pypy2-$VERSION-src
 
 cd pypy/goal
 python ../../rpython/bin/rpython -Ojit targetpypystandalone
 cd ../tool/release
-python package.py --targetdir pypy-$VERSION.tar.bz2 --archive-name pypy --without-tk || true
+python package.py --targetdir pypy2-$VERSION.tar.bz2 --archive-name pypy --without-tk || true
 
-mv -v pypy-$VERSION.tar.bz2 $PREFIX/lib
+mv -v pypy2-$VERSION.tar.bz2 $PREFIX/lib
 cd $PREFIX/lib
 
-rm -rvf pypy && tar -jxf pypy-$VERSION.tar.bz2
-rm -vf pypy-$VERSION.tar.bz2
+rm -rvf pypy && tar -jxf pypy2-$VERSION.tar.bz2
+rm -vf pypy2-$VERSION.tar.bz2
 
 ln -sv ../lib/pypy/bin/pypy $PREFIX/bin/pypy
