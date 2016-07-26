86845a9967590b1035c5ea2c8182a30810b339d6
joamag <joamag@hive.pt>
2016-07-26 16:33:42 +0100

    new build code

--- build.sh
+++ build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+LINUX_X64=${LINUX_X64-1}
+RASP_ARM6=${RASP_ARM6-0}
+RASP_ARM7=${RASP_ARM7-0}
+
 set -e +h
 
 apt-get update
 
-scudum root && scudum deploy
+if [ "$LINUX_X64" == "1" ]; then
+    scudum root && scudum deploy
+fi
 
-SCUDUM_ARCH=arm6 SCUDUM_VENDOR=rasp SCUDUM_SYSTEM=linux-gnueabihf \
-GCC_BUILD_ARCH=armv6zk GCC_BUILD_TUNE=arm1176jzf-s GCC_BUILD_FPU=vfp \
-GCC_BUILD_FLOAT=hard scudum root && scudum deploy
+if [ "$RASP_ARM6" == "1" ]; then
+    SCUDUM_ARCH=arm6 SCUDUM_VENDOR=rasp SCUDUM_SYSTEM=linux-gnueabihf \
+    GCC_BUILD_ARCH=armv6zk GCC_BUILD_TUNE=arm1176jzf-s GCC_BUILD_FPU=vfp \
+    GCC_BUILD_FLOAT=hard scudum root && scudum deploy
+fi
 
-SCUDUM_ARCH=arm7 SCUDUM_VENDOR=rasp SCUDUM_SYSTEM=linux-gnueabihf \
-GCC_BUILD_ARCH=armv7-a GCC_BUILD_TUNE=cortex-a7 GCC_BUILD_FPU=neon-vfpv4 \
-GCC_BUILD_FLOAT=hard scudum root && scudum deploy
+if [ "$RASP_ARM7" == "1" ]; then
+    SCUDUM_ARCH=arm7 SCUDUM_VENDOR=rasp SCUDUM_SYSTEM=linux-gnueabihf \
+    GCC_BUILD_ARCH=armv7-a GCC_BUILD_TUNE=cortex-a7 GCC_BUILD_FPU=neon-vfpv4 \
+    GCC_BUILD_FLOAT=hard scudum root && scudum deploy
+fi
