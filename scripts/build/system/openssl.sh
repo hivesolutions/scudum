13c40a4609b4e1cff5b58f0c7e72a57a58bccb4a
joamag <joamag@hive.pt>
2016-07-26 16:31:43 +0100

    new openssl versions

--- scripts/build/system/openssl.sh
+++ scripts/build/system/openssl.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.0.2e}
+VERSION=${VERSION-1.0.2d}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
--- scripts/build/tools/openssl.sh
+++ scripts/build/tools/openssl.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.0.2e}
+VERSION=${VERSION-1.0.2t}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
