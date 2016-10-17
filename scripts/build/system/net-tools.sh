068edc23295537a28921eaea088404b3c79a728b
joamag <joamag@gmail.com>
2016-10-17 18:22:12 +0100

    changed net toosl support

--- scripts/build/system/net-tools.sh
+++ scripts/build/system/net-tools.sh
@@ -7,9 +7,6 @@ rm -rf net-tools-$VERSION && tar -zxf "net-tools-$VERSION.tar.gz"
 rm -f "net-tools-$VERSION.tar.gz"
 cd net-tools-$VERSION
 
-wget --no-check-certificate http://archive.hive.pt/files/lfs/patches/net-tools-$VERSION-remove_dups-1.patch
-patch -Np1 -i net-tools-$VERSION-remove_dups-1.patch
-
 if [ "$SCUDUM_CROSS" == "1" ]; then
     yes "" | ./configure.sh config.in > /dev/null
     make CC="$CC" LD="$LD" && make install
