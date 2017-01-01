db08a7610f3dc2ef6e3ab50e340392f275d9ac2b
joamag <joamag@gmail.com>
2017-01-01 13:53:20 +0000

    new unzip value

--- scripts/build/extras/unzip.sh
+++ scripts/build/extras/unzip.sh
@@ -12,15 +12,5 @@ rm -rf unzip$VERSION && tar -zxf "unzip$VERSION.tar.gz"
 rm -f "unzip$VERSION.tar.gz"
 cd unzip$VERSION
 
-case $SCUDUM_ARCH in
-    i?86)
-        sed -i -e 's/DASM_CRC"/DASM_CRC -DNO_LCHMOD"/' unix/Makefile
-        make -f unix/Makefile linux
-        ;;
-    *)
-        sed -i -e 's/CFLAGS="-O -Wall/& -DNO_LCHMOD/' unix/Makefile
-        make -f unix/Makefile linux_noasm
-        ;;
-esac
-
-make prefix=$PREFIX install
+make -f unix/Makefile generic
+make prefix=$PREFIX -f unix/Makefile install
