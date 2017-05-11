a6175bb9644e0a5e27d35a8e1672c5125251cbad
joamag <joamag@gmail.com>
2017-05-11 17:11:26 +0100

    new base url

--- scripts/build/extras/logrotate.sh
+++ scripts/build/extras/logrotate.sh
@@ -8,7 +8,7 @@ source $DIR/common.sh
 
 depends "popt"
 
-wget "https://fedorahosted.org/releases/l/o/logrotate/logrotate-$VERSION.tar.gz"
+wget "https://ftp.osuosl.org/pub/blfs/conglomeration/logrotate/logrotate-$VERSION.tar.gz"
 rm -rf logrotate-$VERSION && tar -zxf "logrotate-$VERSION.tar.gz"
 rm -f "logrotate-$VERSION.tar.gz"
 cd logrotate-$VERSION
