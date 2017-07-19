89b366b90efb1d20d24170c4dea6a46787314b57
joamag <joamag@gmail.com>
2017-07-19 23:50:58 +0100

    new nano version

--- scripts/build/system/nano.sh
+++ scripts/build/system/nano.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-2.7.0}
+VERSION=${VERSION-2.8.5}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -6,19 +6,13 @@ set -e +h
 
 source $DIR/../base/functions.sh
 
-rgeti "http://ftp.gnu.org/pub/gnu/nano/nano-$VERSION.tar.gz"\
-    "http://www.nano-editor.org/dist/v2.2/nano-$VERSION.tar.gz"
+wget --no-check-certificate "http://ftp.gnu.org/pub/gnu/nano/nano-$VERSION.tar.gz"
 rm -rf nano-$VERSION && tar -zxf "nano-$VERSION.tar.gz"
 rm -f "nano-$VERSION.tar.gz"
 cd nano-$VERSION
 
-./configure --host=$ARCH_TARGET --prefix=/usr
+./configure --host=$ARCH_TARGET --prefix=/usr --enable-utf8
 
 make && make install
 
 ln -svf nano /usr/bin/pico
-
-for file in /usr/share/nano/*.nanorc; do
-    sed -i 's/\\</\\b/g' $file
-    sed -i 's/\\>/\\b/g' $file
-done
