b5c20b6930b4ae006d0ca9a4fe80f15628c13da9
joamag <joamag@gmail.com>
2017-07-20 21:26:51 +0100

    new code

--- scripts/build/tools/bc.sh
+++ scripts/build/tools/bc.sh
@@ -9,6 +9,17 @@ rm -rf bc-$VERSION && tar -zxf "bc-$VERSION.tar.gz"
 rm -f "bc-$VERSION.tar.gz"
 cd bc-$VERSION
 
+cat > bc/fix-libmath_h << "EOF"
+#! /bin/bash
+sed -e '1   s/^/{"/'\
+    -e 's/$/",/'\
+    -e '2,$ s/^/"/'\
+    -e '$ d'\
+    -i libmath.h
+
+sed -e '$ s/$/0}/' -i libmath.h
+EOF
+
 ./configure --prefix=$PREFIX
 
 make && make install
