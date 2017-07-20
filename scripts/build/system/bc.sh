937a449d13fa171dd73c0c898c5900497aa74d05
joamag <joamag@gmail.com>
2017-07-21 00:15:15 +0100

    reverted version

--- scripts/build/system/bc.sh
+++ scripts/build/system/bc.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.07.1}
+VERSION=${VERSION-1.06}
 
 set -e +h
 
@@ -6,17 +6,6 @@ wget --no-check-certificate "http://ftp.gnu.org/gnu/bc/bc-$VERSION.tar.gz"
 rm -rf bc-$VERSION && tar -zxf "bc-$VERSION.tar.gz"
 rm -f "bc-$VERSION.tar.gz"
 cd bc-$VERSION
-
-cat > bc/fix-libmath_h << "EOF"
-#! /bin/bash
-sed -e '1   s/^/{"/'\
-    -e 's/$/",/'\
-    -e '2,$ s/^/"/'\
-    -e '$ d'\
-    -i libmath.h
-
-sed -e '$ s/$/0}/' -i libmath.h
-EOF
 
 ./configure --host=$ARCH_TARGET --prefix=/usr
 
--- scripts/build/tools/bc.sh
+++ scripts/build/tools/bc.sh
@@ -1,6 +1,6 @@
 [ "$SCUDUM_CROSS" == "0" ] && exit 0 || true
 
-VERSION=${VERSION-1.07.1}
+VERSION=${VERSION-1.06}
 
 set -e +h
 
@@ -8,17 +8,6 @@ wget "http://ftp.gnu.org/gnu/bc/bc-$VERSION.tar.gz"
 rm -rf bc-$VERSION && tar -zxf "bc-$VERSION.tar.gz"
 rm -f "bc-$VERSION.tar.gz"
 cd bc-$VERSION
-
-cat > bc/fix-libmath_h << "EOF"
-#! /bin/bash
-sed -e '1   s/^/{"/'\
-    -e 's/$/",/'\
-    -e '2,$ s/^/"/'\
-    -e '$ d'\
-    -i libmath.h
-
-sed -e '$ s/$/0}/' -i libmath.h
-EOF
 
 ./configure --prefix=$PREFIX
 
