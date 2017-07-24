a47a2bf9f859d0612b1d374a72b25586b040c965
joamag <joamag@gmail.com>
2017-07-24 12:58:19 +0100

    small hack on configuration

--- scripts/build/tools/curl.sh
+++ scripts/build/tools/curl.sh
@@ -9,8 +9,8 @@ rm -rf curl-$VERSION && tar -zxf "curl-$VERSION.tar.gz"
 rm -f "curl-$VERSION.tar.gz"
 cd curl-$VERSION
 
-sed -i -e 's/$SHELL $CONFIG_STATUS $ac_config_status_args/echo "1"/g' configure
-./configure --prefix=$PREFIX --with-ca-bundle=/usr/ssl/ca-bundle.crt || true
+sed -i -e 's/$SHELL $CONFIG_STATUS $ac_config_status_args/exit 0/g' configure
+./configure --prefix=$PREFIX --with-ca-bundle=/usr/ssl/ca-bundle.crt
 bash ./config.status
 
 make && make install
