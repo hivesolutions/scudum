9f37c1834f61c792de673111000a28eb829f7604
joamag <joamag@gmail.com>
2017-09-29 05:13:22 +0100

    new projects

---
+++ scripts/build/extras/open-iscsi.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-2.0.875}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "open-isns"
+
+wget "https://github.com/open-iscsi/open-iscsi/archive/$VERSION5.tar.gz"
+rm -rf open-iscsi-$VERSION && tar -zxf "$VERSION.tar.gz"
+rm -f "$VERSION.tar.gz"
+cd open-iscsi-$VERSION
+
+make && make install
---
+++ scripts/build/extras/open-isns.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-0.98}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "https://github.com/open-iscsi/open-isns/archive/v$VERSION.tar.gz"
+rm -rf open-isns-$VERSION && tar -zxf "v$VERSION.tar.gz"
+rm -f "v$VERSION.tar.gz"
+cd open-isns-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
