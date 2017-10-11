3e855f24eb4eefe78617fef0573509c296b63f1d
joamag <joamag@hive.pt>
2017-10-11 14:42:02 +0100

    new archive file

--- scripts/build/extras/ntfsprogs.sh
+++ scripts/build/extras/ntfsprogs.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-rget "ftp://ftp.mirrorservice.org/sites/distfiles.macports.org/ntfsprogs/ntfsprogs-$VERSION.tar.gz"\
+rget "http://archive.hive.pt/files/ntfsprogs-$VERSION.tar.gz"\
     "http://downloads.sourceforge.net/linux-ntfs/ntfsprogs-$VERSION.tar.gz"
 rm -rf ntfsprogs-$VERSION && tar -zxf "ntfsprogs-$VERSION.tar.gz"
 rm -f "ntfsprogs-$VERSION.tar.gz"
