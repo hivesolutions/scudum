85157507c0c326509d6715deb535145dad99b1b1
joamag <joamag@hive.pt>
2017-10-16 15:32:27 +0100

    new values

--- scripts/build/extras/kernel-src.sh
+++ scripts/build/extras/kernel-src.sh
@@ -1,6 +1,7 @@
 source /etc/scudum/KERNEL
 
-VERSION=${VERSION-}
+VERSION=${VERSION-$KSUFFIX}
+MINOR=${MINOR-$KMINOR}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -8,12 +9,12 @@ set -e +h
 
 source $DIR/common.sh
 
-#@todo we need to review all of these !!!!
+wget "https://builds.stage.hive.pt/kernel/kernel-$VERSION.source.tar.gz"
+rm -rf linux-$MINOR && tar -zxf "kernel-$VERSION.source.tar.gz"
+rm -f "kernel-$VERSION.source.tar.gz"
+mkdir -p $PREFIX/linux
+cp -rp linux-$MINOR $PREFIX/linux
 
-wget "http://www.ijg.org/files/jpegsrc.v$VERSION.tar.gz"
-rm -rf jpeg-$VERSION && tar -zxf "jpegsrc.v$VERSION.tar.gz"
-rm -f "jpegsrc.v$VERSION.tar.gz"
-cd jpeg-$VERSION
-
-./configure --prefix=$PREFIX
-make && make install
+mkdir -p /opt/src
+rm -rf /opt/linux && rm -rf /opt/src/linux
+ln -s /opt/src/linux $PREFIX/linux/linux-$MINOR
