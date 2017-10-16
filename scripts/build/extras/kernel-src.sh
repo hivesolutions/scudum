3b0abb7e9080cf505399e16c5fa8887db037b188
joamag <joamag@gmail.com>
2017-10-16 13:24:29 +0100

    new todo value

--- scripts/build/extras/kernel-src.sh
+++ scripts/build/extras/kernel-src.sh
@@ -1,10 +1,14 @@
-VERSION=${VERSION-9a}
+source /etc/scudum/KERNEL
+
+VERSION=${VERSION-}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 set -e +h
 
 source $DIR/common.sh
+
+#@todo we need to review all of these !!!!
 
 wget "http://www.ijg.org/files/jpegsrc.v$VERSION.tar.gz"
 rm -rf jpeg-$VERSION && tar -zxf "jpegsrc.v$VERSION.tar.gz"
