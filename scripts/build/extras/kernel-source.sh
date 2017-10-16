7babf2c99fa5f007d40dc32f4c25b26fd19e0946
joamag <joamag@hive.pt>
2017-10-16 19:58:48 +0100

    removed extra files

--- scripts/build/extras/kernel-source.sh
+++ scripts/build/extras/kernel-source.sh
@@ -13,8 +13,8 @@ wget "https://builds.stage.hive.pt/kernel/kernel-$VERSION.source.tar.gz"
 rm -rf linux-$MINOR && tar -zxf "kernel-$VERSION.source.tar.gz"
 rm -f "kernel-$VERSION.source.tar.gz"
 
-mkdir -p $PREFIX/linux && rm -rf $PREFIX/linux/linux-$MINOR
-cp -rp linux-$MINOR $PREFIX/linux
+#mkdir -p $PREFIX/linux && rm -rf $PREFIX/linux/linux-$MINOR
+#cp -rp linux-$MINOR $PREFIX/linux
 
-mkdir -p /opt/src && rm -f /opt/src/linux
-ln -s $PREFIX/linux/linux-$MINOR /opt/src/linux
+#mkdir -p /opt/src && rm -f /opt/src/linux
+#ln -s $PREFIX/linux/linux-$MINOR /opt/src/linux
