357aad05e62f715fa68ea974b45f39679390d30e
joamag <joamag@gmail.com>
2017-10-21 19:34:57 +0100

    new values

--- util/armorc/src/armorc/base.py
+++ util/armorc/src/armorc/base.py
@@ -144,7 +144,7 @@ class ArmorClient(object):
         pipe = subprocess.Popen(
             [
                 "mount", "-t", "cifs", "-o",
-                "mfsymlinks,cifsacl,username=" + cifs_username + ",password=" + cifs_password,
+                "mfsymlinks,cifsacl,nosetuids,noperm,username=" + cifs_username + ",password=" + cifs_password,
                 cifs_path, self.data_path
             ]
         )
@@ -161,7 +161,7 @@ class ArmorClient(object):
         pipe = subprocess.Popen(
             [
                 "mount", "-t", "cifs", "-o",
-                "mfsymlinks,cifsacl,forceuid,forcegid,uid=999,gid=999,dir_mode=0700,file_mode=0700,username=" + cifs_username + ",password=" + cifs_password,
+                "mfsymlinks,cifsacl,nosetuids,noperm,forceuid,forcegid,uid=999,gid=999,dir_mode=0700,file_mode=0700,username=" + cifs_username + ",password=" + cifs_password,
                 cifs_path, self.owner_path
             ]
         )
