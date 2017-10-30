ffb67ed7694af11cb59929f91b4a2e3edcd5f9a5
joamag <joamag@gmail.com>
2017-10-30 16:31:24 +0000

    small code change

--- scripts/build/extras/openjdk.sh
+++ scripts/build/extras/openjdk.sh
@@ -17,7 +17,6 @@ ln -svf $PREFIX/jdk-$VERSION/bin/javac $PREFIX/bin/javac
 ln -svf $PREFIX/jdk-$VERSION/bin/keytool $PREFIX/bin/keytool
 
 for file in /usr/ssl/certs/*.pem; do
-    #openssl x509 -in $file -inform pem -out temp.der -outform der
     keytool -import -noprompt -file $file -alias $file -keystore $PREFIX/jdk-$VERSION/lib/security/cacerts -storepass changeit
     rm -f temp.der
 done
