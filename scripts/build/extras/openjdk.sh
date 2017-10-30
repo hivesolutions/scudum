2305a1f1dd5dbf38178d31b870dc191ddb29438d
joamag <joamag@hive.pt>
2017-10-30 15:02:19 +0000

    new code

--- scripts/build/extras/openjdk.sh
+++ scripts/build/extras/openjdk.sh
@@ -7,8 +7,8 @@ set -e +h
 source $DIR/common.sh
 
 wget "http://download.java.net/java/GA/jdk9/$VERSION/binaries/openjdk-${VERSION}_linux-x64_bin.tar.gz"
-rm -rf jdk-$VERSION && tar -zxf "openjdk-$VERSION_linux-x64_bin.tar.gz"
-rm -f "openjdk-$VERSION_linux-x64_bin.tar.gz"
+rm -rf jdk-$VERSION && tar -zxf "openjdk-${VERSION}_linux-x64_bin.tar.gz"
+rm -f "openjdk-${VERSION}_linux-x64_bin.tar.gz"
 
 mv jdk-$VERSION $PREFIX
 
