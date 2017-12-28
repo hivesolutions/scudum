68924247ae39f4668e615c9a33942fc5417f3624
joamag <joamag@hive.pt>
2017-12-28 12:19:01 +0000

    new bootstrap

--- scripts/build/extras/go.sh
+++ scripts/build/extras/go.sh
@@ -7,6 +7,14 @@ set -e +h
 
 source $DIR/common.sh
 
+wget "https://storage.googleapis.com/golang/go$VERSION_LEGACY.tar.gz"
+rm -rf go && tar -zxf "go$VERSION_LEGACY.tar.gz"
+rm -f "go$VERSION_LEGACY.tar.gz"
+rm -rf $PREFIX/go_legacy && mv go go_legacy && mv go_legacy $PREFIX
+
+pushd $PREFIX/go_legacy/src
+    env CGO_ENABLED=0 ./make.bash
+popd
 
 wget "https://storage.googleapis.com/golang/go$VERSION.src.tar.gz"
 rm -rf go && tar -zxf "go$VERSION.src.tar.gz"
