6ffe3e10193a67c378ee12776108da51a49b15aa
joamag <joamag@hive.pt>
2017-12-28 15:20:05 +0000

    new bind key values

--- scripts/build/extras/zsh.sh
+++ scripts/build/extras/zsh.sh
@@ -16,6 +16,19 @@ cd zsh-$VERSION
 make && make install
 
 cat > /root/.zshrc  << "EOF"
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[5~" beginning-of-history
+bindkey "\e[6~" end-of-history
+bindkey "\e[3~" delete-char
+bindkey "\e[2~" quoted-insert
+bindkey "\e[5C" forward-word
+bindkey "\eOc" emacs-forward-word
+bindkey "\e[5D" backward-word
+bindkey "\eOd" emacs-backward-word
+bindkey "\ee[C" forward-word
+bindkey "\ee[D" backward-word
+bindkey "^H" backward-delete-word
 autoload -U compinit promptinit
 compinit
 promptinit
