56c1e0527ed719ed6254c63af1e7a6a24283b4fe
joamag <joamag@hive.pt>
2017-12-28 14:38:38 +0000

    new fish build

---
+++ scripts/build/extras/fish.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.7.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "https://fishshell.com/files/$VERSION/fish-$VERSION.tar.gz"
+rm -rf fish-$VERSION && tar -zxf "fish-$VERSION.tar.gz"
+rm -f "fish-$VERSION.tar.gz"
+cd fish-$VERSION
+
+./configure --host=$ARCH_TARGET --prefix=$PREFIX
+make && make install
