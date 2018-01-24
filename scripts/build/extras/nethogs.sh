e9941fd32e0c3ccb277c80e2086664062ee7dddf
joamag <joamag@hive.pt>
2018-01-24 14:43:36 +0000

    new target

--- scripts/build/extras/nethogs.sh
+++ scripts/build/extras/nethogs.sh
@@ -9,8 +9,8 @@ source $DIR/common.sh
 depends "libpcap"
 
 wget "https://github.com/raboof/nethogs/archive/v$VERSION.tar.gz"
-rm -rf nethogs && tar -zxf "v$VERSION.tar.gz"
+rm -rf nethogs-$VERSION && tar -zxf "v$VERSION.tar.gz"
 rm -f "v$VERSION.tar.gz"
-cd nethogs
+cd nethogs-$VERSION
 
 make && make DESTDIR=$PREFIX install
