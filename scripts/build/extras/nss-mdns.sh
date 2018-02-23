dd06ba38426354f1a8cc365248a0d5a96a95b617
joamag <joamag@hive.pt>
2018-02-23 14:39:14 +0000

    new project added

---
+++ scripts/build/extras/nss-mdns.sh
@@ -0,0 +1,18 @@
+VERSION=${VERSION-0.13.2}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "avahi" "glib"
+
+wget "https://github.com/lathiat/nss-mdns/releases/download/v$VERSION/nss-mdns-$VERSION.tar.gz"
+rm -rf nss-mdns-$VERSION && tar -zxf "nss-mdns-$VERSION.tar.gz"
+rm -f "nss-mdns-$VERSION.tar.gz"
+cd nss-mdns-$VERSION
+
+./configure --prefix=$PREFIX
+
+make && make install
