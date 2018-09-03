9ef92988908b3f0e1adb0d02ad304fdf38a9612c
joamag <joamag@gmail.com>
2018-09-03 20:58:52 +0100

    new haproxy script

---
+++ scripts/build/extras/haproxy.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-1.8.13}
+VERSION_L=${VERSION_L-1.8}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://www.haproxy.org/download/$VERSION_L/src/haproxy-$VERSION.tar.gz"
+rm -rf haproxy-$VERSION && tar -zxf "haproxy-$VERSION.tar.gz"
+rm -f "haproxy-$VERSION.tar.gz"
+cd haproxy-$VERSION
+
+make TARGET=generic && make install
