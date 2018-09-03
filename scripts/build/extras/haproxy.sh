a23805739d73f26be35c6afc1bdf92dc173f7b1b
joamag <joamag@gmail.com>
2018-09-03 21:02:35 +0100

    haproxy install

--- scripts/build/extras/haproxy.sh
+++ scripts/build/extras/haproxy.sh
@@ -7,9 +7,11 @@ set -e +h
 
 source $DIR/common.sh
 
+depends "pcre"
+
 wget "http://www.haproxy.org/download/$VERSION_L/src/haproxy-$VERSION.tar.gz"
 rm -rf haproxy-$VERSION && tar -zxf "haproxy-$VERSION.tar.gz"
 rm -f "haproxy-$VERSION.tar.gz"
 cd haproxy-$VERSION
 
-make TARGET=generic && make install PREFIX=$PREFIX
+make TARGET=generic USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_LIBCRYPT=1 && make install PREFIX=$PREFIX
