a846f1950fbc492725adc0000c40409b14f42a15
João Magalhães <joamag@hive.pt>
2018-09-10 15:56:50 +0100

    new exec permissions

---
+++ scripts/build/extras/xsv.sh
@@ -0,0 +1,13 @@
+VERSION=${VERSION-0.13.0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "https://github.com/BurntSushi/xsv/releases/download/$VERSION/xsv-$VERSION-x86_64-unknown-linux-musl.tar.gz"
+rm -rf xsv && tar -Jxf "xsv-$VERSION-x86_64-unknown-linux-musl.tar.gz"
+rm -f "xsv-$VERSION-x86_64-unknown-linux-musl.tar.gz"
+
+mv xsv $PREFIX/bin
