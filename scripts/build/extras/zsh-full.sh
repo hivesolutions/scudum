a52376a91e4d71c815e9f042913c4c715a742946
João Magalhães <joamag@hive.pt>
2018-09-24 14:42:09 +0100

    new file

---
+++ scripts/build/extras/zsh-full.sh
@@ -0,0 +1,12 @@
+VERSION=${VERSION-5.4.2}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "zsh"
+
+curl -L http://install.ohmyz.sh | sh
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="mh"/g' /root/.zshrc
