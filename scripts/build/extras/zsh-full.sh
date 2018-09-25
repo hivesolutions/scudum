c02a74ca78f4be8d572a1021b57f72970b2012c8
João Magalhães <joamag@hive.pt>
2018-09-25 09:45:50 +0100

    new zsh full

--- scripts/build/extras/zsh-full.sh
+++ scripts/build/extras/zsh-full.sh
@@ -10,3 +10,35 @@ depends "zsh"
 
 curl -L http://install.ohmyz.sh | sh
 sed -i 's/ZSH_THEME=".*"/ZSH_THEME="pygmalion"/g' /root/.zshrc
+
+cat >> /root/.zshrc  << "EOF"
+source /etc/profile.zsh
+EOF
+
+cat > /etc/profile.zsh  << "EOF"
+source /etc/colors
+source /config
+source /etc/environ
+
+DISTRIB=$(cat /etc/scudum/DISTRIB)
+VERSION=$(cat /etc/scudum/VERSION)
+KVERSION=$(uname -r)
+SVERSION=$DISTRIB-$VERSION
+
+UPTIME="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+UPTIME_SECS=$((${UPTIME}%60))
+UPTIME_MINS=$((${UPTIME}/60%60))
+UPTIME_HOURS=$((${UPTIME}/3600%24))
+UPTIME_DAYS=$((${UPTIME}/86400))
+UPTIME_S=$(printf "%d days, %02dh %02dm %02ds" "$UPTIME_DAYS" "$UPTIME_HOURS" "$UPTIME_MINS" "$UPTIME_SECS")
+
+for file in /etc/env/*.env; do
+    source $file
+done
+
+alias "ls=ls --color=auto"
+echo -e "Welcome to "$COLOR_GREEN"Scudum"$COLOR_RESET" $SVERSION (GNU/Linux $KVERSION)"
+echo -e "System uptime: $UPTIME_S"
+
+unset DISTRIB VERSION KVERSION SVERSION UPTIME UPTIME_SECS UPTIME_MINS UPTIME_HOURS UPTIME_DAYS UPTIME_S
+EOF
