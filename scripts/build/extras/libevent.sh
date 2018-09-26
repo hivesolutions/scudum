cf439ddc30a7685b57ae11df6fb2dcb20a65766b
joamag <joamag@gmail.com>
2018-09-26 08:19:33 +0100

    new versions

--- scripts/build/extras/libevent.sh
+++ scripts/build/extras/libevent.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-wget "https://github.com/downloads/libevent/libevent/libevent-$VERSION-stable.tar.gz"
+wget "https://github.com/libevent/libevent/releases/download/release-$VERSION-stable/libevent-$VERSION-stable.tar.gz"
 rm -rf libevent-$VERSION-stable && tar -zxf "libevent-$VERSION-stable.tar.gz"
 rm -f "libevent-$VERSION-stable.tar.gz"
 cd libevent-$VERSION-stable
