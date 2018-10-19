0d81ba6c04c68c166948f1fd6abb14e6e8133847
João Magalhães <joamag@hive.pt>
2018-10-19 10:50:14 +0100

    new bzip2 URLs

--- scripts/build/system/bzip2.sh
+++ scripts/build/system/bzip2.sh
@@ -2,7 +2,7 @@ VERSION=${VERSION-1.0.6}
 
 set -e +h
 
-wget --no-check-certificate "http://www.bzip.org/$VERSION/bzip2-$VERSION.tar.gz"
+wget --no-check-certificate "http://fossies.org/linux/misc/bzip2-$VERSION.tar.gz"
 rm -rf bzip2-$VERSION && tar -zxf "bzip2-$VERSION.tar.gz"
 rm -f "bzip2-$VERSION.tar.gz"
 cd bzip2-$VERSION
--- scripts/build/tools/bzip2.sh
+++ scripts/build/tools/bzip2.sh
@@ -2,7 +2,7 @@ VERSION=${VERSION-1.0.6}
 
 set -e +h
 
-wget "http://www.bzip.org/$VERSION/bzip2-$VERSION.tar.gz"
+wget "http://fossies.org/linux/misc/bzip2-$VERSION.tar.gz"
 rm -rf bzip2-$VERSION && tar -zxf "bzip2-$VERSION.tar.gz"
 rm -f "bzip2-$VERSION.tar.gz"
 cd bzip2-$VERSION
