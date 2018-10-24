63fd00bc83d405edccab370395ce05d74d55e7fe
João Magalhães <joamag@hive.pt>
2018-10-24 19:12:25 +0100

    removed unused rootfs

--- scripts/build/extras/zsh-full.sh
+++ scripts/build/extras/zsh-full.sh
@@ -39,7 +39,6 @@ source /etc/environ.zsh
 
 DISTRIB=$(cat /etc/scudum/DISTRIB)
 VERSION=$(cat /etc/scudum/VERSION)
-ROOTFS=$(cat /etc/scudum/ROOTFS)
 KVERSION=$(uname -r)
 SVERSION=$DISTRIB-$VERSION
 
@@ -58,7 +57,7 @@ for file in /etc/env/*.env; do
 done
 
 alias "ls=ls --color=auto"
-echo -e "Welcome to "$COLOR_GREEN"Scudum"$COLOR_RESET" $SVERSION (RootFS $ROOTFS) (GNU/Linux $KVERSION)"
+echo -e "Welcome to "$COLOR_GREEN"Scudum"$COLOR_RESET" $SVERSION (GNU/Linux $KVERSION)"
 echo -e ""
 echo -e " * Running Processes:  $PROCESSES"
 echo -e " * System uptime:      $UPTIME_S"
