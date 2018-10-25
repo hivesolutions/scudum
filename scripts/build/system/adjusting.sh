cb75a79476e3c5e581a5a190c6b1a8b958149017
joamag <joamag@gmail.com>
2018-10-25 23:37:50 +0100

    removed unused sysroot

--- scripts/build/system/adjusting.sh
+++ scripts/build/system/adjusting.sh
@@ -9,6 +9,8 @@ if [ "$SCUDUM_CROSS" == "0" ]; then
     ln -svf /tools/bin/ld /tools/$ARCH_TARGET/bin/ld
 fi
 
+# runs a series of replace operation that will make sure that
+# the /tools directory is present in the compiler paths
 $CC -dumpspecs | sed -e 's@/tools@@g'\
     -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}'\
     -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >\
