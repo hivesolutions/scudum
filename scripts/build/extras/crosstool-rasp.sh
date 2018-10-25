f46ab510e932a03187e1acb629dde28266147b0a
João Magalhães <joamag@hive.pt>
2018-10-25 13:15:10 +0100

    new patch file

--- scripts/build/extras/crosstool-rasp.sh
+++ scripts/build/extras/crosstool-rasp.sh
@@ -21,9 +21,22 @@ CT_ARCH_CPU="arm1176jzf-s"
 CT_ARCH_TUNE="arm1176jzf-s"
 CT_TARGET_VENDOR="rasp"
 CT_KERNEL_linux=y
-CT_CC_GCC_V_6_4_0=y
+CT_CC_GCC_V_6_3_0=y
 CT_LIBC_glibc=y
 CT_CC_LANG_CXX=y
+EOF
+
+cat > gcc6.patch << "EOF"
+--- a/gcc/ubsan.c
++++ b/gcc/ubsan.c
+@@ -1471,7 +1471,7 @@ ubsan_use_new_style_p (location_t loc)
+ 
+   expanded_location xloc = expand_location (loc);
+   if (xloc.file == NULL || strncmp (xloc.file, "\1", 2) == 0
+-      || xloc.file == '\0' || xloc.file[0] == '\xff'
++      || xloc.file[0] == '\0' || xloc.file[0] == '\xff'
+       || xloc.file[1] == '\xff')
+     return false;
 EOF
 
 ct-ng defconfig
