83679c994b4e6eff4ff06c47f2994e8afd46021c
João Magalhães <joamag@hive.pt>
2018-11-05 15:54:16 +0000

    tiny fix

--- scripts/build/tools/perl.sh
+++ scripts/build/tools/perl.sh
@@ -4,13 +4,16 @@ VERSION_MAJOR=${VERSION_MAJOR-5.0}
 set -e +h
 
 wget "http://www.cpan.org/src/$VERSION_MAJOR/perl-$VERSION.tar.gz"
+
+
+
 rm -rf perl-$VERSION && tar -zxf "perl-$VERSION.tar.gz"
 rm -f "perl-$VERSION.tar.gz"
 cd perl-$VERSION
 
 sh Configure -des -Dprefix=$PREFIX -Dlibs=-lm
 make
-cp -v perl cpan/podlators/pod2man $PREFIX/bin
+cp -v perl cpan/podlators/scripts/pod2man $PREFIX/bin
 mkdir -pv $PREFIX/lib/perl5/$VERSION
 cp -Rv lib/* $PREFIX/lib/perl5/$VERSION
 mkdir -pv $PREFIX/lib/perl5/$VERSION/pod
