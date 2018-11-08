24a08ae667827a7304299481f6fa83a069c5e67c
João Magalhães <joamag@hive.pt>
2018-11-08 12:23:32 +0000

    new jsonc version

--- scripts/build/extras/syslog-ng.sh
+++ scripts/build/extras/syslog-ng.sh
@@ -1,4 +1,5 @@
 VERSION=${VERSION-3.18.1}
+VERSION_JSON=${VERSION_JSON_C-0.13.1}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -10,6 +11,10 @@ wget "https://github.com/balabit/syslog-ng/releases/download/syslog-ng-$VERSION/
 rm -rf syslog-ng-$VERSION && tar -zxf "syslog-ng-$VERSION.tar.gz"
 rm -f "syslog-ng-$VERSION.tar.gz"
 cd syslog-ng-$VERSION
+
+wget "https://s3.amazonaws.com/json-c_releases/releases/json-c-$VERSION_JSON_C.tar.gz"
+tar -zxf "json-c-$VERSION_JSON_C.tar.gz"
+mv json-c-$VERSION_JSON_C jsonc
 
 ./configure --host=$ARCH_TARGET --prefix=$PREFIX
 make && make install
