cd6bffa40a4a13d37169dc60286aaf231a519af0
João Magalhães <joamag@hive.pt>
2018-11-08 15:27:48 +0000

    new lm sensors

---
+++ scripts/build/extras/lm-sensors.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-3-4-0}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+depends "which"
+
+wget "https://github.com/lm-sensors/lm-sensors/archive/V$VERSION.tar.gz"
+rm -rf lm-sensors-$VERSION && tar -zxf "V$VERSION.tar.gz"
+rm -f "V$VERSION.tar.gz"
+cd lm-sensors-$VERSION
+
+make PREFIX=$PREFIX && make install PREFIX=$PREFIX 
