273d569485fc2f80d6124b3f3b4d2c7c61db721c
joamag <joamag@gmail.com>
2018-11-11 23:43:23 +0000

    new gzip version

--- scripts/build/system/gzip.sh
+++ scripts/build/system/gzip.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.8}
+VERSION=${VERSION-1.9}
 
 set -e +h
 
@@ -6,6 +6,9 @@ wget --no-check-certificate "http://ftp.gnu.org/gnu/gzip/gzip-$VERSION.tar.xz"
 rm -rf gzip-$VERSION && tar -Jxf "gzip-$VERSION.tar.xz"
 rm -f "gzip-$VERSION.tar.xz"
 cd gzip-$VERSION
+
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
 
 ./configure --host=$ARCH_TARGET --prefix=/usr --bindir=/bin
 
