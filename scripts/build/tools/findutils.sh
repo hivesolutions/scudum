b966d0d489248a90445745da3afa77f3fc71fe12
joamag <joamag@gmail.com>
2018-11-11 20:31:52 +0000

    small fix added

--- scripts/build/tools/findutils.sh
+++ scripts/build/tools/findutils.sh
@@ -7,5 +7,9 @@ rm -rf findutils-$VERSION && tar -zxf "findutils-$VERSION.tar.gz"
 rm -f "findutils-$VERSION.tar.gz"
 cd findutils-$VERSION
 
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' gl/lib/*.c
+sed -i '/unistd/a #include <sys/sysmacros.h>' gl/lib/mountlist.c
+echo "#define _IO_IN_BACKUP 0x100" >> gl/lib/stdio-impl.h
+
 ./configure --prefix=$PREFIX
 make && make install
