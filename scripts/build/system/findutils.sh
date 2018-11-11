ee8c126c1d1880f6065b3e33f15b8890d2165ccf
joamag <joamag@gmail.com>
2018-11-11 20:30:39 +0000

    small fix in fundutils

--- scripts/build/system/findutils.sh
+++ scripts/build/system/findutils.sh
@@ -7,6 +7,10 @@ rm -rf findutils-$VERSION && tar -zxf "findutils-$VERSION.tar.gz"
 rm -f "findutils-$VERSION.tar.gz"
 cd findutils-$VERSION
 
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' gl/lib/*.c
+sed -i '/unistd/a #include <sys/sysmacros.h>' gl/lib/mountlist.c
+echo "#define _IO_IN_BACKUP 0x100" >> gl/lib/stdio-impl.h
+
 gl_cv_func_wcwidth_works=yes ./configure\
     --host=$ARCH_TARGET\
     --prefix=/usr\
