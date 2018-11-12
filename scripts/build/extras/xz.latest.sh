3075383f3c5d547a5477e7ce7b0ff05a73934aef
joamag <joamag@gmail.com>
2018-11-12 20:19:08 +0000

    new xz latest file

---
+++ scripts/build/extras/xz.latest.sh
@@ -0,0 +1,16 @@
+VERSION=${VERSION-5.2.4}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+wget "http://tukaani.org/xz/xz-$VERSION.tar.xz"
+rm -rf xz-$VERSION && tar -Jxf "xz-$VERSION.tar.xz"
+rm -f "xz-$VERSION.tar.xz"
+cd xz-$VERSION
+
+./configure --host=$ARCH_TARGET --prefix=$PREFIX --libdir=/lib\
+    --docdir=/usr/share/doc/xz-$VERSION
+
+make
+make pkgconfigdir=$PREFIX/lib/pkgconfig install
