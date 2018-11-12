29e64b4c42740b79b7b176035e5bfe0cfe588ce3
joamag <joamag@gmail.com>
2018-11-12 00:11:42 +0000

    new m4 patches

--- scripts/build/system/m4.sh
+++ scripts/build/system/m4.sh
@@ -7,6 +7,9 @@ rm -rf cd m4-$VERSION && tar -jxf "m4-$VERSION.tar.bz2"
 rm -f "m4-$VERSION.tar.bz2"
 cd m4-$VERSION
 
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+
 ./configure --host=$ARCH_TARGET --prefix=/usr
 
 make
