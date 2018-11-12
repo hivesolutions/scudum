6dbd8542cf8a9a3750eedf9a144c823c5b6807b3
João Magalhães <joamag@gmail.com>
2018-11-12 10:02:55 +0000

    new psmisc values

--- scripts/build/system/psmisc.sh
+++ scripts/build/system/psmisc.sh
@@ -1,13 +1,13 @@
-VERSION=${VERSION-22.20}
+VERSION=${VERSION-23.2}
 
 set -e +h
 
 export ac_cv_func_malloc_0_nonnull=yes
 export ac_cv_func_realloc_0_nonnull=yes
 
-wget --no-check-certificate "http://downloads.sourceforge.net/psmisc/psmisc-$VERSION.tar.gz"
-rm -rf psmisc-$VERSION && tar -zxf "psmisc-$VERSION.tar.gz"
-rm -f "psmisc-$VERSION.tar.gz"
+wget --no-check-certificate "http://downloads.sourceforge.net/psmisc/psmisc-$VERSION.tar.xz"
+rm -rf psmisc-$VERSION && tar -Jxf "psmisc-$VERSION.tar.xz"
+rm -f "psmisc-$VERSION.tar.xz"
 cd psmisc-$VERSION
 
 ./configure --host=$ARCH_TARGET --prefix=/usr
