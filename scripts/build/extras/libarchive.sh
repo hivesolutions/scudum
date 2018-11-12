d402e4aa096c9dbc70b95f98b87c78065c84d1a2
joamag <joamag@gmail.com>
2018-11-12 20:38:19 +0000

    small fix

--- scripts/build/extras/libarchive.sh
+++ scripts/build/extras/libarchive.sh
@@ -5,7 +5,7 @@ DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 set -e +h
 
 wget "https://www.libarchive.org/downloads/libarchive-$VERSION.tar.gz"
-rm -rf libarchive-$VERSION && tar -Jxf "libarchive-$VERSION.tar.gz"
+rm -rf libarchive-$VERSION && tar -zxf "libarchive-$VERSION.tar.gz"
 rm -f "libarchive-$VERSION.tar.gz"
 cd libarchive-$VERSION
 
