e88b7fdb294ae20fe72b91f7bdf90b06dd6669e0
João Magalhães <joamag@hive.pt>
2018-11-15 11:52:22 +0000

    new libcap support

---
+++ scripts/build/extras/libcap.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.26}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "https://git.kernel.org/pub/scm/libs/libcap/libcap.git/snapshot/libcap-$VERSION.tar.gz"
+rm -rf libcap-$VERSION && tar -zxf "libcap-$VERSION.tar.gz"
+rm -f "libcap-$VERSION.tar.gz"
+cd libcap-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
