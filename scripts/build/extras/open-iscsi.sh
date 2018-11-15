cc1d0bdb41849bc6714e94856597d018bf975b06
João Magalhães <joamag@hive.pt>
2018-11-15 12:09:03 +0000

    new latest version

--- scripts/build/extras/open-iscsi.sh
+++ scripts/build/extras/open-iscsi.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-2.0.877}
+VERSION=${VERSION-latest}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -8,9 +8,14 @@ source $DIR/common.sh
 
 depends "open-isns" "systemd"
 
-wget "https://github.com/open-iscsi/open-iscsi/archive/$VERSION.tar.gz"
-rm -rf open-iscsi-$VERSION && tar -zxf "$VERSION.tar.gz"
-rm -f "$VERSION.tar.gz"
-cd open-iscsi-$VERSION
+if [ "$VERSION" == "latest" ]; then
+    rm -rf open-iscsi && git clone --depth 1 "https://github.com/open-iscsi/open-iscsi.git"
+    cd open-iscsi
+else
+    wget "https://github.com/open-iscsi/open-iscsi/archive/$VERSION.tar.gz"
+    rm -rf open-iscsi-$VERSION && tar -zxf "$VERSION.tar.gz"
+    rm -f "$VERSION.tar.gz"
+    cd open-iscsi-$VERSION
+fi
 
 make && make install
