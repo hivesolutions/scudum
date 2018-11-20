964eba032d625dc01be2ccde69b78a33a4bb53c3
João Magalhães <joamag@hive.pt>
2018-11-20 15:17:31 +0000

    new acpid values

---
+++ scripts/build/extras/acpid.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-2.0.28}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "http://downloads.sourceforge.net/acpid2/acpid-$VERSION.tar.xz"
+rm -rf acpid-$VERSION && tar -Jxf "acpid-$VERSION.tar.xz"
+rm -f "acpid-$VERSION.tar.xz"
+cd acpid-$VERSION
+
+./configure --prefix=$PREFIX
+make && make install
