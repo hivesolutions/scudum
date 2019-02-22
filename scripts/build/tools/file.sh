50be941ce8868a5b7c68fcf8b1faebd13a8c1a53
joamag <joamag@gmail.com>
2019-02-22 21:18:19 +0000

    Small fix

--- scripts/build/tools/file.sh
+++ scripts/build/tools/file.sh
@@ -1,10 +1,6 @@
 VERSION=${VERSION-5.35}
 
-DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-
 set -e +h
-
-source $DIR/../base/functions.sh
 
 wget "https://fossies.org/linux/misc/file-$VERSION.tar.gz"
 rm -rf file-$VERSION && tar -zxf "file-$VERSION.tar.gz"
