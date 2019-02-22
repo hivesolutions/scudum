5d2ace9da1f499450d2f5d7f5a3e5b1805b859cf
joamag <joamag@gmail.com>
2019-02-22 12:06:45 +0000

    Small fix

--- scripts/build/tools/file.sh
+++ scripts/build/tools/file.sh
@@ -2,6 +2,8 @@ VERSION=${VERSION-5.35}
 
 set -e +h
 
+source $DIR/../base/functions.sh
+
 rget "ftp://ftp.astron.com/pub/file/file-$VERSION.tar.gz"
     "https://src.fedoraproject.org/lookaside/pkgs/file/file-$VERSION.tar.gz
 rm -rf file-$VERSION && tar -zxf "file-$VERSION.tar.gz"
