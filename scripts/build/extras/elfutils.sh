5443219f382f1d692bf2afc36e0ec2ddcb7bbb6c
João Magalhães <joamag@gmail.com>
2019-08-23 00:44:35 +0100

    New elfutils script

---
+++ scripts/build/extras/elfutils.sh
@@ -0,0 +1,15 @@
+VERSION=${VERSION-0.177}
+
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
+wget "https://sourceware.org/elfutils/ftp/$VERSION/elfutils-$VERSION.tar.bz2"
+rm -rf elfutils-$VERSION && tar -jxf "elfutils-$VERSION.tar.bz2"
+rm -f "elfutils-$VERSION.tar.bz2"
+cd elfutils-$VERSION.
+
+./configure --prefix=$PREFIX
+make && make install
