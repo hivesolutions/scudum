334d5d2441b06100972d3daac58d78e7edc4f2a0
joamag <joamag@gmail.com>
2019-09-05 19:05:27 +0100

    New cpi scipt

--- scripts/build/make.cpio.sh
+++ scripts/build/make.cpio.sh
@@ -9,6 +9,7 @@ KVARIANT=${KVARIANT-basic}
 CONFIG=${CONFIG-1}
 CLEANUP=${CLEANUP-1}
 DEPLOY=${DEPLOY-0}
+SQUASH=${SQUASH-1}
 
 CUR=$(pwd)
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
@@ -27,6 +28,14 @@ else
     FILE_LATEST=${FILE_LATEST-$NAME-$DISTRIB-latest.cpio.gz}
 fi
 
+if type apt-get &> /dev/null; then
+    apt-get -y install squashfs-tools
+elif type scu &> /dev/null; then
+    env -u VERSION scu install squashfs-tools
+else
+    exit 1
+fi
+
 if [ "$CONFIG" == "1" ]; then
     SCHEMA=$SCHEMA KVARIANT=$KVARIANT $DIR/config.sh
 fi
@@ -42,9 +51,26 @@ fi
 
 cd $SCUDUM
 
-find . -depth -print | cpio -ocvB | gzip -c > $CUR/$FILE
+rm -rfv images && mkdir -pv images
+tar -zcf images/root.tar.gz root
+tar -zcf images/dev.tar.gz dev
+tar -zcf images/etc.tar.gz etc
 
 cd $CUR
+
+if [ "$SQUASH" == "1" ]; then
+    CPIO_DIR=/tmp/$NAME.iso.dir
+    mksquashfs $(readlink -f $SCUDUM) $NAME.sqfs
+    mkdir -pv $CPIO_DIR
+    cp -rp $SCUDUM/boot $CPIO_DIR
+    cp -rp $SCUDUM/isolinux $CPIO_DIR
+    cp -rp $SCUDUM/usr/share/mkinitramfs/linuxrc $CPIO_DIR
+    mv -v $NAME.sqfs $CPIO_DIR
+else
+    CPIO_DIR=$SCUDUM
+fi
+
+find $CPIO_DIR -depth -print | cpio -ocvB | gzip -c > $FILE
 
 if [ "$DEPLOY" == "1" ]; then
     mkdir -pv $TARGET && mv -v $FILE $TARGET
