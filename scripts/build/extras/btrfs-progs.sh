2e5d14ca35c6c322140aee7f3d3153b33e78a39a
joamag <joamag@gmail.com>
2019-11-14 16:35:58 +0000

    New libraries

--- scripts/build/extras/btrfs-progs.sh
+++ scripts/build/extras/btrfs-progs.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "attr" "lzo" "zstd" "acl" "asciidoc" "xmlto"
+depends "attr" "zstd" "acl"
 
 wget "https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v$VERSION.tar.gz"
 rm -rf btrfs-progs-v$VERSION && tar -zxf "btrfs-progs-v$VERSION.tar.gz"
