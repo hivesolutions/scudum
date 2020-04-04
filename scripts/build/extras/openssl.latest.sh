7322ac5095bd4b5d13a603a152d572a4a80643e6
joamag <joamag@hive.pt>
2020-04-04 10:42:23 +0100

    Changed openssl URL structure

--- scripts/build/extras/openssl.latest.sh
+++ scripts/build/extras/openssl.latest.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-1.1.1}
+VERSION=${VERSION-1.1.1f}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
--- scripts/build/tools/openssl.sh
+++ scripts/build/tools/openssl.sh
@@ -1,4 +1,5 @@
-VERSION=${VERSION-1.0.2p}
+VERSION=${VERSION-1.0.2u}
+VERSION_L=${VERSION_L-1.0.2}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -8,7 +9,7 @@ source $DIR/../base/functions.sh
 
 unset MAKEFLAGS TEST
 
-rget "https://www.openssl.org/source/openssl-$VERSION.tar.gz"\
+rget "https://www.openssl.org/source/old/$VERSION_L/openssl-$VERSION.tar.gz"\
     "http://mirrors.ibiblio.org/openssl/source/openssl-$VERSION.tar.gz"
 rm -rf openssl-$VERSION && tar -zxf "openssl-$VERSION.tar.gz"
 rm -f "openssl-$VERSION.tar.gz"
