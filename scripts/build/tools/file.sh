9b7035c5d8e1fdc7cb03a79d75e60b0dd5533ce7
joamag <joamag@hive.pt>
2020-05-25 18:08:41 +0100

    Added new https support for file

--- scripts/build/tools/file.sh
+++ scripts/build/tools/file.sh
@@ -2,7 +2,7 @@ VERSION=${VERSION-5.37}
 
 set -e +h
 
-wget "http://download.openpkg.org/components/cache/file/file-$VERSION.tar.gz"
+wget "https://sources.buildroot.net/file/file-$VERSION.tar.gz"
 rm -rf file-$VERSION && tar -zxf "file-$VERSION.tar.gz"
 rm -f "file-$VERSION.tar.gz"
 cd file-$VERSION
