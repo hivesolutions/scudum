9eefd82d8466e4e3ebd86741de70915e2d9d8938
joamag <joamag@hive.pt>
2020-06-03 15:40:34 +0100

    Reversed error in sourceforge links

--- scripts/build/extras/docbook-xsl.sh
+++ scripts/build/extras/docbook-xsl.sh
@@ -31,18 +31,18 @@ if [ ! -f /etc/xml/catalog ]; then
 fi
 
 xmlcatalog --noout --add "rewriteSystem"\
-    "http://docbook.sourceforge.net/project/release/xsl/$VERSION"\
+    "http://docbook.sourceforge.net/release/xsl/$VERSION"\
     "$PREFIX/share/xml/docbook/xsl-stylesheets-$VERSION"\
     /etc/xml/docbook
 xmlcatalog --noout --add "rewriteURI"\
-    "http://docbook.sourceforge.net/project/release/xsl/$VERSION"\
+    "http://docbook.sourceforge.net/release/xsl/$VERSION"\
     "$PREFIX/share/xml/docbook/xsl-stylesheets-$VERSION"\
     /etc/xml/docbook
 xmlcatalog --noout --add "rewriteSystem"\
-    "http://docbook.sourceforge.net/project/release/xsl/current"\
+    "http://docbook.sourceforge.net/release/xsl/current"\
     "$PREFIX/share/xml/docbook/xsl-stylesheets-$VERSION"\
     /etc/xml/docbook
 xmlcatalog --noout --add "rewriteURI"\
-    "http://docbook.sourceforge.net/project/release/xsl/current"\
+    "http://docbook.sourceforge.net/release/xsl/current"\
     "$PREFIX/share/xml/docbook/xsl-stylesheets-$VERSION"\
     /etc/xml/docbook
