6f3cbc271f63c04bf1b04f79ea5146b836260eac
joamag <joamag@hive.pt>
2020-06-06 00:55:35 +0100

    Removed problematic docs

--- scripts/build/extras/lshw.sh
+++ scripts/build/extras/lshw.sh
@@ -18,8 +18,9 @@ else
     cd lshw-$VERSION
 fi
 
+rm -rf docs
+
 if [ "$SCUDUM_CROSS" == "1" ]; then
-    rm -rf docs
     make CXX="$CXX" && make install PREFIX=$PREFIX
 else
     make && make install PREFIX=$PREFIX
