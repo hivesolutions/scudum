a08751ad204a4ed3fe1b3efd406c86bbe57cf486
joamag <joamag@hive.pt>
2020-06-06 00:27:43 +0100

    Removed rsync from deps

--- scripts/build/base/deps.sh
+++ scripts/build/base/deps.sh
@@ -2,8 +2,8 @@ set -e +h
 
 if type apt-get &> /dev/null; then
     apt-get -y install wget g++ make bison flex gawk gperf cpio\
-        gzip bzip2 xz-utils patch texinfo rsync libncurses5\
-        libncurses5-dev libssl-dev
+        gzip bzip2 xz-utils patch texinfo libncurses5 libncurses5-dev\
+        libssl-dev
 elif type scu &> /dev/null; then
     echo "scudum: dependencies already installed"
 else
