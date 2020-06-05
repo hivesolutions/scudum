6aacd3b40fe8828192b3edcd9a64f89559ab149e
joamag <joamag@hive.pt>
2020-06-05 21:17:05 +0100

    Added cpio dependency

--- scripts/build/base/deps.sh
+++ scripts/build/base/deps.sh
@@ -1,7 +1,7 @@
 set -e +h
 
 if type apt-get &> /dev/null; then
-    apt-get -y install wget g++ make bison flex gawk gperf\
+    apt-get -y install wget g++ make bison flex gawk gperf cpio\
         gzip bzip2 xz-utils patch texinfo libncurses5 libncurses5-dev\
         libssl-dev
 elif type scu &> /dev/null; then
