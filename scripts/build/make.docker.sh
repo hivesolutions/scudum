8ef091d78b9a73722e024c4d5322f2ce4920f18f
joamag <joamag@hive.pt>
2020-06-06 01:46:05 +0100

    Better docker install

--- scripts/build/make.docker.sh
+++ scripts/build/make.docker.sh
@@ -28,6 +28,9 @@ else
 fi
 
 if type apt-get &> /dev/null; then
+    apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     apt-get -y install docker-ce
 elif type scu &> /dev/null; then
     env -u VERSION scu install docker
