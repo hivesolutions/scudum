e9eb14b695684e97551d0feaadaaeb62cd0ced47
joamag <joamag@hive.pt>
2020-06-08 01:22:12 +0100

    Adde no check certificate python3

--- scripts/build/tools/python3.sh
+++ scripts/build/tools/python3.sh
@@ -2,7 +2,7 @@ VERSION=${VERSION-3.8.3}
 
 set -e +h
 
-wget --content-disposition "http://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz"
+wget --no-check-certificate --content-disposition "https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz"
 rm -rf Python-$VERSION && tar -zxf "Python-$VERSION.tgz"
 rm -f "Python-$VERSION.tgz"
 cd Python-$VERSION
