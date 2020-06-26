4e7b294301b067d16d66a42fe5bb0d5574c85e16
João Magalhães <joamag@gmail.com>
2020-06-26 09:40:53 +0100

    Forced HTTPS in sqlite3

--- scripts/build/extras/sqlite3.sh
+++ scripts/build/extras/sqlite3.sh
@@ -13,7 +13,7 @@ export CFLAGS="$CFLAGS -DSQLITE_ENABLE_FTS3=1\
     -DSQLITE_ENABLE_UNLOCK_NOTIFY=1\
     -DSQLITE_SECURE_DELETE=1"
 
-wget --content-disposition "http://www.sqlite.org/$YEAR/sqlite-autoconf-$VERSION.tar.gz"
+wget --content-disposition "https://www.sqlite.org/$YEAR/sqlite-autoconf-$VERSION.tar.gz"
 rm -rf sqlite-autoconf-$VERSION && tar -zxf "sqlite-autoconf-$VERSION.tar.gz"
 rm -f "sqlite-autoconf-$VERSION.tar.gz"
 cd sqlite-autoconf-$VERSION
