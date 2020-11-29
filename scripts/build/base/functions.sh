bed4c4ee7752a675099bff8b0578a5664d36d1f2
João Magalhães <joamag@gmail.com>
2020-11-29 10:45:32 +0000

    Support for extra params in rget

--- scripts/build/base/functions.sh
+++ scripts/build/base/functions.sh
@@ -5,15 +5,33 @@ depends() {
 }
 
 rget() {
-    for url in "$@"; do
-        wget --content-disposition --tries=1 --timeout=20 $url && return 0
+    urls=()
+    params=()
+    for value in "$@"; do
+        if [[ "$value" == "-"* ]]; then
+            params+=("$value")
+        else
+            urls+=("$value")
+        fi
+    done
+    for url in ${urls[*]}; do
+        wget --content-disposition --tries=1 --timeout=20 $url ${params[*]} && return 0
     done
     return 1
 }
 
 rgeti() {
+    urls=()
+    params=()
+    for value in "$@"; do
+        if [[ "$value" == "-"* ]]; then
+            params+=("$value")
+        else
+            urls+=("$value")
+        fi
+    done
     for url in "$@"; do
-        wget --no-check-certificate --content-disposition --tries=1 --timeout=20 $url && return 0
+        wget --no-check-certificate --content-disposition --tries=1 --timeout=20 $url ${params[*]} && return 0
     done
     return 1
 }
