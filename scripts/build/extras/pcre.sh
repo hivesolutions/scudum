572497b2bae00d06416606a49aa14ca8462aab8c
João Magalhães <joamag@gmail.com>
2022-04-25 19:26:54 +0100

    fix: pcre download site

--- scripts/build/extras/pcre.sh
+++ scripts/build/extras/pcre.sh
@@ -1,4 +1,4 @@
-VERSION=${VERSION-8.44}
+VERSION=${VERSION-8.45}
 
 DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-wget --content-disposition "https://ftp.pcre.org/pub/pcre/pcre-$VERSION.tar.gz"
+wget --content-disposition "http://downloads.sourceforge.net/pcre/pcre-$VERSION.tar.gz?use_mirror=netix" "--output-document=pcre-$VERSION.tar.gz"
 rm -rf pcre-$VERSION && tar -zxf "pcre-$VERSION.tar.gz"
 rm -f "pcre-$VERSION.tar.gz"
 cd pcre-$VERSION
