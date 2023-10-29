e06aad5ea53098decb479ec0e717916e8a74be1d
João Magalhães <joamag@gmail.com>
2023-10-29 14:56:37 +0000

    fix: re-added fpic to ntp Required for cross compile.

--- scripts/build/extras/ntp.sh
+++ scripts/build/extras/ntp.sh
@@ -7,6 +7,9 @@ set -e +h
 
 source $DIR/common.sh
 
+if [ -z "$CFLAGS" ]; then export CFLAGS="-O2"; fi
+export CFLAGS="$CFLAGS -fpic"
+
 rget "https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-$VERSION_M/ntp-$VERSION.tar.gz"\
     "https://ftp.osuosl.org/pub/blfs/conglomeration/ntp/ntp-$VERSION.tar.gz"
 rm -rf ntp-$VERSION && tar -zxf "ntp-$VERSION.tar.gz"
