758b52a0b6ad5e5d7637d8f978b8f1c440a8dba8
João Magalhães <joamag@gmail.com>
2024-01-03 00:23:24 +0000

    chore: small fix

--- scripts/build/extras/ca-certs.sh
+++ scripts/build/extras/ca-certs.sh
@@ -6,7 +6,7 @@ source $DIR/common.sh
 
 rget http://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt
 
-rm -f usr/share/ssl/certdata.txt &&\
+rm -f /usr/share/ssl/certdata.txt &&\
     mv certdata.txt /usr/share/ssl
 
 cert.build
