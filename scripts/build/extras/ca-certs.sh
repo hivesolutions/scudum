2ecad404b623fac29df94439fec1cefcc92dc2dc
João Magalhães <joamag@gmail.com>
2024-01-03 00:20:03 +0000

    chore: removed https requirement

--- scripts/build/extras/ca-certs.sh
+++ scripts/build/extras/ca-certs.sh
@@ -4,7 +4,7 @@ set -e +h
 
 source $DIR/common.sh
 
-rget https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt
+rget http://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt
 
 rm -f usr/share/ssl/certdata.txt &&\
     cp -rp certdata.txt /usr/share/ssl/certdata.txt
