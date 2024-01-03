9bab0832a5797f6dad75fc4800dda1058e1df151
João Magalhães <joamag@gmail.com>
2024-01-03 00:17:10 +0000

    chore: improved script

--- scripts/build/extras/ca-certs.sh
+++ scripts/build/extras/ca-certs.sh
@@ -1,3 +1,9 @@
+DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+set -e +h
+
+source $DIR/common.sh
+
 rget https://hg.mozilla.org/mozilla-central/raw-file/tip/security/nss/lib/ckfw/builtins/certdata.txt
 
 rm -f usr/share/ssl/certdata.txt &&\
