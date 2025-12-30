929f62f27d8d9c7e17556eab101ae42c398dfa8b
João Magalhães <joamag@gmail.com>
2025-12-30 03:27:58 +0000

    chore: removed extra dependencies

--- scripts/build/extras/btrfs-progs.sh
+++ scripts/build/extras/btrfs-progs.sh
@@ -6,7 +6,7 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "attr" "lzo" "zstd" "acl" "asciidoc" "xmlto" "python3"
+depends "attr" "lzo" "zstd" "acl" "python3"
 
 wget --content-disposition "https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v$VERSION.tar.gz"
 rm -rf btrfs-progs-v$VERSION && tar -zxf "btrfs-progs-v$VERSION.tar.gz"
