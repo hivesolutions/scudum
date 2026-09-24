033431e1fb60d563ad1e46f036641493816b1c7a
João Magalhães <joamag@gmail.com>
2026-09-24 20:05:08 +0100

    fix: patch the temporary gawk for builds without mpfr
    - apply the upstream awk.h fix, unassigned array elements broke the gcc options generation

--- scripts/build/tools/gawk.sh
+++ scripts/build/tools/gawk.sh
@@ -8,9 +8,16 @@ source $DIR/../base/functions.sh
 
 rget "https://mirrors.hive.pt/mirrors/scudum/gawk/$VERSION/gawk-$VERSION.tar.xz"\
     "https://ftpmirror.gnu.org/gawk/gawk-$VERSION.tar.xz"
+rget "https://mirrors.hive.pt/mirrors/scudum/gawk/$VERSION/gawk-$VERSION-upstream_fixes-1.patch"\
+    "https://git.savannah.gnu.org/cgit/gawk.git/patch/awk.h?id=bf85f8a3175af703597082d4c7e0abc2066a44d3"\
+    "--output-document=gawk-$VERSION-upstream_fixes-1.patch"
 rm -rf gawk-$VERSION && tar -Jxf "gawk-$VERSION.tar.xz"
 rm -f "gawk-$VERSION.tar.xz"
 cd gawk-$VERSION
+
+# fixes the node structure for builds without mpfr (as this one) where
+# unassigned array elements do not compare equal to the empty string
+patch -Np1 -i ../gawk-$VERSION-upstream_fixes-1.patch
 
 sed -i 's/extras//' Makefile.in
 
