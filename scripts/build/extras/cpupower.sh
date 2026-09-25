23ea8b2b3da5f8391a0f9867c872b51f5c413756
João Magalhães <joamag@gmail.com>
2026-09-25 20:42:39 +0100

    fix: install cpupower under its own prefix and require which
    - the DESTDIR typo made it work by accident, the makefile uses /usr paths
    
    - its makefile looks for msgfmt with which, missing from the root

--- scripts/build/extras/cpupower.sh
+++ scripts/build/extras/cpupower.sh
@@ -7,11 +7,11 @@ set -e +h
 
 source $DIR/common.sh
 
-depends "pci-utils"
+depends "pci-utils" "which"
 
 wget --content-disposition "https://www.kernel.org/pub/linux/kernel/v$VERSION_L/linux-$VERSION.tar.xz"
 rm -rf linux-$VERSION && tar -Jxf "linux-$VERSION.tar.xz"
 rm -f "linux-$VERSION.tar.xz"
 cd linux-$VERSION/tools/power/cpupower
 
-make && make DESTDIR=$PRFEFIX install
+make && make install
