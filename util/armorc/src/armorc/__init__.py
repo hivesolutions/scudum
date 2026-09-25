e09dad22762cb3f0ca0db856824b526b1816e459
João Magalhães <joamag@gmail.com>
2026-09-25 20:42:39 +0100

    chore: new year 2026

--- util/armorc/src/armorc/__init__.py
+++ util/armorc/src/armorc/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Hive Armor
-# Copyright (c) 2008-2022 Hive Solutions Lda.
+# Copyright (c) 2008-2026 Hive Solutions Lda.
 #
 # This file is part of Hive Armor.
 #
@@ -19,7 +19,7 @@
 # You should have received a copy of the Apache License along with
 # Hive Armor. If not, see <http://www.apache.org/licenses/>.
 
-__copyright__ = "Copyright (c) 2008-2022 Hive Solutions Lda."
+__copyright__ = "Copyright (c) 2008-2026 Hive Solutions Lda."
 """ The copyright for the module """
 
 __license__ = "Apache License, Version 2.0"
--- util/armord/src/armord/models/__init__.py
+++ util/armord/src/armord/models/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Hive Armor Daemon
-# Copyright (c) 2008-2022 Hive Solutions Lda.
+# Copyright (c) 2008-2026 Hive Solutions Lda.
 #
 # This file is part of Hive Armor Daemon.
 #
@@ -19,7 +19,7 @@
 # You should have received a copy of the Apache License along with
 # Hive Armor Daemon. If not, see <http://www.apache.org/licenses/>.
 
-__copyright__ = "Copyright (c) 2008-2022 Hive Solutions Lda."
+__copyright__ = "Copyright (c) 2008-2026 Hive Solutions Lda."
 """ The copyright for the module """
 
 __license__ = "Apache License, Version 2.0"
